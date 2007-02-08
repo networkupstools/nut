a5ffa6d71ce944f810d8537ca6fffa92a359a9c4
Alexander Gordeev <agordeev-guest@alioth.debian.org>
2007-02-08 15:27:53 +0000

    drivers/megatec_usb.c: fixed "UPS No Ack" issue...
    ...and optimized get_data_krauler. Also did some code reformatting.
    
    Fossil-ID: SVN r800

--- drivers/megatec_usb.c
+++ drivers/megatec_usb.c
@@ -79,11 +79,11 @@ static usb_ups_t KnownDevices[]={
     { .vid=0 }     /* end of list */
 };
 
-// TODO: Fix matching non-auto selected devices
+/* TODO: Fix matching non-auto selected devices */
 static int comm_usb_match(HIDDevice *d, void *privdata)
 {
     usb_ups_t   *p;
-    
+
     for (p=KnownDevices;p->vid!=0;p++)
     {
         if ( (p->vid==d->VendorID) && (p->pid==d->ProductID) ) 
@@ -153,7 +153,7 @@ int ser_open(const char *port)
     }
 
     ret = usb->open(&udev,&hiddevice,&match,u.report_desc,MODE_OPEN);
-    if (ret<0) 
+    if (ret<0)
         usb_open_error(port);
 
     /* flush input buffers */
@@ -208,7 +208,7 @@ int ser_get_line(int fd, char *buf, size_t buflen, char endchar,
 {
     int len;
     char *src,*dst,c;
-    
+
     if (NULL==udev) 
         return -1;
 
@@ -218,14 +218,14 @@ int ser_get_line(int fd, char *buf, size_t buflen, char endchar,
 
     dst = buf;
 
-    for (src=buf;src!=(buf+len);src++)
+    for (src=buf; src!=(buf+len); src++)
     {
         c = *src;
 
-        if ( c==endchar )
+        if ( c == endchar )
             break;
 
-        if ( (c==0) || ( strchr(ignset,c)!=NULL ) )
+        if ( (c == 0) || ( (ignset != NULL) && (strchr(ignset,c) != NULL) ) )
             continue;
 
         *(dst++) = c;
@@ -245,7 +245,7 @@ int ser_get_line(int fd, char *buf, size_t buflen, char endchar,
     Agiler serial-to-usb device.
 
     Protocol was reverse-engineered from Windows driver
-    HID tables are complitely bogus
+    HID tables are completely bogus
     Data is transferred out as one 8-byte packet with report ID 0
     Data comes in as 6 8-byte reports per line , padded with zeroes
     All constants are hardcoded in windows driver
@@ -309,15 +309,17 @@ static int get_data_agiler(char *buffer,int buffer_size)
 /*
     Krauler serial-to-usb device.
 
-    Protocol was reverse-engineered using Windows driver
+    Protocol was reverse-engineered using Windows driver.
 */
 
-#define KRAULER_COMMAND_BUFFER_SIZE    9
-#define KRAULER_TIMEOUT            5000
+#define KRAULER_COMMAND_BUFFER_SIZE	9
+#define KRAULER_TIMEOUT		5000
+#define KRAULER_WRONG_ANSWER		"PS No Ack"
+#define KRAULER_MAX_ATTEMPTS		4
 
-static    char    krauler_command_buffer[KRAULER_COMMAND_BUFFER_SIZE];
+static	char	krauler_command_buffer[KRAULER_COMMAND_BUFFER_SIZE];
 
-static    int set_data_krauler(const char *str)
+static	int set_data_krauler(const char *str)
 {
     int    len;
 
@@ -335,44 +337,63 @@ static    int set_data_krauler(const char *str)
     return len;
 }
 
-static    int get_data_krauler(char *buffer,int buffer_size)
+static	int get_data_krauler(char *buffer,int buffer_size)
 {
     int res = 0;
-    int i;
+    unsigned char index = 0;
+    int i, j;
+    int retries = KRAULER_MAX_ATTEMPTS;
 
     if (strcmp(krauler_command_buffer, "Q1\r") == 0)
-    {
-        res = usb_get_descriptor(udev, USB_DT_STRING, 0x03, buffer, buffer_size);
-        /*res = usb_control_msg(udev, USB_ENDPOINT_IN+1, USB_REQ_GET_DESCRIPTOR,
-                      (USB_DT_STRING << 8) + 3, 0, buffer, 0x9, USB_TIMEOUT);*/
-    }
+        index = 0x03;
+    else if (strcmp(krauler_command_buffer, "I\r") == 0)
+        index = 0x0c;
+    else if (strcmp(krauler_command_buffer, "F\r") == 0)
+        index = 0x0d;
 
-    if (strcmp(krauler_command_buffer, "I\r") == 0)
-    {
-        res = usb_get_descriptor(udev, USB_DT_STRING, 0x0c, buffer, buffer_size);
-        /*res = usb_control_msg(udev, USB_ENDPOINT_IN+1, USB_REQ_GET_DESCRIPTOR,
-                      (USB_DT_STRING << 8) + 3, 0, buffer, 0x9, USB_TIMEOUT);*/
-    }
+    if (index > 0)
+        while(retries)
+        {
+            /*res = usb_get_descriptor(udev, USB_DT_STRING, index, buffer, buffer_size);*/
+            res = usb_control_msg(udev, USB_ENDPOINT_IN+1, USB_REQ_GET_DESCRIPTOR,
+                                         (USB_DT_STRING << 8) + index, 0, buffer,
+                                         buffer_size, KRAULER_TIMEOUT);
 
-    if (strcmp(krauler_command_buffer, "F\r") == 0)
-    {
-        res = usb_get_descriptor(udev, USB_DT_STRING, 0x0d, buffer, buffer_size);
-        /*res = usb_control_msg(udev, USB_ENDPOINT_IN+1, USB_REQ_GET_DESCRIPTOR,
-                      (USB_DT_STRING << 8) + 3, 0, buffer, 0x9, USB_TIMEOUT);*/
-    }
-    
-    if(res >= 4)
-        memset(buffer,0,4);
+            if(res > 0)
+            {
+                /*hexdump(stdout,buffer,res);
+                asciidump(stdout,buffer,res);*/
+
+                for(i = 4,j = 0; i < res; i++)
+                    if(buffer[i] != 0)
+                    {
+                        buffer[j] = buffer[i];
+                        j++;
+                    }
+                buffer[j] = 0;
+                res = j;
+
+                upsdebugx(5, "get_data_krauler: got data");
+                //upsdebugx(5, "get_data_krauler: %s", buffer);
+                if(strcmp(buffer,KRAULER_WRONG_ANSWER) != 0)
+                    break;
+                else upsdebugx(5, "get_data_krauler: ups no ack");
+            }
+            else break;
+
+            retries--;
+        }
+
 
     krauler_command_buffer[0] = 0;
     return res;
 }
 
 /*
-    Ablerex seraial-to-usb device.
+    Ablerex serial-to-usb device.
 
     Protocol was reverse-engineered from Windows driver
-    HID tables are complitely bogus
+    HID tables are completely bogus
     Data is transferred out as one 8-byte packet with report ID 0
     Data comes in as 1 47-byte report per line , padded with zeroes
     All constants are hardcoded in windows driver
