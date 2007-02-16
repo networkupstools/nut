e06b583e9ad6d35aa8f93a2851076ef50b70b613
Peter Selinger <selinger@users.sourceforge.net>
2007-02-16 21:57:32 +0000

    hotplug/udev: added hotplugging rules for various USB devices
    usbhid-ups: added product id for Belkin F6C100-UNV
    
    Fossil-ID: SVN r820

--- drivers/belkin-hid.c
+++ drivers/belkin-hid.c
@@ -385,6 +385,7 @@ static int belkin_claim(HIDDevice *hd) {
 	/* accept any known UPS - add devices here as needed */
 	case 0x0980:  /* F6C800-UNV */
 	case 0x0900:  /* F6C900-UNV */
+	case 0x0910:  /* F6C100-UNV */
 	case 0x0912:  /* F6C120-UNV */
 	case 0x0551:  /* F6C550-AVR */
 	case 0x0751:  /* F6C1500-TW-RK */
