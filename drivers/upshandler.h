ed8227ced10ed6b40eef887351369f7a4e88d0df
Arjen de Korte <adkorte-guest@alioth.debian.org>
2007-09-28 19:08:21 +0000

    The return values for the setvar() and instcmd() handlers...
    ...are now an enum value, to add return values easily.
    
    Fossil-ID: SVN r1129

--- drivers/upshandler.h
+++ drivers/upshandler.h
@@ -20,6 +20,22 @@
 #ifndef NUT_UPSHANDLER_H
 #define NUT_UPSHANDLER_H
 
+/* return values for instcmd */
+enum {
+	STAT_INSTCMD_HANDLED = 0,	/* completed successfully */
+	STAT_INSTCMD_UNKNOWN,		/* unspecified error */
+	STAT_INSTCMD_INVALID,		/* invalid command */
+	STAT_INSTCMD_FAILED		/* command failed */
+};
+
+/* return values for setvar */
+enum {
+	STAT_SET_HANDLED = 0,		/* completed successfully */
+	STAT_SET_UNKNOWN,		/* unspecified error */
+	STAT_SET_INVALID,		/* not writeable */
+	STAT_SET_FAILED			/* writing failed */
+};
+
 /* structure for funcs that get called by msg parse routine */
 struct ups_handler
 {
