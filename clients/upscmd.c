69ac7e1892ae51de430789c4c6915c9c619fe832
Arjen de Korte <adkorte-guest@alioth.debian.org>
2008-04-01 20:18:42 +0000

    be a good little client that cleans up after itself
    One thing that is not so good about this client, is that it uses
    'getpass' to read the password from the command line. This has been
    marked 'obsolete' by POSIX several years ago already and is generally
    considered to be not very portable (although so far not many people
    seem to have complained about us using it). Any suggestions here?
    
    Fossil-ID: SVN r1412

--- clients/upscmd.c
+++ clients/upscmd.c
@@ -27,12 +27,15 @@
 
 #include "upsclient.h"
 
+static char		*upsname = NULL, *hostname = NULL;
+static UPSCONN_t	*ups = NULL;
+
 struct list_t {
 	char	*name;
-	struct	list_t	*next;
+	struct list_t	*next;
 };
 
-static void usage(char *prog)
+static void usage(const char *prog)
 {
 	printf("Network UPS Tools upscmd %s\n\n", UPS_VERSION);
 	printf("usage: %s [-h]\n", prog);
@@ -48,27 +51,14 @@ static void usage(char *prog)
 	printf("  <ups>		UPS identifier - <upsname>[@<hostname>[:<port>]]\n");
 	printf("  <command>	Valid instant command - test.panel.start, etc.\n");
 	printf("  [<value>]	Additional data for command - number of seconds, etc.\n");
-
-	exit(EXIT_SUCCESS);
 }
 
-static void clean_exit(UPSCONN_t *ups, char *upsname, char *hostname, int code)
+static void print_cmd(char *cmdname)
 {
-	free(upsname);
-	free(hostname);
-
-	upscli_sendline(ups, "LOGOUT\n", 7);
-	upscli_disconnect(ups);
-
-	exit(code);
-}
-
-static void print_cmd(UPSCONN_t *ups, const char *upsname, char *cmdname)
-{
-	int	ret;
+	int		ret;
 	unsigned int	numq, numa;
-	char	**answer;
-	const	char	*query[4];
+	const char	*query[4];
+	char		**answer;
 
 	query[0] = "CMDDESC";
 	query[1] = upsname;
@@ -86,102 +76,68 @@ static void print_cmd(UPSCONN_t *ups, const char *upsname, char *cmdname)
 	printf("%s - %s\n", cmdname, answer[3]);
 }
 
-static void listcmds(char *rawname)
+static void listcmds(void)
 {
-	int	ret, port;
+	int		ret;
 	unsigned int	numq, numa;
-	char	*upsname, *hostname, **answer;
-	const	char	*query[4];
-	UPSCONN_t	ups;
-	struct	list_t	*lhead, *llast, *ltmp, *lnext;
-
-	upsname = hostname = NULL;
-
-	if (upscli_splitname(rawname, &upsname, &hostname, &port) != 0) {
-		fprintf(stderr, "Error: invalid UPS definition.  Required format: upsname[@hostname[:port]]\n");
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
-	}
-
-	if (upscli_connect(&ups, hostname, port, 0) < 0) {
-		fprintf(stderr, "Error: %s\n", upscli_strerror(&ups));
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
-	}
-
-	if (!upsname) {
-		fprintf(stderr, "Error: a UPS name must be specified (upsname[@hostname[:port]])\n");
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
-	}
-
-	llast = lhead = NULL;
+	const char	*query[4];
+	char		**answer;
+	struct list_t	*lhead = NULL, *llast = NULL, *ltmp, *lnext;
 
 	query[0] = "CMD";
 	query[1] = upsname;
 	numq = 2;
 
-	ret = upscli_list_start(&ups, numq, query);
+	ret = upscli_list_start(ups, numq, query);
 
 	if (ret < 0) {
 
 		/* old upsd = no way to continue */
-		if (upscli_upserror(&ups) == UPSCLI_ERR_UNKCOMMAND) {
-			fprintf(stderr, "Error: upsd is too old to support this query\n");
-			clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
+		if (upscli_upserror(ups) == UPSCLI_ERR_UNKCOMMAND) {
+			fatalx(EXIT_FAILURE, "Error: upsd is too old to support this query");
 		}
 
-		fprintf(stderr, "Error: %s\n", upscli_strerror(&ups));
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
+		fatalx(EXIT_FAILURE, "Error: %s", upscli_strerror(ups));
 	}
 
-	printf("Instant commands supported on UPS [%s]:\n\n", rawname);
-
-	ret = upscli_list_next(&ups, numq, query, &numa, &answer);
-
-	while (ret == 1) {
+	while (upscli_list_next(ups, numq, query, &numa, &answer) == 1) {
 
 		/* CMD <upsname> <cmdname> */
-
 		if (numa < 3) {
-			fprintf(stderr, "Error: insufficient data "
-				"(got %d args, need at least 3)\n", numa);
-			clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
+			fatalx(EXIT_FAILURE, "Error: insufficient data (got %d args, need at least 3)", numa);
 		}
 
-		/* sock this entry away for later */
+		/* we must first read the entire list of commands,
+		   before we can start reading the descriptions */
 
-		ltmp = xmalloc(sizeof(struct list_t));
+		ltmp = xcalloc(1, sizeof(*ltmp));
 		ltmp->name = xstrdup(answer[2]);
-		ltmp->next = NULL;
 
-		if (llast)
+		if (llast) {
 			llast->next = ltmp;
-		else
+		} else {
 			lhead = ltmp;
+		}
 
 		llast = ltmp;
-
-		ret = upscli_list_next(&ups, numq, query, &numa, &answer);
 	}
 
 	/* walk the list and try to get descriptions, freeing as we go */
+	printf("Instant commands supported on UPS [%s]:\n\n", upsname);
 
-	ltmp = lhead;
-
-	while (ltmp) {
+	for (ltmp = lhead; ltmp; ltmp = lnext) {
 		lnext = ltmp->next;
 
-		print_cmd(&ups, upsname, ltmp->name);
+		print_cmd(ltmp->name);
 
 		free(ltmp->name);
 		free(ltmp);
-		ltmp = lnext;
 	}
-
-	clean_exit(&ups, upsname, hostname, EXIT_SUCCESS);
 }
 
-static int do_cmd(UPSCONN_t *ups, const char *upsname, const char **argv, const int argc)
+static void do_cmd(char **argv, const int argc)
 {
-	char 	buf[SMALLBUF];
+	char	buf[SMALLBUF];
 
 	if (argc > 1) {
 		snprintf(buf, sizeof(buf), "INSTCMD %s %s %s\n", upsname, argv[0], argv[1]);
@@ -190,103 +146,120 @@ static int do_cmd(UPSCONN_t *ups, const char *upsname, const char **argv, const 
 	}
 
 	if (upscli_sendline(ups, buf, strlen(buf)) < 0) {
-		fprintf(stderr, "Can't send instant command: %s\n",
-			upscli_strerror(ups));
-
-		return EXIT_FAILURE;
+		fatalx(EXIT_FAILURE, "Can't send instant command: %s", upscli_strerror(ups));
 	}
 
 	if (upscli_readline(ups, buf, sizeof(buf)) < 0) {
-		fprintf(stderr, "Instant command failed: %s\n", 
-			upscli_strerror(ups));
+		fatalx(EXIT_FAILURE, "Instant command failed: %s", upscli_strerror(ups));
+	}
+}
 
-		return EXIT_FAILURE;
+static void clean_exit(void)
+{
+	if (ups) {
+		upscli_sendline(ups, "LOGOUT\n", 7);
+		upscli_disconnect(ups);
 	}
 
-	return EXIT_SUCCESS;
+	free(upsname);
+	free(hostname);
+	free(ups);
 }
 
 int main(int argc, char **argv)
 {
-	int	i, ret, have_un, have_pw;
-	char 	*prog, buf[SMALLBUF], username[SMALLBUF], password[SMALLBUF],
-		*pwtmp;
+	int	i, ret, port;
+	int	have_un = 0, have_pw = 0, cmdlist = 0;
+	char	buf[SMALLBUF], username[SMALLBUF], password[SMALLBUF];
+	const char	*prog = xbasename(argv[0]);
 
-	int	port;
-	char	*upsname, *hostname;
-	UPSCONN_t	ups;
+	while ((i = getopt(argc, argv, "+lhu:p:V")) != -1) {
 
-	prog = argv[0];
+		switch (i)
+		{
+		case 'l':
+			cmdlist = 1;
+			break;
 
-	have_un = have_pw = 0;
+		case 'u':
+			snprintf(username, sizeof(username), "%s", optarg);
+			have_un = 1;
+			break;
 
-	while ((i = getopt(argc, argv, "+l:hu:p:V")) != -1) {
-		switch (i) {
-			case 'l':
-				listcmds(optarg);
-				break;
+		case 'p':
+			snprintf(password, sizeof(password), "%s", optarg);
+			have_pw = 1;
+			break;
 
-			case 'u':
-				snprintf(username, sizeof(username), "%s", 
-					optarg);
-				have_un = 1;
-				break;
+		case 'V':
+			fatalx(EXIT_SUCCESS, "Network UPS Tools upscmd %s", UPS_VERSION);
 
-			case 'p':
-				snprintf(password, sizeof(password), "%s", 
-					optarg);
-				have_pw = 1;
-				break;
-
-			case 'V':
-				printf("Network UPS Tools upscmd %s\n",
-					UPS_VERSION);
-				exit(EXIT_SUCCESS);
-
-			case 'h':
-			default:
-				usage(prog);
-				break;
+		case 'h':
+		default:
+			usage(prog);
+			exit(EXIT_SUCCESS);
 		}
 	}
 
 	argc -= optind;
 	argv += optind;
 
-	if (argc < 2)
+	if (argc < 1) {
 		usage(prog);
-
-	upsname = hostname = NULL;
-
-	if (upscli_splitname(argv[0], &upsname, &hostname, &port) != 0) {
-		fprintf(stderr, "Error: invalid UPS definition.  Required format: upsname[@hostname[:port]]\n");
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
+		exit(EXIT_SUCCESS);
 	}
 
-	if (upscli_connect(&ups, hostname, port, 0) < 0) {
-		fprintf(stderr, "Error: %s\n", upscli_strerror(&ups));
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
+	/* be a good little client that cleans up after itself */
+	atexit(clean_exit);
+
+	if (upscli_splitname(argv[0], &upsname, &hostname, &port) != 0) {
+		fatalx(EXIT_FAILURE, "Error: invalid UPS definition.  Required format: upsname[@hostname[:port]]");
+	}
+
+	ups = xcalloc(1, sizeof(*ups));
+
+	if (upscli_connect(ups, hostname, port, 0) < 0) {
+		fatalx(EXIT_FAILURE, "Error: %s", upscli_strerror(ups));
+	}
+
+	if (cmdlist) {
+		listcmds();
+		exit(EXIT_SUCCESS);
+	}
+
+	if (argc < 2) {
+		usage(prog);
+		exit(EXIT_SUCCESS);
+	}
+
+	/* also fallback for old command names */
+	if (!strchr(argv[1], '.')) {
+		fatalx(EXIT_FAILURE, "Error: old command names are not supported");
 	}
 
 	if (!have_un) {
-		struct	passwd	*pw;
+		struct passwd	*pw;
 
 		memset(username, '\0', sizeof(username));
 		pw = getpwuid(getuid());
 
-		if (pw)
+		if (pw) {
 			printf("Username (%s): ", pw->pw_name);
-		else
+		} else {
 			printf("Username: ");
+		}
 
-		fgets(username, SMALLBUF, stdin);
+		if (!fgets(username, sizeof(username), stdin)) {
+			fatalx(EXIT_FAILURE, "Error reading from stdin!");
+		}
 
 		/* deal with that pesky newline */
-		if (strlen(username) > 1)
+		if (strlen(username) > 1) {
 			username[strlen(username) - 1] = '\0';
-		else {
-			if (!pw)
+		} else {
+			if (!pw) {
 				fatalx(EXIT_FAILURE, "No username available - even tried getpwuid");
+			}
 
 			snprintf(username, sizeof(username), "%s", pw->pw_name);
 		}
@@ -294,72 +267,46 @@ int main(int argc, char **argv)
 
 	/* getpass leaks slightly - use -p when testing in valgrind */
 	if (!have_pw) {
-		pwtmp = GETPASS("Password: ");
+		/* using getpass or getpass_r might not be a
+		   good idea here (marked obsolete in POSIX) */
+		char	*pwtmp = GETPASS("Password: ");
 
 		if (!pwtmp) {
-			fprintf(stderr, "getpass failed: %s\n", strerror(errno));
-			clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
+			fatalx(EXIT_FAILURE, "getpass failed: %s", strerror(errno));
 		}
-			
+
 		snprintf(password, sizeof(password), "%s", pwtmp);
 	}
 
 	snprintf(buf, sizeof(buf), "USERNAME %s\n", username);
 
-	if (upscli_sendline(&ups, buf, strlen(buf)) < 0) {
-		fprintf(stderr, "Can't set username: %s\n", 
-			upscli_strerror(&ups));
-
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
+	if (upscli_sendline(ups, buf, strlen(buf)) < 0) {
+		fatalx(EXIT_FAILURE, "Can't set username: %s", upscli_strerror(ups));
 	}
 
-	ret = upscli_readline(&ups, buf, sizeof(buf));
+	ret = upscli_readline(ups, buf, sizeof(buf));
 
 	if (ret < 0) {
-		if (upscli_upserror(&ups) == UPSCLI_ERR_UNKCOMMAND) {
-			fprintf(stderr, "Set username failed due to an "
-				"unknown command.\n");
-
-			fprintf(stderr, "You probably need to upgrade upsd.\n");
-			clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
+		if (upscli_upserror(ups) != UPSCLI_ERR_UNKCOMMAND) {
+			fatalx(EXIT_FAILURE, "Set username failed: %s", upscli_strerror(ups));
 		}
 
-		fprintf(stderr, "Set username failed: %s\n", 
-			upscli_strerror(&ups));
-
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
+		fatalx(EXIT_FAILURE,
+			"Set username failed due to an unknown command.\n"
+			"You probably need to upgrade upsd.");
 	}
 
 	snprintf(buf, sizeof(buf), "PASSWORD %s\n", password);
 
-	if (upscli_sendline(&ups, buf, strlen(buf)) < 0) {
-		fprintf(stderr, "Can't set password: %s\n", 
-			upscli_strerror(&ups));
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
+	if (upscli_sendline(ups, buf, strlen(buf)) < 0) {
+		fatalx(EXIT_FAILURE, "Can't set password: %s", upscli_strerror(ups));
 	}
 
-	if (upscli_readline(&ups, buf, sizeof(buf)) < 0) {
-		fprintf(stderr, "Set password failed: %s\n", 
-			upscli_strerror(&ups));
-
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
+	if (upscli_readline(ups, buf, sizeof(buf)) < 0) {
+		fatalx(EXIT_FAILURE, "Set password failed: %s", upscli_strerror(ups));
 	}
 
-	/* no upsname means die here */
-	if (!upsname) {
-		fprintf(stderr, "Error: a UPS name must be specified (upsname[@hostname[:port]])\n");
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
-	}
+	do_cmd(&argv[1], argc - 1);
 
-	/* also fallback for old command names */
-	if (!strchr(argv[1], '.')) {
-		fprintf(stderr, "Error: old command names are not supported\n");
-		clean_exit(&ups, upsname, hostname, EXIT_FAILURE);
-	}
-
-	ret = do_cmd(&ups, upsname, &argv[1], argc - 1);
-	clean_exit(&ups, upsname, hostname, ret);
-
-	/* NOTREACHED */
-	exit(EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
 }
