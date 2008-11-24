f0255e0eec841bcfd49d67c6d64b19b2ce72c6f2
Arjen de Korte <adkorte-guest@alioth.debian.org>
2008-11-24 16:11:52 +0000

    We should only match SUPPORTED devices.
    Fossil-ID: SVN r1579

--- drivers/megatec_usb.c
+++ drivers/megatec_usb.c
@@ -133,7 +133,16 @@ static int subdriver_match_func(USBDevice_t *d, void *privdata)
 	if(getval("subdriver"))
 		return 1;
 
-	return is_usb_device_supported(megatec_usb_id, d->VendorID, d->ProductID);
+	switch (is_usb_device_supported(blazer_usb_id, d->VendorID, d->ProductID))
+	{
+	case SUPPORTED:
+		return 1;
+
+	case POSSIBLY_SUPPORTED:
+	case NOT_SUPPORTED:
+	default:
+		return 0;
+	}
 }
 
 static USBDeviceMatcher_t subdriver_matcher = {
