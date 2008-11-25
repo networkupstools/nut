a0f571bc9585b685228de1f38da4004981f805b5
Charles Lepple <clepple+nut@gmail.com>
2008-11-25 13:43:21 +0000

    snmp-ups: Use OID ending in .1 for APC temperature and humidity probes.
    (Patch courtesy of Seffen Plotnerr; confirmed by George Ross.)
    
    Discussed here:
    http://thread.gmane.org/gmane.comp.monitoring.nut.devel/3264/focus=3268
    
    Fossil-ID: SVN r1581

--- drivers/apccmib.h
+++ drivers/apccmib.h
@@ -208,10 +208,10 @@ snmp_info_t apcc_mib[] = {
 
 	/* IEM ambient variables */
 /* IEM: integrated environment monitor probe */
-#define APCC_OID_IEM_TEMP       ".1.3.6.1.4.1.318.1.1.10.2.3.2.1.4.0"
-#define APCC_OID_IEM_TEMP_UNIT  ".1.3.6.1.4.1.318.1.1.10.2.3.2.1.5.0"
+#define APCC_OID_IEM_TEMP       ".1.3.6.1.4.1.318.1.1.10.2.3.2.1.4.1"
+#define APCC_OID_IEM_TEMP_UNIT  ".1.3.6.1.4.1.318.1.1.10.2.3.2.1.5.1"
 #define APCC_IEM_FAHRENHEIT	    2
-#define APCC_OID_IEM_HUMID      ".1.3.6.1.4.1.318.1.1.10.2.3.2.1.6.0"
+#define APCC_OID_IEM_HUMID      ".1.3.6.1.4.1.318.1.1.10.2.3.2.1.6.1"
 	{ "ambient.temperature", 0, 1, APCC_OID_IEM_TEMP, "", SU_FLAG_OK, NULL },
 	{ "ambient.humidity", 0, 1, APCC_OID_IEM_HUMID, "", SU_FLAG_OK, NULL },
 
