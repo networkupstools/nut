06a056b914705b033004abf765e325189512bc68
Frederic Bohe <fbohe-guest@alioth.debian.org>
2010-10-20 07:26:44 +0000

    Fix errors appeared when merging from regular 2.4.3 to svn trunk
    Fossil-ID: SVN r2586

--- server/upsd.c
+++ server/upsd.c
@@ -197,8 +197,14 @@ static void setuptcp(stype_t *server)
 	atexit(WSACleanup);
 #endif
 
-#ifndef WIN32
+#ifndef	HAVE_IPV6
+	struct hostent		*host;
+	struct sockaddr_in	sockin;
+	int	res, one = 1;
+
+	memset(&sockin, '\0', sizeof(sockin));
 	host = gethostbyname(server->addr);
+#ifndef WIN32
 
 	if (!host) {
 		struct  in_addr	listenaddr;
@@ -232,7 +238,6 @@ static void setuptcp(stype_t *server)
 		fatal_with_errno(EXIT_FAILURE, "setsockopt(SO_REUSEADDR)");
 	}
 
-	memset(&sockin, '\0', sizeof(sockin));
 	sockin.sin_family = AF_INET;
 	sockin.sin_port = htons(atoi(server->port));
 
@@ -779,6 +784,7 @@ static void mainloop(void)
 	int	i, ret, nfds = 0;
 #else
 	DWORD	ret;
+	int 	nfds = 0;
 #endif
 
 	upstype_t	*ups;
