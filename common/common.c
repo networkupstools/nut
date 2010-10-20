05e2a178f151c5a159494660304be8529404fcdf
Frederic Bohe <fbohe-guest@alioth.debian.org>
2010-10-20 09:13:38 +0000

    Remove hack from confpath function.
    You have to declare a NUT_CONFPATH environnement variable in your
    Windows settings
    
    Fossil-ID: SVN r2588

--- common/common.c
+++ common/common.c
@@ -565,12 +565,7 @@ const char * confpath(void)
 	const char * path;
 
 	if ((path = getenv("NUT_CONFPATH")) == NULL)
-#ifndef WIN32
 		path = CONFPATH;
-#else
-/*FIXME*/
-		path = "./conf";
-#endif
 
 	return path;
 }
