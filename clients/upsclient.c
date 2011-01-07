8c2b9923d3b43bbbaf9bbe1da64d40b94eced74f
Frederic Bohe <fbohe-guest@alioth.debian.org>
2011-01-07 14:02:19 +0000

    Fix client disconnection
    Fossil-ID: SVN r2808

--- clients/upsclient.c
+++ clients/upsclient.c
@@ -412,7 +412,6 @@ int upscli_connect(UPSCONN_t *ups, const char *host, int port, int flags)
 #ifdef WIN32
 	WSADATA WSAdata;
 	WSAStartup(2,&WSAdata);
-	atexit(WSACleanup);
 #endif
 	if (!ups) {
 		return -1;
@@ -1070,6 +1069,9 @@ int upscli_disconnect(UPSCONN_t *ups)
 	ups->host = NULL;
 
 	if (ups->fd < 0) {
+#ifdef WIN32
+		WSACleanup();
+#endif
 		return 0;
 	}
 
@@ -1092,6 +1094,10 @@ int upscli_disconnect(UPSCONN_t *ups)
 
 	close(ups->fd);
 	ups->fd = -1;
+
+#ifdef WIN32
+	WSACleanup();
+#endif
 
 	return 0;
 }
