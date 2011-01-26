a90851e6a3c05fadc7d8fe8269876ec8990e7c81
Frederic Bohe <fbohe-guest@alioth.debian.org>
2011-01-26 15:05:16 +0000

    upsmon does not fork in WIN32
    Fossil-ID: SVN r2852

--- clients/upsmon.c
+++ clients/upsmon.c
@@ -83,7 +83,13 @@ static	char	*certpasswd = NULL;
 static	int	certverify = 0;		/* don't verify by default */
 static	int	forcessl = 0;		/* don't require ssl by default */
 
-static	int	userfsd = 0, use_pipe = 1, pipefd[2];
+static	int	userfsd = 0, pipefd[2];
+#ifndef WIN32
+static	int	use_pipe = 1;
+#else
+	/* Do not fork in WIN32 */
+static	int	use_pipe = 0;
+#endif
 
 static	utype_t	*firstups = NULL;
 
@@ -2369,7 +2375,10 @@ int main(int argc, char *argv[])
 
 		become_user(new_uid);
 	} else {
+#ifndef WIN32
+		/* Note: upsmon does not fork in WIN32 */
 		upslogx(LOG_INFO, "Warning: running as one big root process by request (upsmon -p)");
+#endif
 
 		writepid(prog);
 	}
