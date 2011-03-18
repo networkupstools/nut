e6414e5fe4b0c074fa1e81be91f17fa043ec5808
Frederic Bohe <fbohe-guest@alioth.debian.org>
2011-03-18 14:09:12 +0000

    Remove DLL from the SVN repository.
    They are now gathered from the build system when creating the installer .
    
    Fossil-ID: SVN r2946

--- scripts/Windows/Installer/BuildInstaller.bat
+++ scripts/Windows/Installer/BuildInstaller.bat
@@ -9,6 +9,12 @@ SET NUT-XML-FILE=NUT-Installer.xml
 SET wixobjName=NUT-Installer.wixobj
 SET msiPackageName=NUT-Installer.msi
 
+REM copy DLL files from system
+copy /Y c:\mingw\msys\1.0\bin\msys-1.0.dll .\ImageFiles\Others
+copy /Y c:\mingw\msys\1.0\bin\msys-crypto-1.0.0.dll .\ImageFiles\Others
+copy /Y c:\mingw\msys\1.0\bin\msys-ssl-1.0.0.dll .\ImageFiles\Others
+copy /Y c:\mingw\bin\libgnurx-0.dll .\ImageFiles\Others
+
 REM use "candle.exe" to create the "object" file
 candle.exe "%NUT-XML-FILE%" -out "%wixobjName%" >"log.txt"
 @echo =========================================================
