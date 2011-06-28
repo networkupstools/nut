f38f866d01ee93c06b82e6a30dee22a134fc0ec6
Michal Soltys <msoltyspl-guest@alioth.debian.org>
2011-06-28 12:57:18 +0000

    apcsmart: remove some of the old commented out sections
    Fossil-ID: SVN r3092

--- drivers/apcsmart.c
+++ drivers/apcsmart.c
@@ -163,12 +163,6 @@ static int apc_ser_set(void)
 	struct termios tio;
 	char *cable;
 
-#if 0
-	if (upsfd == -1) {
-		upslog_with_errno(LOG_EMERG, "apc_ser_set: programming error ! port (%s) should be opened !", device_path);
-		return 0;
-	}
-#endif
 	if (upsfd == -1)
 		return 0;
 
@@ -235,10 +229,6 @@ static int apc_ser_try(void)
 {
 	int fd, ret;
 
-#if 0
-	if (upsfd >= 0)
-		return 1;
-#endif
 	if (upsfd >= 0) {
 		upslog_with_errno(LOG_EMERG, "apc_ser_try: programming error ! port (%s) should be closed !", device_path);
 		return 0;
@@ -280,12 +270,6 @@ static int apc_ser_tear(void)
 {
 	int ret;
 
-#if 0
-	if (upsfd == -1) {
-		upslog_with_errno(LOG_EMERG, "apc_ser_tear: programming error ! port (%s) should be opened !", device_path);
-		return 0;
-	}
-#endif
 	if (upsfd == -1)
 		return 1;
 
@@ -2093,79 +2077,3 @@ void upsdrv_updateinfo(void)
 
 	update_info_normal();
 }
-
-#if 0
-
-/* old version for reference, if we wanted to get back to it */
-/*
- * blindly check if variable is actually supported, update vartab accordingly,
- * also get the value; could use some simplifying ...
- */
-static int query_ups(const char *var, int first)
-{
-	int ret, i, j;
-	char temp[APC_LBUF];
-	const char *ptr;
-	apc_vartab_t *vt, *vtn;
-
-	/*
-	 * at first run we know nothing about variable; we have to handle
-	 * APC_MULTI gracefully as well
-	 */
-	for (i = 0; apc_vartab[i].name != NULL; i++) {
-		vt = &apc_vartab[i];
-		if (strcmp(vt->name, var) || vt->flags & APC_DEPR)
-			continue;
-		/*
-		 * ok, found in table & not deprecated - if not the first run
-		 * and not present, bail out
-		 */
-		if (!first && !(vt->flags & APC_PRESENT))
-			break;
-
-		/* found, [try to] get it */
-
-		/* empty the input buffer (while allowing the alert handler to run) */
-		apc_flush(SER_AA);
-		ret = apc_write(vt->cmd);
-		if (ret != 1) {
-			upslog_with_errno(LOG_ERR, "query_ups: apc_write failed");
-			break;
-		}
-		ret = apc_read(temp, sizeof(temp), SER_AA | (first ? SER_TO : 0));
-
-		if (ret < 1 || !strcmp(temp, "NA")) {
-			if (first) {
-				if (vt->flags & APC_MULTI) {
-					vt->flags |= APC_DEPR;
-					continue;
-				}
-				upsdebugx(1, "query_ups: unknown variable %s", var);
-				break;
-			}
-			/* automagically no longer supported by the hardware somehow */
-			if (ret > 0)
-				remove_var("query_ups", vt);
-			break;
-		}
-
-		vt->flags |= APC_PRESENT;
-		ptr = convert_data(vt, temp);
-		dstate_setinfo(vt->name, "%s", ptr);
-
-		/* supported, deprecate all the remaining ones */
-		if (first && vt->flags & APC_MULTI)
-			for (j = i + 1; apc_vartab[j].name != NULL; j++) {
-				vtn = &apc_vartab[j];
-				if (strcmp(vtn->name, vt->name))
-					continue;
-				vtn->flags |= APC_DEPR;
-				vtn->flags &= ~APC_PRESENT;
-			}
-
-		return 1; /* success */
-	}
-
-	return 0;
-}
-#endif
