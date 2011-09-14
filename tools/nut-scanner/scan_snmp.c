e3055b2f7aff60ffae98c32ab47c3af453b866b9
Charles Lepple <clepple+nut@gmail.com>
2011-09-14 12:50:20 +0000

    nut-scanner: use "common.h", which includes "config.h"
    Fossil-ID: SVN r3227

--- tools/nut-scanner/scan_snmp.c
+++ tools/nut-scanner/scan_snmp.c
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-#include "config.h"
+#include "common.h"
 
 #ifdef HAVE_NET_SNMP_NET_SNMP_CONFIG_H
 
@@ -58,7 +58,6 @@
 #include <pthread.h>
 #endif
 #include "nutscan-snmp.h"
-#include "common.h"
 
 
 #define SysOID ".1.3.6.1.2.1.1.2.0"
