ca550a10bd2474703c6b4daffe0002c4bc821fbb
Frederic Bohe <fbohe-guest@alioth.debian.org>
2012-01-16 09:38:20 +0000

    Fix porting of upssched
    Fossil-ID: SVN r3397

--- clients/upssched.h
+++ clients/upssched.h
@@ -8,7 +8,13 @@
 
 /* track client connections */
 typedef struct conn_s {
+#ifndef WIN32
 	int     fd;
+#else
+	HANDLE     fd;
+	char       buf[LARGEBUF];
+	OVERLAPPED read_overlapped;
+#endif
 	PCONF_CTX_t	ctx;
 	struct conn_s	*next;
 } conn_t;
