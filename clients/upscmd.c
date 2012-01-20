0d4a0d77dad1b038d4afed26236c3e2577dcb966
Frederic Bohe <fbohe-guest@alioth.debian.org>
2012-01-20 12:55:40 +0000

    Add getpwuid and getpass functions.
    Fossil-ID: SVN r3405

--- clients/upscmd.c
+++ clients/upscmd.c
@@ -26,6 +26,8 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#else
+#include "wincompat.h"
 #endif
 
 #include "upsclient.h"
@@ -245,7 +247,6 @@ int main(int argc, char **argv)
 	if (!strchr(argv[1], '.')) {
 		fatalx(EXIT_FAILURE, "Error: old command names are not supported");
 	}
-#ifndef WIN32
 	if (!have_un) {
 		struct passwd	*pw;
 
@@ -286,7 +287,6 @@ int main(int argc, char **argv)
 
 		snprintf(password, sizeof(password), "%s", pwtmp);
 	}
-#endif
 	snprintf(buf, sizeof(buf), "USERNAME %s\n", username);
 
 	if (upscli_sendline(ups, buf, strlen(buf)) < 0) {
