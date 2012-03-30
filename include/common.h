07e4c6e25515347aeee8b3efa69d0db3f3a426e5
Arnaud Quette <arnaud.quette@free.fr>
2012-03-30 10:57:54 +0000

    Improve support functions for trimming strings
    Enforce verifications in rtrim, and add an ltrim function
    
    Fossil-ID: SVN r3512

--- include/common.h
+++ include/common.h
@@ -112,6 +112,7 @@ void *xrealloc(void *ptr, size_t size);
 char *xstrdup(const char *string);
 
 char *rtrim(char *in, const char sep);
+char* ltrim(char *in, const char sep);
 
 int select_read(const int fd, void *buf, const size_t buflen, const long d_sec, const long d_usec);
 int select_write(const int fd, const void *buf, const size_t buflen, const long d_sec, const long d_usec);
