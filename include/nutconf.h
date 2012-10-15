f2ddbf50e7ca54526912fa6c2f08c1a9444b04c7
Émilien Kia <emilien.kia@gmail.com>
2012-10-15 23:30:12 +0200

    Implement abstract configuration parser.
    Rename NutConfParser in NutParser.
    Implement the abstract class NutConfParser based on NutParser for configuration file parsing.
    Remove old C sample code.

--- include/nutconf.h
+++ include/nutconf.h
@@ -37,11 +37,11 @@ namespace nut
 /**
  * NUT config parser.
  */
-class NutConfigParser
+class NutParser
 {
 public:
-  NutConfigParser(const char* buffer = NULL);
-  NutConfigParser(const std::string& buffer);
+  NutParser(const char* buffer = NULL);
+  NutParser(const std::string& buffer);
 
   struct Token
   {
@@ -64,7 +64,11 @@ public:
       Token(TokenType type, char c):type(type),str(1, c){}
       Token(const Token& tok):type(tok.type),str(tok.str){}
 
+      bool is(TokenType type)const{return this->type==type;}
+
       bool operator==(const Token& tok)const{return tok.type==type && tok.str==str;}
+
+      operator bool()const{return type!=TOKEN_UNKNOWN && type!=TOKEN_NONE;}
   };
 
   /** Parsing functions
@@ -98,182 +102,24 @@ private:
 };
 
 
+class NutConfigParser : public NutParser
+{
+public:
+    virtual void parseConfig();
+
+protected:
+    NutConfigParser(const char* buffer = NULL);
+    NutConfigParser(const std::string& buffer);
+
+    virtual void onParseBegin()=0;
+    virtual void onParseComment(const std::string& comment)=0;
+    virtual void onParseSectionName(const std::string& sectionName, const std::string& comment = "")=0;
+    virtual void onParseDirective(const std::string& directiveName, char sep = 0, const std::list<std::string>& values = std::list<std::string>(), const std::string& comment = "")=0;
+    virtual void onParseEnd()=0;
+};
+
+
+
 } /* namespace nut */
 #endif /* __cplusplus */
-
-
-#if 0
-
-#ifdef __cplusplus
-/* *INDENT-OFF* */
-extern "C" {
-/* *INDENT-ON* */
-#endif
-
-
-/** Token type enumeration.*/
-typedef enum{
-	TOKEN_UNKNOWN = -1,
-	TOKEN_NONE    = 0,
-	TOKEN_STRING  = 1,
-	TOKEN_QUOTED_STRING,
-	TOKEN_COMMENT,
-	TOKEN_BRACKET_OPEN,
-	TOKEN_BRACKET_CLOSE,
-	TOKEN_EQUAL,
-	TOKEN_COLON,
-	TOKEN_EOL
-}PARSING_TOKEN_e;
-
-
-
-/** Lexical token pointer.
- * It is a fragment of a c-string represented by its first character (begin) and
- * the byte after its last character.
- * Empty token is represented by end==begin.
- * Null token is represented by begin==end==NULL.
- */
-typedef struct {
-    const char* begin;
-    const char* end;
-    PARSING_TOKEN_e type;
-}LEXTOKEN_t;
-
-/** Helper to copy a token to another.*/
-inline void LEXTOKEN_copy(LEXTOKEN_t* tgt, LEXTOKEN_t* src)
-{
-    tgt->begin = src->begin;
-    tgt->end   = src->end;
-    tgt->type  = src->type;
-}
-
-/** Helper to set token values.*/
-inline void LEXTOKEN_set(LEXTOKEN_t* tok, const char* begin, const char* end,
-    PARSING_TOKEN_e type)
-{
-    tok->begin = begin;
-    tok->end   = end;
-    tok->type  = type;
-}
-
-/** Helper to copy a string in a dedicated allocated memory.*/
-inline char* LEXTOKEN_chralloc(LEXTOKEN_t* tok)
-{
-	if(tok->begin && tok->begin<tok->end)
-	{
-		size_t len = tok->end-tok->begin;
-		char* mem = (char*)malloc(len+1);
-		memcpy(mem, tok->begin, len);
-		mem[len] = 0;
-		return mem;
-	}
-	else
-	{
-		return 0;
-	}
-}
-
-
-/** Syntaxical parsing machine state enumeration.*/
-typedef enum {
-	SYNPARSING_STATE_INIT,
-	SYNPARSING_STATE_FINISHED,
-	SYNPARSING_STATE_SECTION_BEGIN,
-	SYNPARSING_STATE_SECTION_NAME,
-	SYNPARSING_STATE_SECTION_END,
-	SYNPARSING_STATE_DIRECTIVE_BEGIN,
-	SYNPARSING_STATE_DIRECTIVE_ARGUMENT
-}SYNPARSING_STATE_e;
-
-/** Line type.*/
-typedef enum {
-    SYNPARSING_LINETYPE_UNKNWON = -1,
-    SYNPARSING_LINETYPE_COMMENT,
-    SYNPARSING_LINETYPE_SECTION,
-    SYNPARSING_LINETYPE_DIRECTIVE_COLON,
-    SYNPARSING_LINETYPE_DIRECTIVE_EQUAL,
-    SYNPARSING_LINETYPE_DIRECTIVE_NOSEP
-}SYNPARSING_LINETYPE_e;
-
-
-/** Syntaxic line parsing result structure. */
-typedef struct {
-	SYNPARSING_LINETYPE_e line_type;
-	PARSING_TOKEN_e       directive_separator;
-	LEXTOKEN_t*           args; /* Array of LEXTOKEN_t. */
-	unsigned int        nb_args; /* Size of array args. */
-	unsigned int        arg_count; /* Number of arg in array args. */
-	LEXTOKEN_t            comment;
-}SYNLINE_t;
-
-
-
-int nutconf_parse_rule_CHARS(const char* src, unsigned int len);
-
-int nutconf_parse_rule_STRCHARS(const char* src, unsigned int len);
-
-
-PARSING_TOKEN_e nutconf_parse_token(const char* src, unsigned int len,
-	LEXTOKEN_t* token);
-
-void nutconf_parse_line(const char* src, unsigned int len,
-	const char** rend, SYNLINE_t* parsed_line);
-
-
-typedef void (*nutconf_parse_line_callback)(SYNLINE_t* line, void* user_data);
-
-void nutconf_parse_memory(const char* src, int len,
-	nutconf_parse_line_callback cb, void* user_data);
-
-
-
-
-
-/** Generic chaining node for values. */
-typedef struct NUTCONF_ARGVALUE_s{
-	char* value;
-	struct NUTCONF_ARGVALUE_s* next;
-}NUTCONF_ARGVALUE_t;
-
-/** Type of argument.
- * TODO Change it to a more semantic naming scheme
- */
-typedef enum {
-	NUTCONF_ARG_NONE,
-	NUTCONF_ARG_COLON,
-	NUTCONF_ARG_EQUAL
-}NUTCONF_ARG_TYPE_e;
-
-/** Generic chaining node for names. */
-typedef struct NUTCONF_ARG_s{
-	char* name;
-	NUTCONF_ARG_TYPE_e type;
-	NUTCONF_ARGVALUE_t* value;
-	struct NUTCONF_ARG_s* next;
-}NUTCONF_ARG_t;
-
-
-/** Generic nutconf storage structure.
- * Bloc of values for one section of data.
- * Have a pointer to an eventually following bloc.
- */
-typedef struct NUTCONF_SECTION_s{
-	char* name;
-	NUTCONF_ARG_t* args;
-	struct NUTCONF_SECTION_s* next;
-}NUTCONF_SECTION_t, NUTCONF_t;
-
-
-/** Generic configuration parsing. */
-NUTCONF_t* nutconf_conf_parse(const char* src, int len);
-
-
-#ifdef __cplusplus
-/* *INDENT-OFF* */
-}
-/* *INDENT-ON* */
-#endif
-
-#endif /* 0 */
-
 #endif	/* NUTCONF_H_SEEN */
