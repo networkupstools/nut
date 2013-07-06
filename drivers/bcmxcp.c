3d7d62af1d6d6dacb2f43d02bcbc7a6170279e77
Alf Høgemark <alf@i100.no>
2013-07-06 09:46:35 +0200

    bcpmxp: Add mapping for input.power to meters
    input.power is not listed in http://www.networkupstools.org/docs/developer-guide.chunked/apas01.html,
    but other drivers use it.
    Not tested on hardware, due to lack of hardware supporting it

--- drivers/bcmxcp.c
+++ drivers/bcmxcp.c
@@ -331,6 +331,7 @@ void init_meter_map()
 	bcmxcp_meter_map[BCMXCP_METER_MAP_INPUT_CURRENT_PHASE_B].nut_entity = "input.L2.current";
 	bcmxcp_meter_map[BCMXCP_METER_MAP_INPUT_CURRENT_PHASE_C].nut_entity = "input.L3.current";
 	bcmxcp_meter_map[BCMXCP_METER_MAP_OUTPUT_VA].nut_entity = "ups.power";
+	bcmxcp_meter_map[BCMXCP_METER_MAP_INPUT_VA].nut_entity = "input.power";
 	bcmxcp_meter_map[BCMXCP_METER_MAP_OUTPUT_POWER_FACTOR].nut_entity = "output.powerfactor";
 	bcmxcp_meter_map[BCMXCP_METER_MAP_INPUT_POWER_FACTOR].nut_entity = "input.powerfactor";
 
