acfa1d071ba5ef9f58c75607b26b8db29ec48fbd
Frédéric BOHE <fredericbohe@eaton.com>
2013-07-23 14:50:03 +0200

    Fix connect in multi-threaded environnement on AIX

--- clients/upsclient.c
+++ clients/upsclient.c
@@ -19,6 +19,9 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
+/* this include is needed on AIX to have errno stored in thread local storage */
+#include <pthread.h>
+
 #include "config.h"	/* safe because it doesn't contain prototypes */
 
 #include <errno.h>
