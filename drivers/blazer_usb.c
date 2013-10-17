65668f6ecfcb2428a992ba2ef94d7ee8d3d20e77
Daniele Pezzini <hyouko@gmail.com>
2013-10-17 13:57:05 +0200

    blazer: Versioning

--- drivers/blazer_usb.c
+++ drivers/blazer_usb.c
@@ -28,7 +28,7 @@
 #include "blazer.h"
 
 #define DRIVER_NAME	"Megatec/Q1 protocol USB driver"
-#define DRIVER_VERSION	"0.09"
+#define DRIVER_VERSION	"0.10"
 
 /* driver description structure */
 upsdrv_info_t upsdrv_info = {
