4724e156a88af30a6c0984617e1e8740041168dc
gavrilov-i <i.o.gavrilov@gmail.com>
2014-10-10 19:30:56 +0300

    Update bcmxcp.c

--- drivers/bcmxcp.c
+++ drivers/bcmxcp.c
@@ -1614,11 +1614,14 @@ void upsdrv_updateinfo(void)
 				status_set("OVER");
 				break;
 			case BCMXCP_STATUS_TRIM: /* Trim */
+				upslogx(LOG_NOTICE, "Entering Trim state");
 				status_set("OL");
 				status_set("TRIM");
 				break;
 			case BCMXCP_STATUS_BOOST1:
+				upslogx(LOG_NOTICE, "Entering Boost state");
 			case BCMXCP_STATUS_BOOST2: /* Boost */
+				upslogx(LOG_NOTICE, "Entering Double Boost state");
 				status_set("OL");
 				status_set("BOOST");
 				break;
