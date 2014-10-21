bb95ef4be14988053cad603c4be1205a7457ecaa
gavrilov-i <gavrilov-i@users.noreply.github.com>
2014-10-21 09:27:14 +0300

    Update bcmxcp.c

--- drivers/bcmxcp.c
+++ drivers/bcmxcp.c
@@ -1619,14 +1619,11 @@ void upsdrv_updateinfo(void)
 				status_set("OVER");
 				break;
 			case BCMXCP_STATUS_TRIM: /* Trim */
-				upslogx(LOG_INFO, "Entering Trim state");
 				status_set("OL");
 				status_set("TRIM");
 				break;
 			case BCMXCP_STATUS_BOOST1:
-				upslogx(LOG_INFO, "Entering Boost state");
 			case BCMXCP_STATUS_BOOST2: /* Boost */
-				upslogx(LOG_INFO, "Entering Double Boost state");
 				status_set("OL");
 				status_set("BOOST");
 				break;
