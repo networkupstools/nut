f94b08049139a13fb773f2718cc62296f98351e1
Michal Vyskocil <michalvyskocil@eaton.com>
2016-03-22 11:13:20 +0100

    snmp-ups: enhance documentation of some flags
    Mark SU_FLAG_OK as deprecated and SU_FLAG_STALE as internal to driver.
    This helps new developers to write or extend snmp subdrivers.

--- drivers/snmp-ups.h
+++ drivers/snmp-ups.h
@@ -126,11 +126,11 @@ typedef struct {
 	int          *setvar;		/* variable to set for SU_FLAG_SETINT */
 } snmp_info_t;
 
-#define SU_FLAG_OK			(1 << 0)	/* show element to upsd. */
+#define SU_FLAG_OK			(1 << 0)	/* DEPRECATED: show element to upsd. */
 #define SU_FLAG_STATIC		(1 << 1)	/* retrieve info only once. */
 #define SU_FLAG_ABSENT		(1 << 2)	/* data is absent in the device,
 										 * use default value. */
-#define SU_FLAG_STALE		(1 << 3)	/* data stale, don't try too often. */
+#define SU_FLAG_STALE		(1 << 3)	/* data stale, don't try too often - internal to snmp driver */
 #define SU_FLAG_NEGINVALID	(1 << 4)	/* Invalid if negative value */
 #define SU_FLAG_UNIQUE		(1 << 5)	/* There can be only be one
 						 				 * provider of this info,
