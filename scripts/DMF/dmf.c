57a476af94a765d0535d4856accce18ffb424968
Carlos Dominguez <cdominguezm@gmail.com>
2016-04-19 15:53:31 +0200

    Problem: Implementation of index collection, basic API, debugging destructor ,method

--- scripts/DMF/dmf.c
+++ scripts/DMF/dmf.c
@@ -8,8 +8,9 @@ typedef struct {
 } info_lkp_t;
 
 typedef struct {
-	info_lkp_t **values;
+	void **values;
 	int size;
+	int capacity;
 	char *name;
 } index_lkp_t;
 
@@ -29,13 +30,27 @@ info_lkp_new (int oid, const char *value)
     self->info_value = strdup (value);
     return self;
 }
+void index_lkp_append(index_lkp_t *self,void *element){
+/*  if(self->values==NULL){
+    self->values=(void**)malloc(sizeof(void*));
+    assert(self->values);
+    self->values[self->size]=NULL;
+  }else{
+    self->values=realloc(self->values,self->size*sizeof());
+  }*/
+/*TODO Check when allocatd memory get full for reallocate more*/
+  if(self->size<self->capacity){
+  self->values[self->size]=element;
+  self->size++;
+  }
+}
 
 // Destroy and NULLify the reference to info_lkp_t
 void
-info_lkp_destroy (info_lkp_t **self_p)
+info_lkp_destroy (void **self_p)
 {
     if (*self_p) {
-        info_lkp_t *self = *self_p;
+        info_lkp_t *self =(info_lkp_t*) *self_p;
 
         if (self->info_value) {
             free ((char*)self->info_value);
@@ -46,62 +61,52 @@ info_lkp_destroy (info_lkp_t **self_p)
     }
 }
 
-/*  Use common API ad info_lkp_t
+void index_lkp_delete_allvalues(index_lkp_t *self){
+  do{
+    info_lkp_destroy((void**)&self->values[self->size]);
+    self->size--;
+  }while(self->size>0);
+  free(self->values);
+}
+
+/*  Use common API ad info_lkp_t*/
 // step #1
-index_lkp_new ();
+index_lkp_t *index_lkp_new (const char *name){
+  index_lkp_t *index=(index_lkp_t*)malloc(sizeof(index_lkp_t));
+  assert(index);
+  memset(index, 0, sizeof(index_lkp_t));
+  index->name=strdup(name);
+  index->size=0;
+  index->values=(void**)malloc(16*sizeof(void*));
+  assert(index->values);
+  index->capacity=16;
+  index->values[index->size]=NULL;
+  return index;
+}
 
 // step #2
-index_lkp_append (index_lkp_t *self, void *item);
+//index_lkp_append (index_lkp_t *self, void *item);
 
 // step #3
-index_lkp_destroy (index_lkp_t **self_p);
+//index_lkp_destroy (index_lkp_t **self_p);
 
 // step #4
-index_lkp_set_destructor (index_lkp_t *);
-*/
-int index_add(index_lkp_t *index, int pos, const char **attrs){
-  
-    if(!index)return ERR;
-    
-    index_lkp_t *info = &index[pos];
-  
-    if(info->values==NULL){/*Check if its first pair for crate new pair values array*/
-      info->values=(info_lkp_t**)malloc(sizeof(info_lkp_t));
-      assert(info->values);
-      *(info->values)=NULL;
-      info->values[0]=info_lkp_new(atoi(attrs[1]),attrs[3]);
-      info->size=1;/*Set counter a 1 element in values array of pairs*/
-     
-    }else{/*If its a non empty colection we just add a pair*/
-      info->values=(info_lkp_t**)realloc(info->values,(info->size+1)*sizeof(info_lkp_t));
-      if(info->values==NULL)return ERR;
-      info->values[info->size]=info_lkp_new(atoi(attrs[1]),attrs[3]);
-      info->size++;/*After set the values of the pair just increment the size counter of array of pairs*/
-      
-    }
-    return OK;
-}
-int index_del(index_lkp_t *index, int pos){
-  if(!index)return ERR;
-  printf("%s size: %d : Oid: %d Value: %s  \n",index->name, index->size, (*index->values)->oid_value, (*index->values)->info_value);
-  index_lkp_t *info = &index[pos];
-  info_lkp_destroy(info->values);
-  free(info->values);
-  return OK;
-}
+//index_lkp_set_destructor (index_lkp_t *);
+
 
 int main ()
 {
     //info_lkp_t * lkp = info_lkp_new (1, "one");
-    index_lkp_t *index=(index_lkp_t*)malloc(sizeof(index_lkp_t));
-    assert(index);
-    memset(index, 0, sizeof(index_lkp_t));
     const char *attrs[]={"Sensitivity","1","","OK",""};
-    index->name=strdup(attrs[0]);
-    assert(index);
-    index_add(index,0,attrs);
-    index_del(index,0);
+    index_lkp_t *index=index_lkp_new(attrs[0]);
+    info_lkp_t *element=info_lkp_new(atoi(attrs[1]),attrs[3]);
+    index_lkp_append(index,element);
+    
+    index_lkp_delete_allvalues(index);
+    //index_add(index,0,attrs);
+    //index_del(index,0);
     free(index->name);
+    
     free(index);
     
 
