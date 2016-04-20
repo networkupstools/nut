34e4c28dfe81ecfc2853564b00aaf5b9afa5cac2
Michal Vyskocil <michalvyskocil@eaton.com>
2016-04-20 08:14:16 +0200

    Problem: index_lkp_t is not a good name for common collection struct
    Solution: rename to alist (arraylist)

--- scripts/DMF/dmf.c
+++ scripts/DMF/dmf.c
@@ -1,7 +1,10 @@
 //TODO: not in final
 #include <malamute.h>
 
-
+/*
+ *      HEADER FILE
+ *
+ */
 typedef struct {
 	int oid_value;			/* OID value */
 	const char *info_value;	/* INFO_* value */
@@ -12,7 +15,7 @@ typedef struct {
 	int size;
 	int capacity;
 	char *name;
-} index_lkp_t;
+} alist_t;
 
 typedef enum {
     ERR = -1,
@@ -20,6 +23,19 @@ typedef enum {
 } state_t;
 
 // Create and initialize info_lkp_t
+info_lkp_t *
+    info_lkp_new (int oid, const char *value);
+
+// Destroy and NULLify the reference to info_lkp_t
+void
+    info_lkp_destroy (void **self_p);
+
+/*
+ *
+ *  C FILE
+ *
+ */
+
 info_lkp_t *
 info_lkp_new (int oid, const char *value)
 {
@@ -30,22 +46,7 @@ info_lkp_new (int oid, const char *value)
     self->info_value = strdup (value);
     return self;
 }
-void index_lkp_append(index_lkp_t *self,void *element){
-/*  if(self->values==NULL){
-    self->values=(void**)malloc(sizeof(void*));
-    assert(self->values);
-    self->values[self->size]=NULL;
-  }else{
-    self->values=realloc(self->values,self->size*sizeof());
-  }*/
-/*TODO Check when allocatd memory get full for reallocate more*/
-  if(self->size<self->capacity){
-  self->values[self->size]=element;
-  self->size++;
-  }
-}
 
-// Destroy and NULLify the reference to info_lkp_t
 void
 info_lkp_destroy (void **self_p)
 {
@@ -61,7 +62,22 @@ info_lkp_destroy (void **self_p)
     }
 }
 
-void index_lkp_delete_allvalues(index_lkp_t *self){
+void alist_append(alist_t *self,void *element){
+/*  if(self->values==NULL){
+    self->values=(void**)malloc(sizeof(void*));
+    assert(self->values);
+    self->values[self->size]=NULL;
+  }else{
+    self->values=realloc(self->values,self->size*sizeof());
+  }*/
+/*TODO Check when allocatd memory get full for reallocate more*/
+  if(self->size<self->capacity){
+  self->values[self->size]=element;
+  self->size++;
+  }
+}
+
+void alist_delete_allvalues(alist_t *self){
   do{
     info_lkp_destroy((void**)&self->values[self->size]);
     self->size--;
@@ -71,10 +87,10 @@ void index_lkp_delete_allvalues(index_lkp_t *self){
 
 /*  Use common API ad info_lkp_t*/
 // step #1
-index_lkp_t *index_lkp_new (const char *name){
-  index_lkp_t *index=(index_lkp_t*)malloc(sizeof(index_lkp_t));
+alist_t *alist_new (const char *name){
+  alist_t *index=(alist_t*)malloc(sizeof(alist_t));
   assert(index);
-  memset(index, 0, sizeof(index_lkp_t));
+  memset(index, 0, sizeof(alist_t));
   index->name=strdup(name);
   index->size=0;
   index->values=(void**)malloc(16*sizeof(void*));
@@ -85,24 +101,24 @@ index_lkp_t *index_lkp_new (const char *name){
 }
 
 // step #2
-//index_lkp_append (index_lkp_t *self, void *item);
+//alist_append (alist_t *self, void *item);
 
 // step #3
-//index_lkp_destroy (index_lkp_t **self_p);
+//alist_destroy (alist_t **self_p);
 
 // step #4
-//index_lkp_set_destructor (index_lkp_t *);
+//alist_set_destructor (alist_t *);
 
 
 int main ()
 {
     //info_lkp_t * lkp = info_lkp_new (1, "one");
     const char *attrs[]={"Sensitivity","1","","OK",""};
-    index_lkp_t *index=index_lkp_new(attrs[0]);
+    alist_t *index=alist_new(attrs[0]);
     info_lkp_t *element=info_lkp_new(atoi(attrs[1]),attrs[3]);
-    index_lkp_append(index,element);
+    alist_append(index,element);
     
-    index_lkp_delete_allvalues(index);
+    alist_delete_allvalues(index);
     //index_add(index,0,attrs);
     //index_del(index,0);
     free(index->name);
