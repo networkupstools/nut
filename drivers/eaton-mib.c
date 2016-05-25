aec36342654578a0e99cbb81624c9888fefdaec7
Jim Klimov <jim@jimklimov.com>
2016-05-25 09:52:37 +0200

    eaton-mib - renamed "outlet_*" lookups to "eaton_outlet_*" (maybe another name would be better?)

--- drivers/eaton-mib.c
+++ drivers/eaton-mib.c
@@ -112,7 +112,7 @@ static snmp_info_t eaton_aphel_genesisII_mib[] = {
 #define AR_OID_OUTLET_NAME				AR_BASE_OID ".1.2.2.1.2"
 #define AR_OID_OUTLET_STATUS			AR_BASE_OID ".1.2.2.1.3"
 
-static info_lkp_t outlet_status_info[] = {
+static info_lkp_t eaton_outlet_status_info[] = {
 	{ -1, "error" },
 	{ 0, "off" },
 	{ 1, "on" },
@@ -191,7 +191,7 @@ static snmp_info_t eaton_aphel_revelation_mib[] = {
 	{ "outlet.%i.switchable", 0, 1, AR_OID_OUTLET_STATUS ".%i", "yes", SU_FLAG_STATIC | SU_OUTLET, &revelation_outlet_switchability_info[0], NULL },
 	{ "outlet.%i.id", 0, 1, NULL, "%i", SU_FLAG_STATIC | SU_FLAG_ABSENT | SU_FLAG_OK | SU_OUTLET, NULL, NULL },
 	{ "outlet.%i.desc", ST_FLAG_RW | ST_FLAG_STRING, SU_INFOSIZE, AR_OID_OUTLET_NAME ".%i", NULL, SU_OUTLET, NULL, NULL },
-	{ "outlet.%i.status", ST_FLAG_STRING, SU_INFOSIZE, AR_OID_OUTLET_STATUS ".%i", NULL, SU_FLAG_OK | SU_OUTLET, &outlet_status_info[0], NULL },
+	{ "outlet.%i.status", ST_FLAG_STRING, SU_INFOSIZE, AR_OID_OUTLET_STATUS ".%i", NULL, SU_FLAG_OK | SU_OUTLET, &eaton_outlet_status_info[0], NULL },
 	{ "outlet.%i.current", 0, 0.001, AR_OID_OUTLET_CURRENT ".%i", NULL, SU_OUTLET, NULL, NULL },
 	{ "outlet.%i.current.maximum", 0, 0.001, AR_OID_OUTLET_MAXCURRENT ".%i", NULL, SU_OUTLET, NULL, NULL },
 	{ "outlet.%i.realpower", 0, 1.0, AR_OID_OUTLET_ACTIVEPOWER ".%i", NULL, SU_OUTLET, NULL, NULL },
@@ -255,14 +255,14 @@ static info_lkp_t marlin_outletgroups_status_info[] = {
 
 /* Ugly hack: having the matching OID present means that the outlet is
  * switchable. So, it should not require this value lookup */
-static info_lkp_t outlet_switchability_info[] = {
+static info_lkp_t eaton_outlet_switchability_info[] = {
 	{ -1, "yes" },
 	{ 0, "yes" },
 	{ 0, NULL }
 };
 
 /* The physical type of outlet */
-static info_lkp_t outlet_type_info[] = {
+static info_lkp_t eaton_outlet_type_info[] = {
 	{ 0, "unknown" },
 	{ 1, "iecC13" },
 	{ 2, "iecC19" },
@@ -607,7 +607,7 @@ static snmp_info_t eaton_marlin_mib[] = {
 	{ "outlet.%i.power", 0, 1.0, ".1.3.6.1.4.1.534.6.6.7.6.5.1.2.%i.%i", NULL, SU_OUTLET | SU_TYPE_DAISY_1, NULL, NULL },
 	/* FIXME: handle non switchable units (only measurements), which do not expose this OID */
 	{ "outlet.%i.switchable", ST_FLAG_STRING, SU_INFOSIZE, ".1.3.6.1.4.1.534.6.6.7.6.6.1.3.%i.%i", "no", SU_FLAG_STATIC | SU_OUTLET | SU_FLAG_OK | SU_TYPE_DAISY_1, &outlet_switchability_info[0], NULL },
-	{ "outlet.%i.type", ST_FLAG_STRING, SU_INFOSIZE, ".1.3.6.1.4.1.534.6.6.7.6.6.1.5.0.%i", "unknown", SU_FLAG_STATIC | SU_OUTLET, &outlet_type_info[0], NULL },
+	{ "outlet.%i.type", ST_FLAG_STRING, SU_INFOSIZE, ".1.3.6.1.4.1.534.6.6.7.6.6.1.5.0.%i", "unknown", SU_FLAG_STATIC | SU_OUTLET, &eaton_outlet_type_info[0], NULL },
 
 	/* TODO: handle statistics
 	 * outletWh.0.1
