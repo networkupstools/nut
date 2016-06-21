0681636300e7fcd9dd2b2999bcb5e4d540ba8817
Carlos Dominguez <cdominguezm@gmail.com>
2016-06-21 14:40:45 +0200

    Some makeup.

--- drivers/snmp-ups.c
+++ drivers/snmp-ups.c
@@ -2184,7 +2184,7 @@ int publish_Lua_dstate(lua_State *L){
   
   if((info_type) && (value))
       dstate_setinfo(info_type, value);
-  return 1;
+  return 0;
 }
 
 int lua_C_gateway(lua_State *L){
