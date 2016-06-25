cbe74e9a40686d6d709b9f3f7daea19c3c81ac50
zipotron <zipotron@localhost.(none)>
2016-06-25 08:57:23 +0200

    Lua library update

--- drivers/snmp-ups.h
+++ drivers/snmp-ups.h
@@ -50,9 +50,9 @@
 #define SNMP_UPS_H
 
 #ifdef WITH_DMF_LUA
-# include <lua.h>
-# include <lauxlib.h>
-# include <lualib.h>
+# include <lua5.2/lua.h>
+# include <lua5.2/lauxlib.h>
+# include <lua5.2/lualib.h>
 #endif
 
 /* FIXME: still needed?
