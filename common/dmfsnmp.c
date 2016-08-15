3a7c3ae43af1aee1dcd8a9cde10f5659457dcbf8
Jim Klimov <jim@jimklimov.com>
2016-08-15 12:42:52 +0200

    dmfsnmp.c : report the pattern searched for DMF files in a directory (S*.dmf, not plain *.dmf)

--- common/dmfsnmp.c
+++ common/dmfsnmp.c
@@ -1571,10 +1571,10 @@ mibdmf_parse_dir (char *dir_name, mibdmf_parser_t *dmp)
 	unload_neon_lib();
 
 	if (i==0) {
-		upsdebugx(1, "WARN: No DMF files were found or readable in directory '%s'",
+		upsdebugx(1, "WARN: No 'S*.dmf' DMF files were found or readable in directory '%s'",
 			dir_name ? dir_name : "<NULL>");
 	} else {
-		upsdebugx(1, "INFO: %d DMF files were inspected in directory '%s'",
+		upsdebugx(1, "INFO: %d 'S*.dmf' DMF files were inspected in directory '%s'",
 			i, dir_name ? dir_name : "<NULL>");
 	}
 	if (result!=0 || x>0) {
