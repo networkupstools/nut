766a01a469981db0aad84c64ee60db4e00862451
Michal Vyskocil <michalvyskocil@eaton.com>
2016-08-15 14:26:28 +0200

    Problem: not so easy to debug nut-scanner failures
    Solution: add a few developer debugs

--- common/dmfsnmp.c
+++ common/dmfsnmp.c
@@ -1478,6 +1478,7 @@ mibdmf_parse_dir (char *dir_name, mibdmf_parser_t *dmp)
 	int c;
 	for (c = 0; c < n; c++)
 	{
+		upsdebugx (5, "%s: dir_ent[%d]->d_name=%s", __PRETTY_FUNCTION__, c, dir_ent[c]->d_name);
 		if (strstr(dir_ent[c]->d_name, ".dmf"))
 		{
 			i++;
@@ -1486,6 +1487,7 @@ mibdmf_parse_dir (char *dir_name, mibdmf_parser_t *dmp)
 				sprintf(file_path, "%s/%s", dir_name, dir_ent[c]->d_name);
 				assert(file_path);
 				int res = mibdmf_parse_file(file_path, dmp);
+				upsdebugx (5, "mibdmf_parse_file (\"%s\", <%p>)=%d", file_path, (void*)dmp, res);
 				if ( res != 0 )
 				{
 					x++;
