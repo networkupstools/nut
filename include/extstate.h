9542fc54bf47621533118cd64473820f550df1d6
Arnaud Quette <arnaud.quette@free.fr>
2016-08-16 15:50:32 +0200

    Add an explicit type NONE (ST_FLAG_NONE)
    Reference: https://github.com/networkupstools/nut/issues/267

--- include/extstate.h
+++ include/extstate.h
@@ -13,6 +13,7 @@ extern "C" {
 #define ST_MAX_VALUE_LEN 256
 
 /* state tree flags */
+#define ST_FLAG_NONE      0x0000
 #define ST_FLAG_RW        0x0001
 #define ST_FLAG_STRING    0x0002 /* not STRING implies NUMBER */
 #define ST_FLAG_NUMBER    0x0004
