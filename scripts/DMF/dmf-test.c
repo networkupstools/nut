e4381de5f8d324fdd635d4e3fd61b89688d5fcc9
Jim Klimov <jim@jimklimov.com>
2016-09-08 00:46:40 +0200

    dmf-test.c : Update the tests with detailed parsing log, and try to look up some data in the mapping tables as part of test

--- scripts/DMF/dmf-test.c
+++ scripts/DMF/dmf-test.c
@@ -44,7 +44,11 @@
 int
 main ()
 {
+	nut_debug_level = 10;
+
 	int result;
+	// Array of pointers to singular instances of mib2nut_info_t
+	mib2nut_info_t **mib2nut = NULL;
 	mibdmf_parser_t * dmp = mibdmf_parser_new();
 	if (!dmp) {
 		fprintf(stderr,"FATAL: Can not allocate the DMF parsing structures\n");
@@ -94,10 +98,40 @@ main ()
 			result = 1;
 		}
 
-		/*printf("\n\n");
-		 *printf("=== DMF-Test: Original C structures (sample for 'eaton_epdu'):\n\n");
-		 *print_mib2nut_memory_struct(&eaton_marlin);
-		 *End debugging */
+		/* printf("\n\n");
+		 * printf("=== DMF-Test: Original C structures (sample for 'eaton_epdu'):\n\n");
+		 * print_mib2nut_memory_struct(&eaton_marlin);
+		 * End debugging */
+
+#if WITH_DMF_FUNCTIONS
+#if WITH_DMF_LUA
+		mib2nut = *(mibdmf_get_mib2nut_table_ptr)(dmp);
+		if ( mib2nut == NULL ) {
+			upsdebugx(1,"FATAL: Could not access the mib2nut index table");
+			result = 1;
+		} else {
+			int i;
+/* Note: the tests are lax: they rely on two first items in both test DMFs
+ * being function-interpreted. Normally we'd search and match that... */
+			for (i = 0; mib2nut[i] != NULL; i++) {
+				if (strcmp("mge", mib2nut[i]->mib_name)==0) {
+					printf("=== DMF-Test: Found an mge MIB mapping, dumping snmp_info; iterator == %i \n\n", i);
+					print_snmp_memory_struct(&(mib2nut[i]->snmp_info[0]));
+					print_snmp_memory_struct(&(mib2nut[i]->snmp_info[1]));
+					break;
+				}
+			}
+			for (i = 0; mib2nut[i] != NULL; i++) {
+				if (strcmp("eaton_epdu", mib2nut[i]->mib_name)==0) {
+					printf("=== DMF-Test: Found an eaton_epdu MIB mapping, dumping snmp_info (note: the gateway routines manipulate driver state (snmp-ups) and do not make sense here, not linked, return error as expected); iterator == %i \n\n", i);
+					print_snmp_memory_struct(&(mib2nut[i]->snmp_info[0]));
+					print_snmp_memory_struct(&(mib2nut[i]->snmp_info[1]));
+					break;
+				}
+			}
+		}
+#endif
+#endif
 
 		printf("=== DMF-Test: Freeing data...\n\n");
 		mibdmf_parser_destroy(&dmp);
