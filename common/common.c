24d3d2df2722366e091833fb9df23b75596c78b2
Jim Klimov <jim@jimklimov.com>
2016-09-14 10:41:08 +0200

    common.c : revised upsdebugx() and upslogx() calls to not trail an extra newline

--- common/common.c
+++ common/common.c
@@ -708,6 +708,7 @@ char * get_libname(const char* base_libname)
 		closedir(dp);
 	}
 
-	upsdebugx(1,"Looking for lib %s, found %s\n", base_libname, (libname_path!=NULL)?libname_path:"NULL");
+	upsdebugx(1,"Looking for lib %s, found %s",
+		base_libname, (libname_path!=NULL)?libname_path:"NULL");
 	return libname_path;
 }
