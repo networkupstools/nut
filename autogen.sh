fc5f808a827d5d80c50d3d0a3c36c20b9d987cd7
Michal Soltys <soltys@ziu.info>
2016-11-17 14:36:19 +0100

    autogen.sh: minor script fixup
    In case any of the perl / python parts fails (for example if
    system default is python3, the script will complain about print),
    cd ../.. won't be executed.
    
    Furthermore - if any of those scripts fail, there's no point in
    continuing as both files are mandatory.

--- autogen.sh
+++ autogen.sh
@@ -8,12 +8,16 @@ if [ ! -f scripts/augeas/nutupsconf.aug.in ]
 then
 	if python -c "import re,glob,codecs"; then
 		echo "Regenerating Augeas ups.conf lens..."
-		cd scripts/augeas && ./gen-nutupsconf-aug.py && cd ../..
+		cd scripts/augeas && {
+			./gen-nutupsconf-aug.py || exit 1
+			cd ../..
+		}
 	else
 		echo "----------------------------------------------------------------------"
-		echo "Warning: Python is not available."
-		echo "Skipping Augeas ups.conf lens regeneration."
+		echo "Error: Python is not available."
+		echo "Unable to regenerate Augeas ups.conf lens."
 		echo "----------------------------------------------------------------------"
+		exit 1
 	fi
 fi
 
@@ -22,12 +26,16 @@ if [ ! -f scripts/udev/nut-usbups.rules.in -o \
 then
 	if perl -e 1; then
 		echo "Regenerating the USB helper files..."
-		cd tools && ./nut-usbinfo.pl && cd ..
+		cd tools && {
+			./nut-usbinfo.pl || exit 1
+			cd ..
+		}
 	else 
 		echo "----------------------------------------------------------------------"
 		echo "Error: Perl is not available."
-		echo "Skipping the USB helper files regeneration."
+		echo "Unable to regenerate USB helper files."
 		echo "----------------------------------------------------------------------"
+		exit 1
 	fi
 fi
 
