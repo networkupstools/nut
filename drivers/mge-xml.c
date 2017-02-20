f12b97e0b4902e38bacf10efa14219be42b7ec0e
Jim Klimov <jim@jimklimov.com>
2017-02-20 11:49:15 +0100

    drivers/mge-xml.c : in convert_deci() now report it is deprecated (with debug level 5) so we get reports if deprecation bites anyone in reality

--- drivers/mge-xml.c
+++ drivers/mge-xml.c
@@ -415,6 +415,7 @@ static const char *convert_deci(const char *val)
 	 * by e.g. reported voltage and amps (to be an order of magnitude for power).
 	 * Alternately we can look at model names and/or firmware versions or release
 	 * dates, if we get those and if we know enough to map them to either logic. */
+	upsdebugx(5, "%s() is now deprecated, so value '%s' is not decimated. If you happen to have an old MGE NetXML-capable device that now shows measurements 10x too big, and a firmware update does not solve this, please inform NUT devs via the GitHub issue tracker with details about your hardware and firmware versions", __func__, val);
 	return val;
 
 /* Old code for old devices: */
