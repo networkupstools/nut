d8815d7d05e90d3d75910a293705581670997156
Petr Kubanek <petr@pc115b.fzu.cz>
2017-03-14 06:36:32 +0100

    fixed problem with reading from driver by lines

--- drivers/ve-direct.c
+++ drivers/ve-direct.c
@@ -36,10 +36,6 @@ upsdrv_info_t upsdrv_info = {
 #define ENDCHAR	'\r'
 #define IGNCHARS "\n"
 
-int  sdwdelay = 0;  /* shutdown after 0 second */
-
-static int start_is_datastale = 1;
-
 static int instcmd(const char *cmdname, const char *extra)
 {
 	upsdebugx(1, "instcmd: unknown command: %s", cmdname);
@@ -55,10 +51,14 @@ void upsdrv_initinfo(void)
 void upsdrv_updateinfo(void)
 {
 	/* ups.status */ 
-	char line[200];
-	char var[200];
-	char vn[200];
-	
+	char line[2000];
+	char vn[50];
+
+	char *v_name;
+	char *v_value;
+
+	size_t r_start = 0;
+
 	status_init();
 	status_set("OL");
 	status_commit();
@@ -68,24 +68,71 @@ void upsdrv_updateinfo(void)
 	/************** ups.x ***************************/
 	while (1)
 	{
-		int nred = ser_get_line(upsfd, line, sizeof(line), '\t', "", 1, 0);
+		if (r_start >= sizeof(line))
+			r_start = 0;
+		memset(line + r_start, 0, sizeof(line) - r_start);
+		int nred = ser_get_buf(upsfd, line + r_start, sizeof(line) - r_start, 1, 0);
 		if (nred < 0)
 			break;
 		if (nred == 0)
 			continue;
-		if (!strcmp(line, "Checksum"))
-			break;
-		nred = ser_get_line(upsfd, var, sizeof(var), '\n', "", 1, 0);
-		if (nred < 0)
-			break;
-		if (nred == 0)
+		upsdebugx (3, "%s: buf '%s'", __func__, line);
+		r_start += nred;
+		// find checksum, verify checksum
+		char *checksum = strstr(line, "Checksum\t");
+		if (checksum == NULL || checksum + 9 >= line + r_start)
 			continue;
-		upsdebugx(1, "upsdrv_updateinfo: read %s %s\n", line, var);
-		snprintf (vn, "ve-direct.%s", line);
 
-		dstate_setinfo(vn, "%s", var);
+		// calculate checksum
+		char ch = 0;
+		for (v_name = line; v_name <= checksum + 8; v_name++)
+			ch += *v_name;
+		ch = ~ch + 1;
+		if (ch != checksum[9])
+		{
+			upslogx(1, "invalid checksum: %d %d", ch, checksum[9]);
+			r_start = 0;
+			continue;
+		}
+
+		checksum[9] = '\0';
+		upsdebugx(2, "buffer: %s", line);
+
+		// parse what is in buffer..
+		v_name = line;
+		while (v_name < checksum)
+		{
+			if (v_name[0] != '\r' || v_name[1] != '\n')
+				break;
+			v_name += 2;
+			v_value = v_name;
+			while (*v_value != '\t' && *v_value != '\0')
+				v_value++;
+			if (v_value >= checksum)
+				break;
+			*v_value = '\0';
+			v_value++;
+			char *end = v_value;
+			while (*end != '\r' && *end != '\0')
+				end++;
+			if (*end != '\r')
+				break;
+			*end = '\0';
+
+			snprintf(vn, sizeof(vn), "ve-direct.%s", v_name);
+			upsdebugx(1, "name %s value %s", vn, v_value);
+			dstate_setinfo(vn, "%s", v_value);
+
+			v_name = end;
+			*v_name = '\r';
+		}
+		size_t left = (line + r_start) - (checksum + 9);
+		if (left > 0)
+			memmove(line, checksum + 10, left);
+
+		r_start = left;
+		break;
 	}
-  
 
 	dstate_dataok();
 }
@@ -101,21 +148,12 @@ void upsdrv_help(void)
 /* list flags and values that you want to receive via -x */
 void upsdrv_makevartable(void)
 {
-	addvar(VAR_VALUE, "usd", "Seting delay before shutdown");
 }
 
 void upsdrv_initups(void)
 {
-	char *usd = NULL;
-   
 	upsfd = ser_open(device_path);
 	ser_set_speed(upsfd, device_path, B19200);
-
-
-	if ((usd = getval("usd"))) {
-		sdwdelay=atoi(usd);
-		upsdebugx(1, "(-x) Delay before shutdown %i",sdwdelay);
-	}
 }
 
 void upsdrv_cleanup(void)
