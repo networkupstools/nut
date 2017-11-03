db3f38fd1ea6c57de8d650bf98cae0f3a20bb138
Arnaud Quette <ArnaudQuette@Eaton.com>
2017-11-03 12:14:34 +0100

    Add support for dry contacts to Eaton ATS16 and UPS
    Add support for the 2 GPI accessible through EMP001 environmental sensor,
    connected to a UPS or ATS16. The same is already available for Eaton ePDU.
    This affect the snmp-ups driver (eaton_ats16 and pw/pxgx_ups MIBs), and the
    netxml-ups driver
    
    Signed-off-by: Arnaud Quette <ArnaudQuette@Eaton.com>

--- drivers/powerware-mib.c
+++ drivers/powerware-mib.c
@@ -25,7 +25,7 @@
 
 #include "powerware-mib.h"
 
-#define PW_MIB_VERSION "0.90"
+#define PW_MIB_VERSION "0.91"
 
 /* TODO: more sysOID and MIBs support:
  * 
@@ -194,6 +194,15 @@ static info_lkp_t pw_yes_no_info[] = {
 	{ 0, NULL }
 };
 
+static info_lkp_t pw_ambient_drycontacts_info[] = {
+	{ -1, "unknown" },
+	{ 1, "open" },
+	{ 2, "closed" },
+	{ 3, "open" },   /* openWithNotice   */
+	{ 4, "closed" }, /* closedWithNotice */
+	{ 0, NULL }
+};
+
 /* Snmp2NUT lookup table */
 
 static snmp_info_t pw_mib[] = {
@@ -358,6 +367,12 @@ static snmp_info_t pw_mib[] = {
 	{ "ambient.humidity.low", ST_FLAG_RW, 1.0, "1.3.6.1.4.1.534.1.6.11.0", "", 0, NULL },
 	/* XUPS-MIB::xupsEnvRemoteHumidityUpperLimit.0 */
 	{ "ambient.humidity.high", ST_FLAG_RW, 1.0, "1.3.6.1.4.1.534.1.6.12.0", "", 0, NULL },
+	/* XUPS-MIB::xupsContactState.1 */
+	{ "ambient.contacts.1.status", ST_FLAG_STRING, SU_INFOSIZE,
+		".1.3.6.1.4.1.534.1.6.8.1.3.1", "", 0, &pw_ambient_drycontacts_info[0] },
+	/* XUPS-MIB::xupsContactState.2 */
+	{ "ambient.contacts.2.status", ST_FLAG_STRING, SU_INFOSIZE,
+		".1.3.6.1.4.1.534.1.6.8.1.3.2", "", 0, &pw_ambient_drycontacts_info[0] },
 
 	/* instant commands */
 	{ "test.battery.start.quick", 0, 1, PW_OID_BATTEST_START, "",
