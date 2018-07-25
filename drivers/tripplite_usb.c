a0d59c845ebc77947866028757c7b8dcd79efead
mbastiaan <35669404+mbastiaan@users.noreply.github.com>
2018-07-25 14:37:17 +0200

    Update tripplite_usb.c
    Incremented driver version for pull request #584

--- drivers/tripplite_usb.c
+++ drivers/tripplite_usb.c
@@ -136,7 +136,7 @@
 #include "usb-common.h"
 
 #define DRIVER_NAME		"Tripp Lite OMNIVS / SMARTPRO driver"
-#define DRIVER_VERSION	"0.29"
+#define DRIVER_VERSION	"0.30"
 
 /* driver description structure */
 upsdrv_info_t	upsdrv_info = {
