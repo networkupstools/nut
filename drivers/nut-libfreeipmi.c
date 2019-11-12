dbabfad7380fc3c0f1dc78e44e883ddb824036d5
Arnaud Quette <ArnaudQuette@Eaton.com>
2019-11-12 16:48:00 +0100

    IPMI driver (FreeIPMI backend): fix signedness warnings #2
    Signed-off-by: Arnaud Quette <ArnaudQuette@Eaton.com>

--- drivers/nut-libfreeipmi.c
+++ drivers/nut-libfreeipmi.c
@@ -351,16 +351,16 @@ static int libfreeipmi_get_psu_info (const void *areabuf,
 	unsigned int overall_capacity;
 	int low_end_input_voltage_range_1;
 	int high_end_input_voltage_range_1;
-	int low_end_input_frequency_range;
-	int high_end_input_frequency_range;
+	unsigned int low_end_input_frequency_range;
+	unsigned int high_end_input_frequency_range;
 	unsigned int voltage_1;
 
 	/* FIXME: check for the interest and capability to use these data */
 	unsigned int peak_va;
 	unsigned int inrush_current;
 	unsigned int inrush_interval;
-	unsigned int low_end_input_voltage_range_2;
-	unsigned int high_end_input_voltage_range_2;
+	int low_end_input_voltage_range_2;
+	int high_end_input_voltage_range_2;
 	unsigned int ac_dropout_tolerance;
 	unsigned int predictive_fail_support;
 	unsigned int power_factor_correction;
