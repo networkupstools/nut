eb86469b5ddeaa5edc36db1306b7eebe496312c5
Jim Klimov <jim@jimklimov.com>
2020-11-05 00:25:50 +0100

    drivers/bestfortress.c: mark up NUT_UNUSED_VARIABLEs

--- drivers/bestfortress.c
+++ drivers/bestfortress.c
@@ -193,7 +193,7 @@ static int upsrecv(char *buf,size_t bufsize,char ec,const char *ic)
 	                    SER_WAIT_SEC, SER_WAIT_USEC);
 }
 
-static int upsflushin(int f,int verbose,const char *ignset)
+static int upsflushin(int NUT_UNUSED(f),int verbose,const char *ignset)
 {
 	return ser_flush_in(upsfd, ignset, verbose);
 }
