7471ea115b085d34c9f5b7462ec05f0f1ba3a132
Jim Klimov <jim@jimklimov.com>
2020-11-06 18:29:52 +0100

    tools/nut-scanner/nut-scanner.c: fix whitespaces

--- tools/nut-scanner/nut-scanner.c
+++ tools/nut-scanner/nut-scanner.c
@@ -101,7 +101,7 @@ static void * run_snmp(void * arg)
 {
 	nutscan_snmp_t * sec = (nutscan_snmp_t *)arg;
 
-	dev[TYPE_SNMP] = nutscan_scan_snmp(start_ip,end_ip,timeout,sec);
+	dev[TYPE_SNMP] = nutscan_scan_snmp(start_ip, end_ip, timeout, sec);
 	return NULL;
 }
 
@@ -115,7 +115,7 @@ static void * run_xml(void * arg)
 
 static void * run_nut_old(void * arg)
 {
-	dev[TYPE_NUT] = nutscan_scan_nut(start_ip,end_ip,port,timeout);
+	dev[TYPE_NUT] = nutscan_scan_nut(start_ip, end_ip, port, timeout);
 	return NULL;
 }
 
@@ -129,13 +129,13 @@ static void * run_ipmi(void * arg)
 {
 	nutscan_ipmi_t * sec = (nutscan_ipmi_t *)arg;
 
-	dev[TYPE_IPMI] = nutscan_scan_ipmi(start_ip,end_ip,sec);
+	dev[TYPE_IPMI] = nutscan_scan_ipmi(start_ip, end_ip, sec);
 	return NULL;
 }
 
 static void * run_eaton_serial(void * arg)
 {
-	dev[TYPE_EATON_SERIAL] = nutscan_scan_eaton_serial (serial_ports);
+	dev[TYPE_EATON_SERIAL] = nutscan_scan_eaton_serial(serial_ports);
 	return NULL;
 }
 
