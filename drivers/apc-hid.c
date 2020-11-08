50ddec752311832ba3b2f53453852d7262ed4e35
Jim Klimov <jim@jimklimov.com>
2020-11-08 06:39:53 +0100

    drivers/apc-hid.c: mark up NUT_UNUSED_VARIABLEs (and comment a FIXME about possible use for that)

--- drivers/apc-hid.c
+++ drivers/apc-hid.c
@@ -48,9 +48,10 @@ char * tweak_max_report[] = {
 	NULL};
 
 /* Don't use interrupt pipe on 5G models (used by proprietary protocol) */
-static void *disable_interrupt_pipe(USBDevice_t *device)
+static void *disable_interrupt_pipe(USBDevice_t *  NUT_UNUSED(device))
 {
 	if (use_interrupt_pipe == TRUE) {
+		/* FIXME? Suggest data from "device" to help the setup below? */
 		upslogx(LOG_INFO, "interrupt pipe disabled (add 'pollonly' flag to 'ups.conf' to get rid of this message)");
 		use_interrupt_pipe= FALSE;
 	}
