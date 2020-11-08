ab618908b2cb3e7997f7ff4bfb90b923cd145af4
Jim Klimov <jim@jimklimov.com>
2020-11-08 06:42:28 +0100

    drivers/bcmxcp_usb.c: mark up NUT_UNUSED_VARIABLEs

--- drivers/bcmxcp_usb.c
+++ drivers/bcmxcp_usb.c
@@ -51,7 +51,7 @@ static int usb_set_powerware(usb_dev_handle *udev, unsigned char type, unsigned 
 	return usb_control_msg(udev, USB_ENDPOINT_OUT, USB_REQ_SET_DESCRIPTOR, (type << 8) + index, 0, buf, size, 1000);
 }
 
-static void *powerware_ups(USBDevice_t *device) {
+static void *powerware_ups(USBDevice_t * NUT_UNUSED(device)) {
 	usb_set_descriptor = &usb_set_powerware;
 	return NULL;
 }
@@ -59,10 +59,12 @@ static void *powerware_ups(USBDevice_t *device) {
 /* usb_set_descriptor() for Phoenixtec devices */
 static int usb_set_phoenixtec(usb_dev_handle *udev, unsigned char type, unsigned char index, void *buf, int size)
 {
+	NUT_UNUSED_VARIABLE(index);
+	NUT_UNUSED_VARIABLE(type);
 	return usb_control_msg(udev, 0x42, 0x0d, (0x00 << 8) + 0x0, 0, buf, size, 1000);
 }
 
-static void *phoenixtec_ups(USBDevice_t *device) {
+static void *phoenixtec_ups(USBDevice_t * NUT_UNUSED(device)) {
 	usb_set_descriptor = &usb_set_phoenixtec;
 	return NULL;
 }
@@ -464,7 +466,7 @@ usb_dev_handle *nutusb_open(const char *port)
 }
 
 /* FIXME: this part can go into common... */
-int nutusb_close(usb_dev_handle *dev_h, const char *port)
+int nutusb_close(usb_dev_handle *dev_h, const char * NUT_UNUSED(port))
 {
 	if (dev_h)
 	{
