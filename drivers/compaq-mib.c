aa9c57e37097df1fcc3cc6f0af85343026b15db9
Jim Klimov <jim@jimklimov.com>
2020-11-08 04:56:20 +0100

    drivers/compaq-mib.c: initialize all fields of info_lkp_t arrays

--- drivers/compaq-mib.c
+++ drivers/compaq-mib.c
@@ -84,87 +84,87 @@
 
 /* Not used, as no longer supported by MIB ver. 1.76 (Github issue 118)
 static info_lkp_t cpqpower_alarm_ob[] = {
-	{ 1, "OB" },
-	{ 0, NULL }
+	{ 1, "OB", NULL, NULL },
+	{ 0, NULL, NULL, NULL }
 };
 */
 
 /* Not used, as no longer supported by MIB ver. 1.76 (Github issue 118)
 static info_lkp_t cpqpower_alarm_lb[] = {
-	{ 1, "LB" },
-	{ 0, NULL }
+	{ 1, "LB", NULL, NULL },
+	{ 0, NULL, NULL, NULL }
 };
 */
 
 /* Defines for CPQPOWER_OID_POWER_STATUS (1) */
 static info_lkp_t cpqpower_pwr_info[] = {
-	{ 1, ""       /* other */ },
-	{ 2, "OFF"    /* none */ },
-	{ 3, "OL"     /* normal */ },
-	{ 4, "OL BYPASS" /* bypass */ },
-	{ 5, "OB"     /* battery */ },
-	{ 6, "OL BOOST"  /* booster */ },
-	{ 7, "OL TRIM"   /* reducer */ },
-	{ 8, "OL"   /* parallelCapacity */ },
-	{ 9, "OL"   /* parallelRedundant */ },
-	{ 10, "OL" /* HighEfficiencyMode */ },
-	{ 0, NULL }
+	{ 1, ""       /* other */, NULL, NULL },
+	{ 2, "OFF"    /* none */, NULL, NULL },
+	{ 3, "OL"     /* normal */, NULL, NULL },
+	{ 4, "OL BYPASS" /* bypass */, NULL, NULL },
+	{ 5, "OB"     /* battery */, NULL, NULL },
+	{ 6, "OL BOOST"  /* booster */, NULL, NULL },
+	{ 7, "OL TRIM"   /* reducer */, NULL, NULL },
+	{ 8, "OL"   /* parallelCapacity */, NULL, NULL },
+	{ 9, "OL"   /* parallelRedundant */, NULL, NULL },
+	{ 10, "OL" /* HighEfficiencyMode */, NULL, NULL },
+	{ 0, NULL, NULL, NULL }
 } ;
 
 static info_lkp_t cpqpower_mode_info[] = {
-	{ 1, ""  },
-	{ 2, ""  },
-	{ 3, "normal" },
-	{ 4, "" },
-	{ 5, "" },
-	{ 6, "" },
-	{ 7, "" },
-	{ 8, "parallel capacity" },
-	{ 9, "parallel redundancy" },
-	{10, "high efficiency" },
-	{ 0, NULL }
+	{ 1, "", NULL, NULL },
+	{ 2, "", NULL, NULL },
+	{ 3, "normal", NULL, NULL },
+	{ 4, "", NULL, NULL },
+	{ 5, "", NULL, NULL },
+	{ 6, "", NULL, NULL },
+	{ 7, "", NULL, NULL },
+	{ 8, "parallel capacity", NULL, NULL },
+	{ 9, "parallel redundancy", NULL, NULL },
+	{10, "high efficiency", NULL, NULL },
+	{ 0, NULL, NULL, NULL }
 };
 
 static info_lkp_t cpqpower_battery_abm_status[] = {
-	{ 1, "CHRG" },
-	{ 2, "DISCHRG" },
-/*	{ 3, "Floating" }, */
-/*	{ 4, "Resting" }, */
-/*	{ 5, "Unknown" }, */
-	{ 0, NULL }
+	{ 1, "CHRG", NULL, NULL },
+	{ 2, "DISCHRG", NULL, NULL },
+/*	{ 3, "Floating", NULL, NULL }, */
+/*	{ 4, "Resting", NULL, NULL }, */
+/*	{ 5, "Unknown", NULL, NULL }, */
+	{ 0, NULL, NULL, NULL }
 } ;
 
 /* Defines for CPQPOWER_OID_UPS_TEST_RES */
 static info_lkp_t cpqpower_test_res_info[] = {
-	{ 1, "Unknown" },
-	{ 2, "Done and passed" },
-	{ 3, "Done and error" },
-	{ 4, "In progress" },
-	{ 5, "Not supported" },
-	{ 6, "Inhibited" },
-	{ 7, "Scheduled" },
-	{ 0, NULL }
+	{ 1, "Unknown", NULL, NULL },
+	{ 2, "Done and passed", NULL, NULL },
+	{ 3, "Done and error", NULL, NULL },
+	{ 4, "In progress", NULL, NULL },
+	{ 5, "Not supported", NULL, NULL },
+	{ 6, "Inhibited", NULL, NULL },
+	{ 7, "Scheduled", NULL, NULL },
+	{ 0, NULL, NULL, NULL }
 } ;
 
 #define CPQPOWER_START_TEST		"1"
 
 static info_lkp_t cpqpower_outlet_status_info[] = {
-	{ 1, "on" },
-	{ 2, "off" },
-	{ 3, "pendingOff" }, /* transitional status */
-	{ 4, "pendingOn" }, /* transitional status */
-	{ 5, "unknown" },
-	{ 0, NULL }
+	{ 1, "on", NULL, NULL },
+	{ 2, "off", NULL, NULL },
+	{ 3, "pendingOff", NULL, NULL }, /* transitional status */
+	{ 4, "pendingOn", NULL, NULL }, /* transitional status */
+	{ 5, "unknown", NULL, NULL },
+	{ 0, NULL, NULL, NULL }
 };
 
 /* Ugly hack: having the matching OID present means that the outlet is
  * switchable. So, it should not require this value lookup */
 static info_lkp_t cpqpower_outlet_switchability_info[] = {
-	{ 1, "yes" },
-	{ 2, "yes" },
-	{ 3, "yes" },
-	{ 4, "yes" },
-	{ 0, NULL }
+	{ 1, "yes", NULL, NULL },
+	{ 2, "yes", NULL, NULL },
+	{ 3, "yes", NULL, NULL },
+	{ 4, "yes", NULL, NULL },
+	{ 0, NULL, NULL, NULL }
 };
 
 #define CPQPOWER_OID_SD_AFTER_DELAY	 ".1.3.6.1.4.1.232.165.3.8.1.0"	/* UPS-MIB::upsControlOutputOffDelay */
