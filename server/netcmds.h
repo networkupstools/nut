972ddf8a2a2c617448758760dde1ce787e058ecf
Jim Klimov <jim@jimklimov.com>
2020-11-10 02:07:48 +0100

    server/netcmds.h: add (C) and license header

--- server/netcmds.h
+++ server/netcmds.h
@@ -1,6 +1,11 @@
 /* netcmds.h - upsd support structure details
 
    Copyright (C) 2001  Russell Kroll <rkroll@exploits.org>
+	2005	Arnaud Quette <arnaud.quette@free.fr>
+	2007	Peter Selinger <selinger@users.sourceforge.net>
+	2010	Arjen de Korte <adkorte-guest@alioth.debian.org>
+	2012	Emilien Kia <kiae.dev@gmail.com>
+	2020	Jim Klimov <jimklimov@gmail.com>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
