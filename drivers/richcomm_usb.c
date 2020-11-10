1cfd9752bd627ccb55bc939d53e4935956594c38
Jim Klimov <jim@jimklimov.com>
2020-11-10 21:06:20 +0100

    drivers/richcomm_usb.c: mark up NUT_UNUSED_VARIABLEs in the more portable manner

--- drivers/richcomm_usb.c
+++ drivers/richcomm_usb.c
@@ -63,8 +63,10 @@ static usb_dev_handle	*udev = NULL;
 static USBDevice_t	usbdevice;
 static unsigned int	comm_failures = 0;
 
-static int device_match_func(USBDevice_t *device, void * NUT_UNUSED(privdata))
+static int device_match_func(USBDevice_t *device, void *privdata)
 {
+	NUT_UNUSED_VARIABLE(privdata);
+
 	switch (is_usb_device_supported(richcomm_usb_id, device))
 	{
 	case SUPPORTED:
@@ -174,8 +176,10 @@ static void usb_comm_good(void)
  * caller, don't do this here. Return < 0 on error, 0 or higher on
  * success.
  */
-static int driver_callback(usb_dev_handle *handle, USBDevice_t * NUT_UNUSED(device))
+static int driver_callback(usb_dev_handle *handle, USBDevice_t *device)
 {
+	NUT_UNUSED_VARIABLE(device);
+
 	if (usb_set_configuration(handle, 1) < 0) {
 		upsdebugx(5, "Can't set USB configuration");
 		return -1;
