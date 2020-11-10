76cd5a7e36f617a84069bed94d0023452f83c357
Jim Klimov <jim@jimklimov.com>
2020-11-10 03:01:30 +0100

    include/upsconf.h: refine whitespace

--- include/upsconf.h
+++ include/upsconf.h
@@ -7,10 +7,10 @@ extern "C" {
 
 /* callback function from read_upsconf */
 void do_upsconf_args(char *upsname, char *var, char *val);
-   
+
 /* open the ups.conf, parse it, and call back do_upsconf_args() */
 void read_upsconf(void);
-   
+
 #ifdef __cplusplus
 /* *INDENT-OFF* */
 }
