5ffb72c4917a24948facf4cdfcd31ef0658f39c6
Jim Klimov <jim@jimklimov.com>
2020-11-10 02:47:02 +0100

    include/attribute.h: add (C) and license header

--- include/attribute.h
+++ include/attribute.h
@@ -1,4 +1,23 @@
-/* portability hacks for __attribute__ usage in other header files */
+/* attribute.h - portability hacks for __attribute__ usage in other header files
+
+   Copyright (C) 2001  Russell Kroll <rkroll@exploits.org>
+	2005	Arnaud Quette <arnaud.quette@free.fr>
+	2020	Jim Klimov <jimklimov@gmail.com>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
 
 #ifndef NUT_ATTRIBUTE_H_SEEN
 #define NUT_ATTRIBUTE_H_SEEN 1
