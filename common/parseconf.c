2c49ef03ab6e13f1d26568cc7feccea10730db8b
Jim Klimov <jim@jimklimov.com>
2020-11-17 12:54:29 +0100

    parseconf.c: add_arg_word(): fix argpos to size_t, we do not expect negative offsets

--- common/parseconf.c
+++ common/parseconf.c
@@ -110,7 +110,7 @@ static void pconf_fatal(PCONF_CTX_t *ctx, const char *errtxt)
 
 static void add_arg_word(PCONF_CTX_t *ctx)
 {
-	int	argpos;
+	size_t	argpos;
 	size_t	wbuflen;
 
 	/* this is where the new value goes */
