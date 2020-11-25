b3d777fd175f24860c334d17a138337cd7b2b470
Jim Klimov <jim@jimklimov.com>
2020-11-25 13:12:23 +0100

    tests/nutlogtest.c: use HAVE_PRAGMA_GCC_DIAGNOSTIC_IGNORED_FORMAT_OVERFLOW detection to quiesce warnings about "%s" resolving from NULL pointer
    First shot at issue NUT #904

--- tests/nutlogtest.c
+++ tests/nutlogtest.c
@@ -7,6 +7,15 @@
 int main(void) {
     const char *s1 = "!NULL";
     const char *s2 = NULL;
+
+#if (defined HAVE_PRAGMA_GCC_DIAGNOSTIC_PUSH_POP) && (defined HAVE_PRAGMA_GCC_DIAGNOSTIC_IGNORED_FORMAT_OVERFLOW)
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wformat-overflow"
+#endif
     upsdebugx(0, "D: '%s' vs '%s'", s1, s2);
+#if (defined HAVE_PRAGMA_GCC_DIAGNOSTIC_PUSH_POP) && (defined HAVE_PRAGMA_GCC_DIAGNOSTIC_IGNORED_FORMAT_OVERFLOW)
+#  pragma GCC diagnostic pop
+#endif
+
     return 0;
 }
