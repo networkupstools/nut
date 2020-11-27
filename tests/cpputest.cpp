da0bb0d6c114988a79880131a1bb4a6401f3b71d
Jim Klimov <jim@jimklimov.com>
2020-11-27 11:18:09 +0100

    cpputest.cpp: be sure to be able to handle exception objects

--- tests/cpputest.cpp
+++ tests/cpputest.cpp
@@ -19,6 +19,7 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
+#include <stdexcept>
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
