96db93e4f30334a5993a1b2f84a155048e0b04d8
Jim Klimov <jim@jimklimov.com>
2021-01-20 19:48:00 +0100

    drivers/eaton-pdu-marlin-mib.c: drop duplicate induced by merge

--- drivers/eaton-pdu-marlin-mib.c
+++ drivers/eaton-pdu-marlin-mib.c
@@ -65,15 +65,6 @@ static info_lkp_t g2_unit_outlet_switchability_info[] = {
 	{ 0, NULL, NULL, NULL }
 };
 
-/* Ugly hack for older G2 ePDU:
- * having the matching OID present means that the outlet/unit is
- * switchable. So, it should not require this value lookup */
-static info_lkp_t g2_unit_outlet_switchability_info[] = {
-	{ -1, "yes" },
-	{ 0, "yes" },
-	{ 0, NULL }
-};
-
 static info_lkp_t marlin_outlet_switchability_info[] = {
 	{ 1, "yes", NULL, NULL }, /* switchable */
 	{ 2, "no", NULL, NULL }, /* notSwitchable */
