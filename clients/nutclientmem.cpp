05f1f21e2670ccea509cfd5ab7a341fd03ad3a00
Clappier, Eric <EricClappier@Eaton.com>
2021-05-17 08:45:25 +0200

    Take into account pull request remarks

--- clients/nutclientmem.cpp
+++ clients/nutclientmem.cpp
@@ -1,6 +1,6 @@
 /* nutclientmem.cpp - nutclientmem C++ library implementation
 
-   Copyright (C) 2021
+   Copyright (C) 2021  Eric Clappier <ericclappier@eaton.com>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
