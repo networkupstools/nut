ca163a7d85469ecd4cb4b37624cbee09cb0421a7
dtsecon <dimitris.s.economou@gmail.com>
2021-06-25 19:22:57 +0300

    removing unnecessary code

--- drivers/generic_modbus.c
+++ drivers/generic_modbus.c
@@ -405,29 +405,6 @@ int register_write(modbus_t *mb, int addr, regtype_t type, void *data)
     upsdebugx(3, "register addr: 0x%x, register type: %d read: %d",addr, type, *(uint *)data);
     return rval;
 }
-int
-timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y)
-{
-  /* Perform the carry for the later subtraction by updating y. */
-  if (x->tv_usec < y->tv_usec) {
-    int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-    y->tv_usec -= 1000000 * nsec;
-    y->tv_sec += nsec;
-  }
-  if (x->tv_usec - y->tv_usec > 1000000) {
-    int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-    y->tv_usec += 1000000 * nsec;
-    y->tv_sec -= nsec;
-  }
-
-  /* Compute the time remaining to wait.
-     tv_usec is certainly positive. */
-  result->tv_sec = x->tv_sec - y->tv_sec;
-  result->tv_usec = x->tv_usec - y->tv_usec;
-
-  /* Return 1 if result is negative. */
-  return x->tv_sec < y->tv_sec;
-}
 
 /* returns the time elapsed since start in milliseconds */
 long time_ellapsed(struct timeval *start)
