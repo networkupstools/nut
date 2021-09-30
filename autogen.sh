a0e9681a9ff2c58a35ae7ecbc7581f168295d06f
Jim Klimov <jimklimov+nut@gmail.com>
2021-09-30 08:20:28 +0000

    nut-common.tmpfiles.in: fix systemd magic vs autoconf magic
    Follows up from PR #1037 for issue #1030 to let the trick work for
    distcheck and similar out-of-tree builds.

--- autogen.sh
+++ autogen.sh
@@ -68,7 +68,7 @@ echo "Regenerating the list of legacy *-mib.c files in current codebase to produ
 ( cd scripts/DMF && ./gen-legacy-mibfiles-list.sh )
 
 if [ ! -e scripts/systemd/nut-common.tmpfiles.in ]; then
-    echo '# autoconf requires this file exists before generating configure script' > scripts/systemd/nut-common.tmpfiles.in
+	echo '# autoconf requires this file exists before generating configure script; it will be overwritten by configure during a build' > scripts/systemd/nut-common.tmpfiles.in
 fi
 
 # now we can safely call autoreconf
