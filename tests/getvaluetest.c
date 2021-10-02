46d1ccc3e0a4304fa94322c888ce3774d22d0536
Jim Klimov <jim@jimklimov.com>
2021-10-02 15:47:47 +0200

    tests/getvaluetest.c: add (C) and descriptive heading

--- tests/getvaluetest.c
+++ tests/getvaluetest.c
@@ -1,3 +1,31 @@
+/* getvaluetest - check that the bitness/endianness dependent conversions
+ * of representation of numeric types between wire protocols and different
+ * CPU computations produce expected results.
+ *
+ * See also:
+ *  https://github.com/networkupstools/nut/pull/1055
+ *  https://github.com/networkupstools/nut/pull/1040
+ *  https://github.com/networkupstools/nut/pull/1024
+ *  https://github.com/networkupstools/nut/issues/1023
+ *
+ * Copyright (C)
+ *      2021    Nick Briggs <nicholas.h.briggs@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 #include <stdint.h>
 #include <inttypes.h>
 #include <stdio.h>
