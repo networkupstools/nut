49f1aa509f3091f82ee6574777f9049893799852
Jim Klimov <jim@jimklimov.com>
2021-10-04 21:07:16 +0200

    drivers/apcupsd-ups.c: upsdrv_initups(): fix type of port

--- drivers/apcupsd-ups.c
+++ drivers/apcupsd-ups.c
@@ -42,7 +42,7 @@ upsdrv_info_t upsdrv_info = {
 	{ NULL }
 };
 
-static int port=3551;
+static uint16_t port=3551;
 static struct sockaddr_in host;
 
 static void process(char *item,char *data)
@@ -302,9 +302,11 @@ void upsdrv_initups(void)
 		/* TODO: fix parsing since bare IPv6 addresses contain colons */
 		if((p=strchr(device_path,':')))
 		{
+			int i;
 			*p++=0;
-			port=atoi(p);
-			if(port<1||port>65535)port=0;
+			i=atoi(p);
+			if(i<1||i>65535)i=0;
+			port = (uint16_t)i;
 		}
 	}
 	else device_path="localhost";
