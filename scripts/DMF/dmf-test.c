d24ccda3cc6d0c3b73dec42b2c3877bc7985c213
Jim Klimov <jim@jimklimov.com>
2021-11-16 15:45:11 +0100

    scripts/DMF/dmf-test.c: include config.h first

--- scripts/DMF/dmf-test.c
+++ scripts/DMF/dmf-test.c
@@ -23,6 +23,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
+#include "config.h"	/* must be the first header */
+
 #include <errno.h>
 #include <dirent.h>
 #include <assert.h>
