de0bb6c78fef617625a60166103ea088520cacf3
Jim Klimov <jim@jimklimov.com>
2021-11-16 13:08:12 +0100

    drivers/snmp-ups.c: include main.h => common.h => config.h first

--- drivers/snmp-ups.c
+++ drivers/snmp-ups.c
@@ -31,14 +31,14 @@
  *
  */
 
-#include <ctype.h> /* for isprint() */
-
 /* NUT SNMP common functions */
-#include "main.h"
+#include "main.h"	/* includes "config.h" which must be the first header */
 #include "nut_float.h"
 #include "nut_stdint.h"
 #include "snmp-ups.h"
 #include "parseconf.h"
+
+#include <ctype.h> /* for isprint() */
 
 /* include all known mib2nut lookup tables */
 #include "apc-mib.h"
