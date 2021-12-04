b44c56b787a0cbe8910df95529fa77d6c3edb779
Jim Klimov <jim@jimklimov.com>
2021-12-04 16:07:04 +0100

    drivers/apcsmart.c: include main.h => config.h first

--- drivers/apcsmart.c
+++ drivers/apcsmart.c
@@ -20,13 +20,14 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
+#include "main.h"	/* Must be first, includes "config.h" */
+
 #include <sys/types.h>
 #include <sys/file.h>
 #include <regex.h>
 #include <ctype.h>
 #include <strings.h> /* strcasecmp() */
 
-#include "main.h"
 #include "serial.h"
 #include "timehead.h"
 
