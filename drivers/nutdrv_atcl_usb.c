f968ed3aa7bab01dd2b316e9aa050e4aaf9f4e67
Jim Klimov <jim@jimklimov.com>
2021-12-20 21:32:50 +0100

    drivers/nutdrv_atcl_usb.c: drop local libusb-0.1 vs 1.0 definitions now provided by usb-common.h

--- drivers/nutdrv_atcl_usb.c
+++ drivers/nutdrv_atcl_usb.c
@@ -461,14 +461,18 @@ static int usb_device_open(usb_dev_handle **handlep, USBDevice_t *device, USBDev
 				/* this method requires at least libusb 0.1.8:
 				 * it forces device claiming by unbinding
 				 * attached driver... From libhid */
-				if (usb_detach_kernel_driver_np(handle, 0) < 0) {
-					upsdebugx(1, "failed to detach kernel driver from USB device: %s", usb_strerror());
+				if ((ret = usb_detach_kernel_driver_np(handle, 0)) < 0) {
+					upsdebugx(1,
+						"failed to detach kernel driver from USB device: %s",
+						nut_usb_strerror(ret));
 				} else {
 					upsdebugx(4, "detached kernel driver from USB device...");
 				}
 #elif HAVE_LIBUSB_DETACH_KERNEL_DRIVER
 				if ((ret = libusb_detach_kernel_driver(udev, 0)) < 0) {
-					upsdebugx(4, "failed to detach kernel driver from USB device: %s", nut_usb_strerror(ret));
+					upsdebugx(4,
+						"failed to detach kernel driver from USB device: %s",
+						nut_usb_strerror(ret));
 				} else {
 					upsdebugx(4, "detached kernel driver from USB device...");
 				}
