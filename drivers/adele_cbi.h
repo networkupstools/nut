6d6258533a4b24637164cd1336235e6e8cda2c23
Dimitris Economou <dimitris.s.economou@gmail.com>
2022-01-13 00:22:29 +0200

    modify regs_data memory, devreg enum rearrangement

--- drivers/adele_cbi.h
+++ drivers/adele_cbi.h
@@ -467,29 +467,29 @@ static alrm_ar_t obta = {
 
 /* UPS device reg enum */
 enum devreg {
-    CHRG = 0,       /* Charging status, "battery.charger.status" */
-    BATV,           /* Battery voltage, "battery.voltage" */
-    BCEF = 6,       /* Battery charge efficiency factor (CEF) */
-    BSOH,           /* Battery state-of-health */
-    BSOC = 9,       /* Battery state-of-charge, "battery.charge" */
-    BTMP = 11,      /* Battery temperature in Kelvin units, "battery.temperature" */
-    PMNG = 15,      /* Power management, "ups.status" */
-    OTMP = 20,      /* Onboard temperature, "ups.temperature" */
-    PRDN,           /* Product name, "ups.model" */
-    VAC  = 24,      /* AC voltage, "input.voltage" */
-    LVDC,           /* Load voltage, "output.voltage" */
-    LCUR,           /* Load current, "output.current" */
-    BINH = 79,      /* Backup inhibit */
-    FSD,            /* Force shutdown */
-    TBUF,           /* Time buffering, "battery.runtime" */
-    BSTA = 89,      /* Battery status alarms */
-    SCSH,           /* SoH and SoC alarms */
-    BVAL,           /* Battery voltage alarm */
-    BTSF,           /* Battery temp sensor failure */
-    DEVF,           /* Device failure */
-    OBTA,           /* On board temp alarm */
-    VACA,           /* VAC alarms */
-    MAIN            /* Mains status */
+    CHRG = 4,           /* Charging status, "battery.charger.status" */
+    BATV = 7,           /* Battery voltage, "battery.voltage" */
+    BCEF = 18,          /* Battery charge efficiency factor (CEF) */
+    BSOH = 20,          /* Battery state-of-health */
+    BSOC = 22,          /* Battery state-of-charge, "battery.charge" */
+    BTMP = 25,          /* Battery temperature in Kelvin units, "battery.temperature" */
+    PMNG = 5,           /* Power management, "ups.status" */
+    OTMP = 28,          /* Onboard temperature, "ups.temperature" */
+    PRDN = 67,          /* Product name, "ups.model" */
+    VAC  = 29,          /* AC voltage, "input.voltage" */
+    LVDC = 10,          /* Load voltage, "output.voltage" */
+    LCUR = 19,          /* Load current, "output.current" */
+    BINH = 87,          /* Backup inhibit */
+    FSD  = 40,          /* Force shutdown */
+    TBUF = 103,         /* Time buffering, "battery.runtime" */
+    BSTA = 31,          /* Battery status alarms */
+    SCSH,               /* SoH and SoC alarms */
+    BVAL = 34,          /* Battery voltage alarm */
+    BTSF = 43,          /* Battery temp sensor failure */
+    DEVF = 42,          /* Device failure */
+    OBTA = 46,          /* On board temp alarm */
+    VACA = 44,          /* VAC alarms */
+    MAIN                /* Mains status */
 };
 typedef enum devreg devreg_t;
 
@@ -519,6 +519,7 @@ static uint16_t regs_data[MAX_REGS];
 
 /* ADELE CBI registers */
 static regattr_t regs[] = {
+<<<<<<< HEAD
 =======
 =======
 
@@ -885,6 +886,134 @@ static regattr_t regs[] = {
 <<<<<<< HEAD
          {40046, 0, 0, 1, HOLDING},   /* Mains status */
          {40038, 0, 0, 1, HOLDING}    /* Load alarm */
+=======
+         {40001, 0, 0, 1, HOLDING},     /* Address of slave unit */
+         {40002, 0, 0, 1, HOLDING},     /* Baud rate for serial communication */
+         {40003, 0, 0, 1, HOLDING},     /* Parity bit for serial communication */
+         {40004, 0, 0, 1, HOLDING},
+         {40005, 0, 0, 1, HOLDING},     /* Charging status */
+         {40006, 0, 0, 1, HOLDING},     /* Power management
+                                         * 0:Backup 1:Charging 2:boost 3:Not charging
+                                         */
+         {40007, 0, 0, 1, HOLDING},     /* Nominal output voltage */
+         {40008, 0, 0, 1, HOLDING},     /* Battery voltage */
+         {40009, 0, 0, 1, HOLDING},     /* Parameter map version ID */
+         {40010, 0, 0, 1, HOLDING},     /* Software ID */
+         {40011, 0, 0, 1, HOLDING},     /* Output load voltage */
+         {40012, 0, 0, 1, HOLDING}, 
+         {40013, 0, 0, 1, HOLDING}, 
+         {40014, 0, 0, 1, HOLDING},     /* Battery charge current */
+         {40015, 0, 0, 1, HOLDING}, 
+         {40016, 0, 0, .1, HOLDING},    /* Battery capacity consumed */
+         {40017, 0, 0, 1, HOLDING},     /* Battery discharge current */
+         {40018, 0, 0, .1, HOLDING},    /* Effective battery capacity */
+         {40019, 0, 0, 1, HOLDING},     /* Battery charge efficiency factor (CEF) */
+         {40020, 0, 0, 1, HOLDING},     /* Output load current */
+         {40021, 0, 0, 1, HOLDING},     /* Battery state-of-health */
+         {40022, 0, 0, 1, HOLDING},     /* Time remaining to 100% discharge */
+         {40023, 0, 0, .1, HOLDING},    /* Battery state-of-charge */
+         {40024, 0, 0, 1, HOLDING},     /* Battery type currently selected */
+         {40025, 0, 0, 1, HOLDING}, 
+         {40026, 0, 0, 1, HOLDING},     /* Battery temperature in Kelvin units */
+         {40027, 0, 0, 1, HOLDING},     /* Configuration mode */
+         {40028, 0, 0, .1, HOLDING},    /* Battery net internal resistance */
+         {40029, 0, 0, 1, HOLDING},     /* On-board temperature */
+         {40030, 0, 0, 1, HOLDING},     /* AC input voltage */
+         {40031, 0, 0, 1, HOLDING}, 
+         {40032, 0, 0, 1, HOLDING},     /* Battery status alarm */
+         {40033, 0, 0, 1, HOLDING},     /* Battery State of Charge and State of Health */
+         {40034, 0, 0, 1, HOLDING},     /* Load output off duration after PC shutdown */
+         {40035, 0, 0, 1, HOLDING},     /* Battery voltage alarm */
+         {40036, 0, 0, 1, HOLDING},     /* Low AC input voltage alarm threshold */
+         {40037, 0, 0, 1, HOLDING},     /* High AC input voltage alarm threshold */
+         {40038, 0, 0, 1, HOLDING},     /* Load alarm */         
+         {40039, 0, 0, 1, HOLDING},     /* Device variant */
+         {40040, 0, 0, 1, HOLDING}, 
+         {40041, 0, 0, 1, HOLDING},     /* Force shutdown */
+         {40042, 0, 0, 1, HOLDING}, 
+         {40043, 0, 0, 1, HOLDING},     /* Device failure */
+         {40044, 0, 0, 1, HOLDING},     /* Battery temperature sensor failure */
+         {40045, 0, 0, 1, HOLDING},     /* AC input voltage alarm */
+         {40046, 0, 0, 1, HOLDING},     /* Mains status */
+         {40047, 0, 0, 1, HOLDING},     /* On board temperature alarm */
+         {40048, 0, 0, 1, HOLDING},     /* Number of charge cycles completed */
+         {40049, 0, 0, 1, HOLDING},     /* Charge cycles not completed */
+         {40050, 0, 0, .1, HOLDING},    /* Ah charged */
+         {40051, 0, 0, 1, HOLDING},     /* Total run time */
+         {40052, 0, 0, 1, HOLDING},     /* Number of low battery voltage events */
+         {40053, 0, 0, 1, HOLDING},     /* Number of high battery voltage events */
+         {40054, 0, 0, 1, HOLDING},     /* Number of low VAC events at mains input */
+         {40055, 0, 0, 1, HOLDING},     /* Number of High VAC events at mains input */
+         {40056, 0, 0, 1, HOLDING},     /* Number of over temperature inside events */
+         {40057, 0, 0, 1, HOLDING},     /* Number of mains-backup transitions */
+         {40058, 0, 0, 1, HOLDING},     /* Number power boost events */
+         {40059, 0, 0, 1, HOLDING},     /* Highest battery voltage */
+         {40060, 0, 0, 1, HOLDING},     /* Highest output load voltage */
+         {40061, 0, 0, .1, HOLDING},    /* Maximum depth of discharge */
+         {40062, 0, 0, 1, HOLDING},     /* Lowest battery voltage */
+         {40063, 0, 0, 1, HOLDING},     /* Lowest output load voltage */
+         {40064, 0, 0, .1, HOLDING},    /* Average depth of discharge */
+         {40065, 0, 0, 1, HOLDING},     /* History clear all */
+         {40066, 0, 0, 1, HOLDING},     /* Factory settings */
+         {40067, 0, 0, 1, HOLDING},     /* Product name */
+         {40068, 0, 0, 1, HOLDING}, 
+         {40069, 0, 0, 1, HOLDING},     /* Reset internal battery model */         
+         {40070, 0, 0, 1, HOLDING}, 
+         {40071, 0, 0, 1, HOLDING},     /* Deep discharge battery prevention */
+         {40072, 0, 0, 1, HOLDING},     /* Maximum charge current */
+         {40073, 0, 0, 1, HOLDING},     /* Bulk voltage */
+         {40074, 0, 0, 1, HOLDING},     /* Max bulk timer */
+         {40075, 0, 0, 1, HOLDING},     /* Min bulk timer */
+         {40076, 0, 0, 1, HOLDING},     
+         {40077, 0, 0, 1, HOLDING},     /* Absorption voltage */
+         {40078, 0, 0, 1, HOLDING},     /* Max absorption timer */
+         {40079, 0, 0, 1, HOLDING},     /* Min absorption timer */
+         {40080, 0, 0, 1, HOLDING},     /* Return Amperes to float */
+         {40081, 0, 0, 1, HOLDING},     /* Return amps timer */
+         {40082, 0, 0, 1, HOLDING},     /* Float voltage */
+         {40083, 0, 0, 1, HOLDING},     /* Force boost charge */
+         {40084, 0, 0, 1, HOLDING},     /* Return to bulk voltage from float */
+         {40085, 0, 0, 1, HOLDING},     /* Return to bulk delay */
+         {40086, 0, 0, 1, HOLDING},     
+         {40087, 0, 0, 1, HOLDING},     /* Switchoff voltage without mains */
+         {40088, 0, 0, 1, HOLDING},     /* Backup Inhibit 0 = Backup allowed
+                                         *                1 = Backup not allowed
+                                         */ 
+         {40089, 0, 0, 1, HOLDING},     /* Number of battery cells */
+         {40090, 0, 0, 1, HOLDING},     /* Temperature compensation coefficient */
+         {40091, 0, 0, 1, HOLDING}, 
+         {40092, 0, 0, 1, HOLDING},     /* Lifetest enable */
+         {40093, 0, 0, 1, HOLDING},     /* Max alarm temp */
+         {40094, 0, 0, 1, HOLDING},     /* Min alarm temp */
+         {40095, 0, 0, 1, HOLDING}, 
+         {40096, 0, 0, 1, HOLDING}, 
+         {40097, 0, 0, 1, HOLDING},     /* Low battery threshold */         
+         {40098, 0, 0, 1, HOLDING},     /* SoC/SoH test period */
+         {40099, 0, 0, 1, HOLDING},     /* Manual SoC/SoH test request */
+         {40100, 0, 0, 1, HOLDING},     /* SoC/SoH test possible */
+         {40101, 0, 0, .1, HOLDING},    /* Nominal battery internal resistance */
+         {40102, 0, 0, .1, HOLDING},    /* Nominal battery cables resistance */
+         {40103, 0, 0, 1, HOLDING},     /* Firmware ID */
+         {40104, 0, 0, 1, HOLDING},     /* Time buffering */
+         {40105, 0, 0, .1, HOLDING},    /* Battery capacity C20 */
+         {40106, 0, 0, .1, HOLDING},    /* Battery Capacity C10 */
+         {40107, 0, 0, 1, HOLDING},     /* Device switchoff delay */
+         {40108, 0, 0, .1, HOLDING},    /* Battery Capacity C5 */
+         {40109, 0, 0, .1, HOLDING},    /* Battery Capacity C2 */
+         {40110, 0, 0, 1, HOLDING},     
+         {40111, 0, 0, 1, HOLDING},     /* PC power supply removal delay */
+         {40112, 0, 0, .1, HOLDING},    /* Battery Capacity C1 */
+         {40113, 0, 0, .1, HOLDING},    /* Low state-of-charge */
+         {40114, 0, 0, 1, HOLDING},     
+         {40115, 0, 0, 1, HOLDING}, 
+         {40116, 0, 0, 1, HOLDING}, 
+         {40117, 0, 0, 1, HOLDING}, 
+         {40118, 0, 0, 1, HOLDING}, 
+         {40119, 0, 0, 1, HOLDING}, 
+         {40120, 0, 0, 1, HOLDING}      /* Zero-SoC reference */
+
+
+>>>>>>> modify regs_data memory, devreg enum rearrangement
 };
 =======
          {40046, 0, 0, 1, HOLDING},   /* Input mains on / backup */
