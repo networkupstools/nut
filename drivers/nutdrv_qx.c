111da7410f3286eefa0555f80d1602562faee0a2
Jim Klimov <jimklimov+nut@gmail.com>
2022-03-19 15:29:29 +0100

    drivers/nutdrv_qx.c: snr_command(): adapt to usb_ctrl_charbuf

--- drivers/nutdrv_qx.c
+++ drivers/nutdrv_qx.c
@@ -1571,7 +1571,9 @@ static int	snr_command(const char *cmd, char *buf, size_t buflen)
 	 * Without the interrupt UPS returns zeros for some time,
 	 * and afterwards NUT returns a communications error.
 	 */
-	usb_interrupt_read(udev, 0x81, buf, 102, 1000);
+	usb_interrupt_read(udev,
+		0x81,
+		(usb_ctrl_charbuf)buf, 102, 1000);
 
 	for (i = 0; command[i].str; i++) {
 
