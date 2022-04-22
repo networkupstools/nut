07ae174b6c3f7d7eaeeea0427eff4ca869c64c55
Jim Klimov <jimklimov+nut@gmail.com>
2022-04-22 23:45:29 +0000

    tests/NIT/nit.sh: testcase_sandbox_upsc_query_timer(): do not waste time if we did get the answer we want

--- tests/NIT/nit.sh
+++ tests/NIT/nit.sh
@@ -582,9 +582,17 @@ testcase_sandbox_upsc_query_timer() {
     log_info "Test that dummy-ups TIMER action changes the reported state"
     # Driver is set up to flip ups.status every 5 sec, so check every 3
     OUT1="`upsc dummy@localhost:$NUT_PORT ups.status`" || die "upsd does not respond: $OUT1" ; sleep 3
-    OUT2="`upsc dummy@localhost:$NUT_PORT ups.status`" || die "upsd does not respond: $OUT2" ; sleep 3
-    OUT3="`upsc dummy@localhost:$NUT_PORT ups.status`" || die "upsd does not respond: $OUT3" ; sleep 3
-    OUT4="`upsc dummy@localhost:$NUT_PORT ups.status`" || die "upsd does not respond: $OUT4" ; sleep 3
+    OUT2="`upsc dummy@localhost:$NUT_PORT ups.status`" || die "upsd does not respond: $OUT2"
+    OUT3=""
+    OUT4=""
+    if [ x"$OUT1" = x"$OUT2" ]; then
+        sleep 3
+        OUT3="`upsc dummy@localhost:$NUT_PORT ups.status`" || die "upsd does not respond: $OUT3"
+        if [ x"$OUT2" = x"$OUT3" ]; then
+            sleep 3
+            OUT4="`upsc dummy@localhost:$NUT_PORT ups.status`" || die "upsd does not respond: $OUT4"
+        fi
+    fi
     if echo "$OUT1$OUT2$OUT3$OUT4" | grep "OB" && echo "$OUT1$OUT2$OUT3$OUT4" | grep "OL" ; then
         log_info "OK, ups.status flips over time"
         PASSED="`expr $PASSED + 1`"
