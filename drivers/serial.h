6897c67c0fd07f8d958f4bae5889bcd64cf79b26
Jim Klimov <jimklimov+nut@gmail.com>
2022-05-20 13:19:28 +0200

    drivers/serial.h: include config.h first

--- drivers/serial.h
+++ drivers/serial.h
@@ -1,9 +1,9 @@
 #ifndef SERIAL_H_SEEN
 #define SERIAL_H_SEEN 1
 
-#include "attribute.h"
+#include "config.h"	/* should be first */
 
-#include "config.h"
+#include "attribute.h"
 
 #if defined(HAVE_SYS_TERMIOS_H)
 #  include <sys/termios.h>      /* for speed_t */
