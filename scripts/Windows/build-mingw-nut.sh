6f23a9ad8a6f472d662a0396365d065c2de5f0ca
Jim Klimov <jimklimov+nut@gmail.com>
2022-06-09 14:44:25 +0000

    scripts/Windows/README: document use of a SOURCEMODE and some other settings

--- scripts/Windows/build-mingw-nut.sh
+++ scripts/Windows/build-mingw-nut.sh
@@ -10,34 +10,60 @@ TOP_DIR=$WINDIR/../..
 BUILD_DIR=$WINDIR/nut_build
 INSTALL_DIR=$WINDIR/nut_install
 
-#VER_OPT=2.8.0.1
+# This should match the tarball and directory name,
+# if a stable version is used:
+[ -n "$VER_OPT" ] || VER_OPT=2.8.0
 DEBUG=true
 
 # default to 32bits build
+# Note: README specifies dependencies to pre-build and install;
+# those DLLs should correspond to same architecture selection
 cmd=all32
 if [ -n "$1" ] ; then
 	cmd=$1
 fi
 
+[ -n "$SOURCEMODE" ] || SOURCEMODE="out-of-tree"
+
+rm -rf $BUILD_DIR $INSTALL_DIR
+CONFIGURE_SCRIPT="./configure"
+case "$SOURCEMODE" in
+stable)
 # FIXME
 # Stable version (download the latest stable archive)
-#	if [ ! -f nut-$VER_OPT.tar.gz ] ; then
-#		wget http://www.networkupstools.org/source/2.6/nut-$VER_OPT.tar.gz
-#	fi
-#	rm -rf nut-$VER_OPT
-#	tar -xzf nut-$VER_OPT.tar.gz
-#	mv nut-$VER_OPT $BUILD_DIR
+	VER_OPT_SHORT="`echo "$VER_OPT" | awk -F. '{print $1"."$2}'`"
+	if [ ! -s nut-$VER_OPT.tar.gz ] ; then
+		wget http://www.networkupstools.org/source/$VER_OPT_SHORT/nut-$VER_OPT.tar.gz
+	fi
+	rm -rf nut-$VER_OPT
+	tar -xzf nut-$VER_OPT.tar.gz
+	mv nut-$VER_OPT $BUILD_DIR
+	;;
+dist)
+	# In-place version (no download)
+	cd ../..
+	rm -f nut-?.?.?*.tar.gz
+	[ -s Makefile ] || { ./autogen.sh && ./configure; }
+	make dist
+	SRC_ARCHIVE=$(ls -1 nut-?.?.?*.tar.gz | sort -n | tail -1)
+	cd scripts/Windows
+	tar -xzf ../../$SRC_ARCHIVE
+	mv nut-?.?.?* $BUILD_DIR
+	;;
+out-of-tree)
+	CONFIGURE_SCRIPT="../../../configure"
+	cd ../..
+	if [ ! -x ./configure ]; then
+		./autogen.sh
+	fi
+	if [ -s Makefile ]; then
+		make distclean
+	fi
+	cd scripts/Windows
+	mkdir -p $BUILD_DIR
+	;;
+esac
 
-# In-place version (no download)
-rm -rf $BUILD_DIR $INSTALL_DIR
-cd ../..
-rm -f nut-?.?.?*.tar.gz
-[ -s Makefile ] || { ./autogen.sh && ./configure; }
-make dist
-SRC_ARCHIVE=$(ls -1 nut-?.?.?*.tar.gz | sort -n | tail -1)
-cd scripts/Windows
-tar -xzf ../../$SRC_ARCHIVE
-mv nut-?.?.?* $BUILD_DIR
 cd $BUILD_DIR || exit
 
 if [ "$cmd" == "all64" ] || [ "$cmd" == "b64" ] || [ "$cmd" == "all32" ] || [ "$cmd" == "b32" ] ; then
@@ -56,11 +82,12 @@ if [ "$cmd" == "all64" ] || [ "$cmd" == "b64" ] || [ "$cmd" == "all32" ] || [ "$
 
 	export CFLAGS+="-D_POSIX=1 -I/usr/$ARCH/include/"
 	export LDFLAGS+="-L/usr/$ARCH/lib/"
-	./configure $HOST_FLAG $BUILD_FLAG --prefix=$INSTALL_DIR \
+	$CONFIGURE_SCRIPT $HOST_FLAG $BUILD_FLAG --prefix=$INSTALL_DIR \
 	    --without-pkg-config --with-all=auto \
-	    --without-systemdsystemunitdir
-	make 1>/dev/null
-	make install
+	    --without-systemdsystemunitdir \
+	|| exit
+	make 1>/dev/null || exit
+	make install || exit
 	# Be sure upsmon can run even if at cost of some duplication
 	# (maybe even do "cp -pf" if some system dislikes "ln"); also
 	# on a modern Windows one could go to their installed "sbin" to
@@ -75,4 +102,5 @@ else
 	echo "Usage:"
 	echo "		$0 [all64 | b64 | all32 | b32]"
 	echo "		Default: 'all32'"
+	echo "Optionally export SOURCEMODE=[stable|dist|out-of-tree]"
 fi
