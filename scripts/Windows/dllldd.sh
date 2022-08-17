eff9eaa744e8c069bcbad7959d1a8c5e268a0ec8
Jim Klimov <jimklimov+nut@gmail.com>
2022-08-17 15:09:01 +0200

    scripts/Windows/dllldd.sh: fallback support for MSYS2 path search [#1492]

--- scripts/Windows/dllldd.sh
+++ scripts/Windows/dllldd.sh
@@ -15,17 +15,33 @@ dllldd() {
 	# which are provided in the cross-build env (not system ones).
 	# Assume no whitespaces in paths and filenames of interest.
 
-	# if `ldd` handles Windows PE, we are lucky:
+	# if `ldd` handles Windows PE (e.g. on MSYS2), we are lucky:
 	#         libiconv-2.dll => /mingw64/bin/libiconv-2.dll (0x7ffd26c90000)
 	OUT="`ldd "$1" 2>/dev/null | grep -Ei '\.dll' | grep -E '/(bin|lib)/' | sed "s,^${REGEX_WS}*\(${REGEX_NOT_WS}${REGEX_NOT_WS}*\)${REGEX_WS}${REGEX_WS}*=>${REGEX_WS}${REGEX_WS}*\(${REGEX_NOT_WS}${REGEX_NOT_WS}*\)${REGEX_WS}.*\$,\2,"`" \
 	&& [ -n "$OUT" ] && { echo "$OUT" ; return 0 ; }
 
-	# Otherwise try objdump
-	for OD in objdump "$ARCH-objdump" ; do
-		(command -v "$OD" >/dev/null 2>/dev/null) || continue
-		OUT="`$OD -x "$1" 2>/dev/null | grep -Ei "DLL Name:" | awk '{print $NF}' | while read F ; do ls -1 "/usr/$ARCH/"{bin,lib}/"$F" 2>/dev/null || true ; done`" \
-		&& [ -n "$OUT" ] && { echo "$OUT" ; return 0 ; }
-	done
+	# Otherwise try objdump, if ARCH is known (linux+mingw builds) or not (MSYS2 builds)
+	if [ -n "${ARCH-}${MINGW_PREFIX-}${MSYSTEM_PREFIX-}" ] ; then
+		for OD in objdump "$ARCH-objdump" ; do
+			(command -v "$OD" >/dev/null 2>/dev/null) || continue
+
+			ODOUT="`$OD -x "$1" 2>/dev/null | grep -Ei "DLL Name:" | awk '{print $NF}'`" \
+			&& [ -n "$ODOUT" ] || continue
+
+			if [ -n "$ARCH" ] ; then
+				OUT="`for F in $ODOUT ; do ls -1 "/usr/${ARCH}/"{bin,lib}/"$F" 2>/dev/null || true ; done`" \
+				&& [ -n "$OUT" ] && { echo "$OUT" ; return 0 ; }
+			fi
+			if [ -n "$MSYSTEM_PREFIX" ] ; then
+				OUT="`for F in $ODOUT ; do ls -1 "${MSYSTEM_PREFIX}/"{bin,lib}/"$F" 2>/dev/null || true ; done`" \
+				&& [ -n "$OUT" ] && { echo "$OUT" ; return 0 ; }
+			fi
+			if [ -n "$MINGW_PREFIX" ] && [ "$MINGW_PREFIX" != "$MSYSTEM_PREFIX" ] ; then
+				OUT="`for F in $ODOUT ; do ls -1 "${MINGW_PREFIX}/"{bin,lib}/"$F" 2>/dev/null || true ; done`" \
+				&& [ -n "$OUT" ] && { echo "$OUT" ; return 0 ; }
+			fi
+		done
+	fi
 
 	return 1
 }
