6b152b7a789b54c5e4231d0a6c374a07eb5b9bfa
Jim Klimov <jimklimov@gmail.com>
2023-03-05 20:28:48 +0100

    Update libusb1.c
    Change to conventional error-message format

--- drivers/libusb1.c
+++ drivers/libusb1.c
@@ -412,7 +412,7 @@ static int nut_libusb_open(libusb_device_handle **udevp,
 				upsdebugx(2, "successfully set kernel driver auto-detach flag");
 			}
 		} else {
-			upsdebugx(3, "libusb_kernel_driver_active() returned %d (%s)",
+			upsdebugx(3, "libusb_kernel_driver_active() returned %d: %s",
 				ret, libusb_strerror((enum libusb_error)ret));
 		}
 #endif
