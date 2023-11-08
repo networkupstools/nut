b9f915549b454aea38edf6bf03f06536b006d981
Jim Klimov <jimklimov+nut@gmail.com>
2023-11-08 12:10:55 +0100

    scripts/python/module/setup.py: note the renaming of README.adoc
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- scripts/python/module/setup.py
+++ scripts/python/module/setup.py
@@ -10,7 +10,7 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+with codecs.open(os.path.join(here, "README.adoc"), encoding="utf-8") as fh:
     long_description = "\\n" + fh.read()
 
 setup(
