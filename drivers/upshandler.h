7a8430dc160bd5821a3a2f9e09dd739ce9164fc5
Axel Gembe <axel@gembe.net>
2023-11-14 01:47:38 +0700

    apc_modbus: Updates, command, writable variables and outlet group support
    This adds support for commands, writable variables and outlet groups.
    
    - For commands there is a new table called `apc_modbus_command_map`
      which defines the supported commands as a tuple of command name,
      register offset and value to write. This also adds a new instcmd
      status called `STAT_INSTCMD_CONVERSION_FAILED` for when conversion
      of values fails. On startup all the commands are registered using
      `dstate_addcmd`.
      This also adds support for the `upsdrv_shutdown` function.
    - For writable variables we added a new flag called `APC_VF_RW` to the
      existing variables that indicates a writable variable. We added code
      to convert from a string to UINT/INT/STRING variables with output in
      APCs register format. There is a new `_apc_modbus_setvar` function
      that handles setting variables and rereading them from the device.
      This also adds a new setvar status called `STAT_SET_CONVERSION_FAILED`
      for when conversion of values fails. Variables are now correctly set
      as `ST_FLAG_STRING` and `ST_FLAG_RW` and we call `dstate_setaux` to
      give a maximum length for strings.
    - For outlet groups, we now have names, configurable delays and commands
      per outlet group. Devices have an outlet group called MOG (Main outlet
      group) that switches all the outputs of the UPS and 1-3 SOGs (Switched
      outlet groups) that can be controlled independently. Note that MOG is
      `outlet.group.0` and the SOGs start at `outlet.group.1`. The outlet
      groups should have markings with the same index at the back of the
      unit.
    - This also reduces the length of some of the defines to make the
      variable maps more readable.
    - It also adds a comment to when the reopen matcher is created that
      clarifies why we create it.
    
    Signed-off-by: Axel Gembe <axel@gembe.net>

--- drivers/upshandler.h
+++ drivers/upshandler.h
@@ -22,18 +22,20 @@
 
 /* return values for instcmd */
 enum {
-	STAT_INSTCMD_HANDLED = 0,	/* completed successfully */
-	STAT_INSTCMD_UNKNOWN,		/* unspecified error */
-	STAT_INSTCMD_INVALID,		/* invalid command */
-	STAT_INSTCMD_FAILED		/* command failed */
+	STAT_INSTCMD_HANDLED = 0,		/* completed successfully */
+	STAT_INSTCMD_UNKNOWN,			/* unspecified error */
+	STAT_INSTCMD_INVALID,			/* invalid command */
+	STAT_INSTCMD_FAILED,			/* command failed */
+	STAT_INSTCMD_CONVERSION_FAILED	/* could not convert value */
 };
 
 /* return values for setvar */
 enum {
-	STAT_SET_HANDLED = 0,	/* completed successfully */
-	STAT_SET_UNKNOWN,	/* unspecified error */
-	STAT_SET_INVALID,	/* not writeable */
-	STAT_SET_FAILED		/* writing failed */
+	STAT_SET_HANDLED = 0,			/* completed successfully */
+	STAT_SET_UNKNOWN,				/* unspecified error */
+	STAT_SET_INVALID,				/* not writeable */
+	STAT_SET_FAILED,				/* writing failed */
+	STAT_SET_CONVERSION_FAILED		/* could not convert value from string */
 };
 
 /* structure for funcs that get called by msg parse routine */
