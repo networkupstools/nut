8ee9cbca34c4202df76b7746ca55e75a6cae10d1
Axel Gembe <axel@gembe.net>
2023-11-23 00:56:09 +0700

    apc_modbus: Add ups.test.result
    This outputs the test result, the source of the start of the test and
    a result modifier.
    
    Signed-off-by: Axel Gembe <axel@gembe.net>

--- drivers/apc_modbus.c
+++ drivers/apc_modbus.c
@@ -654,6 +654,94 @@ static int _apc_modbus_status_change_cause_to_nut(const apc_modbus_value_t *valu
 
 static apc_modbus_converter_t _apc_modbus_status_change_cause_conversion = { _apc_modbus_status_change_cause_to_nut, NULL };
 
+static int _apc_modbus_string_join(const char *values[], size_t values_len, const char *separator, char *output, size_t output_len)
+{
+	size_t i;
+	size_t output_idx;
+	int res;
+
+	if (values == NULL || values_len == 0 || separator == NULL || output == NULL || output_len == 0) {
+		/* Invalid parameters */
+		return 0;
+	}
+
+	output_idx = 0;
+
+	for (i = 0; i < values_len && output_idx < output_len; i++) {
+		if (values[i] == NULL)
+			continue;
+
+		if (i == 0) {
+			res = snprintf(output + output_idx, output_len - output_idx, "%s", values[i]);
+		} else {
+			res = snprintf(output + output_idx, output_len - output_idx, "%s%s", separator, values[i]);
+		}
+
+		if (res < 0 || (size_t)res >= output_len) {
+			return 0;
+		}
+
+		output_idx += res;
+	}
+
+	return 1;
+}
+
+static int _apc_modbus_battery_test_status_to_nut(const apc_modbus_value_t *value, char *output, size_t output_len)
+{
+	const char *result, *source, *modifier;
+	const char *values[3];
+
+	if (value == NULL || output == NULL || output_len == 0) {
+		/* Invalid parameters */
+		return 0;
+	}
+
+	if (value->type != APC_VT_UINT) {
+		return 0;
+	}
+
+	result = NULL;
+	if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_PENDING)) {
+		result = "Pending";
+	} else if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_INPROGRESS)) {
+		result = "InProgress";
+	} else if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_PASSED)) {
+		result = "Passed";
+	} else if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_FAILED)) {
+		result = "Failed";
+	} else if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_REFUSED)) {
+		result = "Refused";
+	} else if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_ABORTED)) {
+		result = "Aborted";
+	}
+
+	source = NULL;
+	if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_SOURCE_PROTOCOL)) {
+		source = "Source: Protocol";
+	} else if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_SOURCE_LOCALUI)) {
+		source = "Source: LocalUI";
+	} else if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_SOURCE_INTERNAL)) {
+		source = "Source: Internal";
+	}
+
+	modifier = NULL;
+	if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_MOD_INVALIDSTATE)) {
+		modifier = "Modifier: InvalidState";
+	} else if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_MOD_INTERNALFAULT)) {
+		modifier = "Modifier: InternalFault";
+	} else if ((value->data.uint_value & APC_MODBUS_REPLACEBATTERYTESTSTATUS_BF_MOD_STATEOFCHARGENOTACCEPTABLE)) {
+		modifier = "Modifier: StateOfChargeNotAcceptable";
+	}
+
+	values[0] = result;
+	values[1] = source;
+	values[2] = modifier;
+	return _apc_modbus_string_join(values, SIZEOF_ARRAY(values), ", ", output, output_len);
+}
+
+static apc_modbus_converter_t _apc_modbus_battery_test_status_conversion = { _apc_modbus_battery_test_status_to_nut, NULL };
+
 static const time_t apc_date_start_offset = 946684800; /* 2000-01-01 00:00 */
 
 static int _apc_modbus_date_to_nut(const apc_modbus_value_t *value, char *output, size_t output_len)
@@ -735,6 +823,7 @@ static apc_modbus_register_t apc_modbus_register_map_inventory[] = {
 
 static apc_modbus_register_t apc_modbus_register_map_status[] = {
 	{ "input.transfer.reason",          2,      1,  APC_VT_UINT,     0,         &_apc_modbus_status_change_cause_conversion,    NULL,   0,  NULL    },
+	{ "ups.test.result",          		23,     1,  APC_VT_UINT,     0,         &_apc_modbus_battery_test_status_conversion,    NULL,   0,  NULL    },
 	{ NULL, 0, 0, 0, 0, NULL, NULL, 0.0f, NULL }
 };
 
