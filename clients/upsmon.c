3c3d006c900c17720ea8bd7c39e883da62152746
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-01-26 13:16:44 +0100

    clients/upsmon.c: fix offstate not being a requirement for a critical UPS on OFF
    Signed-off-by: desertwitch <24509509+desertwitch@users.noreply.github.com>

--- clients/upsmon.c
+++ clients/upsmon.c
@@ -1100,7 +1100,7 @@ static int is_ups_critical(utype_t *ups)
 
 	/* administratively OFF (long enough, see OFFDURATION) */
 	if (flag_isset(ups->status, ST_OFF) && offdurationtime >= 0
-	&& (ups->linestate == 0 || ups->offstate == 1)) {
+	&& ups->offstate == 1) {
 		upslogx(LOG_WARNING,
 			"UPS [%s] is reported as (administratively) OFF",
 			ups->sys);
