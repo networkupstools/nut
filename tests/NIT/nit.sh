349dfb4cc7b3f1d58f873dd5bdfc11a75c06cf20
Jim Klimov <jimklimov+nut@gmail.com>
2024-04-17 13:45:52 +0200

    tests/NIT/nit.sh: generatecfg_upsmon_trivial(): fix SHUTDOWNCMD to use the expanded NUT_STATEPATH value
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- tests/NIT/nit.sh
+++ tests/NIT/nit.sh
@@ -491,7 +491,7 @@ generatecfg_upsmon_trivial() {
     # Populate the configs for the run
     rm -f "$NUT_STATEPATH/upsmon.sd.log"
     (  echo 'MINSUPPLIES 0' > "$NUT_CONFPATH/upsmon.conf" || exit
-       echo 'SHUTDOWNCMD "echo \"`date`: TESTING_DUMMY_SHUTDOWN_NOW\" | tee \"$NUT_STATEPATH\"/upsmon.sd.log"' >> "$NUT_CONFPATH/upsmon.conf" || exit
+       echo 'SHUTDOWNCMD "echo \"`date`: TESTING_DUMMY_SHUTDOWN_NOW\" | tee \"'"$NUT_STATEPATH"'/upsmon.sd.log\" "' >> "$NUT_CONFPATH/upsmon.conf" || exit
 
        if [ -n "${NUT_DEBUG_MIN-}" ] ; then
            echo "DEBUG_MIN ${NUT_DEBUG_MIN}" >> "$NUT_CONFPATH/upsmon.conf" || exit
