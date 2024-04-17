df30787f8488e0b97df33bda03f18e98820a0aec
Jim Klimov <jimklimov+nut@gmail.com>
2024-04-17 11:24:46 +0200

    tests/NIT/nit.sh: add PATH into generated NIT.env
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- tests/NIT/nit.sh
+++ tests/NIT/nit.sh
@@ -361,10 +361,10 @@ log_info "Using NUT_PORT=${NUT_PORT} for this test run"
 # the values when fallback is used. If this is a
 # problem on any platform (Win/Mac and spaces in
 # paths?) please investigate and fix accordingly.
-set | grep -E '^(NUT_|LD_LIBRARY_PATH|DEBUG).*=' \
+set | grep -E '^(NUT_|LD_LIBRARY_PATH|DEBUG|PATH).*=' \
 | while IFS='=' read K V ; do
     case "$K" in
-        LD_LIBRARY_PATH_CLIENT|LD_LIBRARY_PATH_ORIG|NUT_PORT_*)
+        LD_LIBRARY_PATH_CLIENT|LD_LIBRARY_PATH_ORIG|PATH_*|NUT_PORT_*)
             continue
             ;;
     esac
