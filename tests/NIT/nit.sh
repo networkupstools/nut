dbcf1593b9dfe32bc45c693d79d4e0da183e7ceb
Jim Klimov <jimklimov+nut@gmail.com>
2024-08-21 15:15:02 +0200

    tests/NIT/nit.sh: export TESTPASS_* values to NIT.env [#2597]
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- tests/NIT/nit.sh
+++ tests/NIT/nit.sh
@@ -413,7 +413,7 @@ log_info "Using NUT_PORT=${NUT_PORT} for this test run"
 # the values when fallback is used. If this is a
 # problem on any platform (Win/Mac and spaces in
 # paths?) please investigate and fix accordingly.
-set | grep -E '^(NUT_|LD_LIBRARY_PATH|DEBUG|PATH).*=' \
+set | grep -E '^(NUT_|TESTPASS_|LD_LIBRARY_PATH|DEBUG|PATH).*=' \
 | while IFS='=' read K V ; do
     case "$K" in
         LD_LIBRARY_PATH_CLIENT|LD_LIBRARY_PATH_ORIG|PATH_*|NUT_PORT_*)
