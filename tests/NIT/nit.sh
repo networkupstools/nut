805a216d20e18fad1597928d4adf8f68e9ba950d
Jim Klimov <jimklimov+nut@gmail.com>
2024-09-01 13:43:26 +0200

    tests/NIT/nit.sh: check that /proc/net/tcp* are readable, not just existing
    Builds in Android with Termux lack the permissions to see open ports.
    
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- tests/NIT/nit.sh
+++ tests/NIT/nit.sh
@@ -110,7 +110,7 @@ isBusy_NUT_PORT() {
     [ -n "${NUT_PORT}" ] || return
 
     log_debug "isBusy_NUT_PORT() Trying to report if NUT_PORT=${NUT_PORT} is used"
-    if [ -e /proc/net/tcp ] || [ -e /proc/net/tcp6 ]; then
+    if [ -r /proc/net/tcp ] || [ -r /proc/net/tcp6 ]; then
         # Assume Linux - hex-encoded
         # IPv4:
         #   sl  local_address rem_address   st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode
