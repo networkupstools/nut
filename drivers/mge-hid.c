1c6c0e137c252ae5e25793e9296ffbf1ebfc2dc8
DaRK AnGeL <28630321+masterwishx@users.noreply.github.com>
2024-10-23 13:29:58 +0300

    typo in commment
    Signed-off-by: DaRK AnGeL <28630321+masterwishx@users.noreply.github.com>

--- drivers/mge-hid.c
+++ drivers/mge-hid.c
@@ -198,7 +198,7 @@ static char		mge_scratch_buf[20];
 
 /* Some untis like Eaton 9E Models that using "x.ChargerType=5" and "x.Status=2"
  * instead of other units that has ABM by "x.ABMEnable and "x.Mode".
- * NOTE: Eaton 5P logs unit has "x.ABMEnable=1 and "x.Mode=4","x.ChargerType=4" and "x.Status=19?" */
+ * NOTE: Eaton 5P unit has "x.ABMEnable=1 and "x.Mode=4","x.ChargerType=4" and "x.Status=19?" */
 #define			ABM_ENABLED_TYPE 4
 
 /* Define if we have battery.charger.type.status for 9E Models and others */
