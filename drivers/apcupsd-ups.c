c18e0aae3166e897476a68242cbe18c034557c47
Jim Klimov <jimklimov+nut@gmail.com>
2024-11-14 15:00:06 +0100

    drivers/apcupsd-ups.c: update to use "sdcommands" (if customized by user) [#2670]
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- drivers/apcupsd-ups.c
+++ drivers/apcupsd-ups.c
@@ -57,7 +57,7 @@ typedef struct pollfd {
 #include "nut_stdint.h"
 
 #define DRIVER_NAME	"apcupsd network client UPS driver"
-#define DRIVER_VERSION	"0.72"
+#define DRIVER_VERSION	"0.73"
 
 #define POLL_INTERVAL_MIN 10
 
@@ -364,8 +364,17 @@ void upsdrv_updateinfo(void)
 void upsdrv_shutdown(void)
 {
 	/* replace with a proper shutdown function */
-	upslogx(LOG_ERR, "shutdown not supported");
-	set_exit_flag(-1);
+
+	/* NOTE: User-provided commands may be something other
+	 * than actual shutdown, e.g. a beeper to test that the
+	 * INSTCMD happened such and when expected without
+	 * impacting the load fed by the UPS.
+	 */
+	if (loop_shutdown_commands(NULL, NULL) != STAT_INSTCMD_HANDLED) {
+		upslogx(LOG_ERR, "shutdown not supported");
+		/* FIXME: Should the UPS shutdown mean the driver shutdown? */
+		set_exit_flag(-1);
+	}
 }
 
 void upsdrv_help(void)
