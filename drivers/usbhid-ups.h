2da901205c83d322e4b6e317db610d43783a8261
DaRK AnGeL <28630321+masterwishx@users.noreply.github.com>
2024-11-20 20:09:02 +0200

    add ess mode
    Signed-off-by: DaRK AnGeL <28630321+masterwishx@users.noreply.github.com>

--- drivers/usbhid-ups.h
+++ drivers/usbhid-ups.h
@@ -144,6 +144,7 @@ typedef enum {
 	BYPASSAUTO,	/* on automatic bypass */
 	BYPASSMAN,	/* on manual/service bypass */
 	ECOMODE,    /* High Efficiency (aka ECO Mode) */
+	ESSMODE,    /* Energy Saver System */
 	OFF,		/* ups is off */
 	CALIB,		/* calibration */
 	OVERHEAT,	/* overheat; Belkin, TrippLite */
