559dca6f54c39d5197d6f4aaf68d274f0f0ef93f
Viktor Drobot <linux776@gmail.com>
2024-12-10 16:03:08 +0300

    Checked remaining classic Qx commands (beeper toggle removed as not recognized by hardware). Added firmware version and serial number of UPS. Reorganized readings to minimize data between host and UPS according to the developer docs. Added known input, output and bypass readings as comments for future development (3/3 versions).
    Signed-off-by: Viktor Drobot <linux776@gmail.com>

--- drivers/nutdrv_qx_innovart31.c
+++ drivers/nutdrv_qx_innovart31.c
@@ -43,38 +43,10 @@ static int innovart31_x1000(item_t *item, char *value, const size_t valuelen) {
 static item_t	innovart31_qx2nut[] = {
 
 	/*
-	 * > [Q1\r]
-	 * < [(226.0 195.0 226.0 014 49.0 27.5 30.0 00001000\r]
-	 *    01234567890123456789012345678901234567890123456
-	 *    0         1         2         3         4
-	 */
-
-	/*
 	 * > [Q6\r]
 	 * < [(227.0 225.6 230.0 50.0 229.9 000.0 000.0 49.9 007 000 000 327.8 000.0 23.0 06932 100 32 00000000 00000000 11\r]
 	 *    01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
 	 *    0         1         2         3         4         5         6         7         8         9         10
-	 */
-
-	/*
-	 * > [WA\r]
-	 * < [(001.4 000.0 000.0 001.4 000.0 000.0 001.4 001.4 006.5 000.0 000.0 007 00000000\r]
-	 *    01234567890123456789012345678901234567890123456789012345678901234567890123456789
-	 *    0         1         2         3         4         5         6         7
-	 */
-
-	/*
-	 * > [BPS\r]
-	 * < [(230.4 000.0 000.0 49.9\r]
-	 *    012345678901234567890123
-	 *    0         1         2
-	 */
-
-	/*
-	 * > [F\r]
-	 * < [#230.0 087 288.0 50.0\r]
-	 *    0123456789012345678901
-	 *    0         1         2
 	 */
 
 	/* Common parameters */
@@ -83,18 +55,82 @@ static item_t	innovart31_qx2nut[] = {
 	{ "input.L2-N.voltage",		0,	NULL,	"Q6\r",	"",	110,	'(',	"",	7,	11,	"%.1f",	0,	NULL,	NULL,	NULL },
 	{ "input.L3-N.voltage",		0,	NULL,	"Q6\r",	"",	110,	'(',	"",	13,	17,	"%.1f",	0,	NULL,	NULL,	NULL },
 	{ "input.frequency",		0,	NULL,	"Q6\r",	"",	110,	'(',	"",	19,	22,	"%.1f",	0,	NULL,	NULL,	NULL },
-	{ "input.bypass.voltage",		0,	NULL,	"BPS\r",	"",	24,	'(',	"",	1,	5,	"%.1f",	0,	NULL,	NULL,	NULL },
-	{ "input.bypass.frequency",		0,	NULL,	"BPS\r",	"",	24,	'(',	"",	19,	22,	"%.1f",	0,	NULL,	NULL,	NULL },
 	{ "output.voltage",		0,	NULL,	"Q6\r",	"",	110,	'(',	"",	24,	28,	"%.1f",	0,	NULL,	NULL,	NULL },
-	{ "output.current",		0,	NULL,	"WA\r",	"",	80,	'(',	"",	49,	53,	"%.1f",	0,	NULL,	NULL,	NULL },
+/*	{ "output.L1-N.voltage",		0,	NULL,	"Q6\r",	"",	110,	'(',	"",	24,	28,	"%.1f",	0,	NULL,	NULL,	NULL },
+	{ "output.L2-N.voltage",		0,	NULL,	"Q6\r",	"",	110,	'(',	"",	30,	34,	"%.1f",	0,	NULL,	NULL,	NULL },
+	{ "output.L3-N.voltage",		0,	NULL,	"Q6\r",	"",	110,	'(',	"",	36,	40,	"%.1f",	0,	NULL,	NULL,	NULL },*/
 	{ "output.frequency",		0,	NULL,	"Q6\r",	"",	110,	'(',	"",	42,	45,	"%.1f",	0,	NULL,	NULL,	NULL },
 	{ "ups.load",			0,	NULL,	"Q6\r",	"",	110,	'(',	"",	47,	49,	"%.0f",	0,	NULL,	NULL,	NULL },
-	{ "ups.realpower",		0,	NULL,	"WA\r",	"",	80,	'(',	"",	37,	41,	"%.1f",	0,	NULL,	NULL,	innovart31_x1000 },
-	{ "ups.power",		0,	NULL,	"WA\r",	"",	80,	'(',	"",	43,	47,	"%.1f",	0,	NULL,	NULL,	innovart31_x1000 },
 	{ "ups.temperature",		0,	NULL,	"Q6\r",	"",	110,	'(',	"",	71,	74,	"%.1f",	0,	NULL,	NULL,	NULL },
 	{ "battery.voltage",		0,	NULL,	"Q6\r",	"",	110,	'(',	"",	59,	63,	"%.2f",	0,	NULL,	NULL,	qx_multiply_battvolt },
 	{ "battery.runtime",			0,	NULL,	"Q6\r",	"",	110,	'(',	"",	76,	80,	"%.0f",	0,	NULL,	NULL,	NULL },
 	{ "battery.charge",			0,	NULL,	"Q6\r",	"",	110,	'(',	"",	82,	84,	"%.0f",	0,	NULL,	NULL,	NULL },
+
+	/*
+	 * > [WA\r]
+	 * < [(001.4 000.0 000.0 001.4 000.0 000.0 001.4 001.4 006.5 000.0 000.0 007 00000000\r]
+	 *    01234567890123456789012345678901234567890123456789012345678901234567890123456789
+	 *    0         1         2         3         4         5         6         7
+	 */
+
+	/* Output consumption parameters */
+	{ "output.current",		0,	NULL,	"WA\r",	"",	80,	'(',	"",	49,	53,	"%.1f",	0,	NULL,	NULL,	NULL },
+	{ "ups.realpower",		0,	NULL,	"WA\r",	"",	80,	'(',	"",	37,	41,	"%.1f",	0,	NULL,	NULL,	innovart31_x1000 },
+	{ "ups.power",		0,	NULL,	"WA\r",	"",	80,	'(',	"",	43,	47,	"%.1f",	0,	NULL,	NULL,	innovart31_x1000 },
+
+	/*
+	 * > [BPS\r]
+	 * < [(230.4 000.0 000.0 49.9\r]
+	 *    012345678901234567890123
+	 *    0         1         2
+	 */
+
+	/* Bypass parameters */
+	{ "input.bypass.voltage",		0,	NULL,	"BPS\r",	"",	24,	'(',	"",	1,	5,	"%.1f",	0,	NULL,	NULL,	NULL },
+/*	{ "input.bypass.L1-N.voltage",		0,	NULL,	"BPS\r",	"",	24,	'(',	"",	1,	5,	"%.1f",	0,	NULL,	NULL,	NULL },
+	{ "input.bypass.L2-N.voltage",		0,	NULL,	"BPS\r",	"",	24,	'(',	"",	7,	11,	"%.1f",	0,	NULL,	NULL,	NULL },
+	{ "input.bypass.L3-N.voltage",		0,	NULL,	"BPS\r",	"",	24,	'(',	"",	13,	17,	"%.1f",	0,	NULL,	NULL,	NULL },*/
+	{ "input.bypass.frequency",		0,	NULL,	"BPS\r",	"",	24,	'(',	"",	19,	22,	"%.1f",	0,	NULL,	NULL,	NULL },
+
+	/*
+	 * > [F\r]
+	 * < [#230.0 087 288.0 50.0\r]
+	 *    0123456789012345678901
+	 *    0         1         2
+	 */
+
+	/* Nominal parameters (ratings) */
+	{ "input.voltage.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	1,	5,	"%.0f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
+	{ "input.current.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	7,	9,	"%.1f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
+	{ "battery.voltage.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	11,	15,	"%.1f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
+	{ "input.frequency.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	17,	20,	"%.0f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
+
+	/*
+	 * > [FW?\r]
+	 * < [05516-1100\r]
+	 *    01234567890
+	 *    0         1
+	 */
+
+	/* Firmware version */
+	{ "ups.firmware",	0,	NULL,	"FW?\r",	"",	0,	0,	"",	0,	0,	"%s",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
+
+	/*
+	 * > [SASV07?\r]
+	 * < [GASV07DN05K10KB100004      \r]
+	 *    0123456789012345678901234567
+	 *    0         1         2
+	 */
+
+	/* UPS serial number */
+	{ "ups.serial",	0,	NULL,	"SASV07?\r",	"",	28,	0,	"",	7,	0,	"%s",	QX_FLAG_STATIC | QX_FLAG_TRIM,	NULL,	NULL,	NULL },
+
+	/*
+	 * > [Q1\r]
+	 * < [(226.0 195.0 226.0 014 49.0 27.5 30.0 00001000\r]
+	 *    01234567890123456789012345678901234567890123456
+	 *    0         1         2         3         4
+	 */
 
 	/* Status bits */
 	{ "ups.status",			0,	NULL,	"Q1\r",	"",	47,	'(',	"",	38,	38,	NULL,	QX_FLAG_QUICK_POLL,	NULL,	NULL,	blazer_process_status_bits },	/* Utility Fail (Immediate) */
@@ -106,14 +142,8 @@ static item_t	innovart31_qx2nut[] = {
 	{ "ups.status",			0,	NULL,	"Q1\r",	"",	47,	'(',	"",	44,	44,	NULL,	QX_FLAG_QUICK_POLL,	NULL,	NULL,	blazer_process_status_bits },	/* Shutdown Active */
 	{ "ups.beeper.status",		0,	NULL,	"Q1\r",	"",	47,	'(',	"",	45,	45,	"%s",	0,			NULL,	NULL,	blazer_process_status_bits },	/* Beeper status */
 
-	/* Nominal parameters (ratings) */
-	{ "input.voltage.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	1,	5,	"%.0f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
-	{ "input.current.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	7,	9,	"%.1f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
-	{ "battery.voltage.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	11,	15,	"%.1f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
-	{ "input.frequency.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	17,	20,	"%.0f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
-
 	/* Instant commands */
-	{ "beeper.toggle",		0,	NULL,	"Q\r",		"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	NULL },
+	/* NB: seems like beeper toggling doesn't work for Innova RT 3/1 by Ippon - UPS simply doesn't respond  */
 	{ "load.off",			0,	NULL,	"S00R0000\r",	"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	NULL },
 	{ "load.on",			0,	NULL,	"C\r",		"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	NULL },
 	{ "shutdown.return",		0,	NULL,	"S%s\r",	"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	blazer_process_command },
@@ -140,11 +170,12 @@ static testing_t	innovart31_testing[] = {
 	{ "WA\r",	"(001.4 000.0 000.0 001.4 000.0 000.0 001.4 001.4 006.5 000.0 000.0 007 00000000\r",	-1 },
 	{ "BPS\r",	"(230.4 000.0 000.0 49.9\r",	-1 },
 	{ "F\r",	"#230.0 087 288.0 50.0\r",	-1 },
-	{ "Q\r",	"",	-1 },
-	{ "S03\r",	"",	-1 }, /* TODO recheck */
-	{ "C\r",	"",	-1 }, /* TODO recheck */
-	{ "S02R0005\r",	"",	-1 }, /* TODO recheck */
-	{ "S.5R0000\r",	"",	-1 }, /* TODO recheck */
+	{ "FW?\r",	"05516-1100\r",	-1 },
+	{ "SASV07?\r",	"GASV07DN05K10KB100004      \r",	-1 },
+	{ "S03\r",	"",	-1 },
+	{ "C\r",	"",	-1 },
+	{ "S02R0005\r",	"",	-1 },
+	{ "S.5R0000\r",	"",	-1 },
 	{ "T04\r",	"ACK",	-1 },
 	{ "TL\r",	"ACK",	-1 },
 	{ "T\r",	"ACK",	-1 },
