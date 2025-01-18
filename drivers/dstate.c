f05f7c00dd6552e69ca217af414b66ceb332a8ed
Jim Klimov <jimklimov+nut@gmail.com>
2025-01-18 21:55:27 +0100

    NEWS.adoc, conf/nut.conf.sample, drivers/dstate.c: introduce NUT_QUIET_INIT_LISTENER envvar [#2764]
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- drivers/dstate.c
+++ drivers/dstate.c
@@ -166,7 +166,8 @@ static TYPE_FD sock_open(const char *fn)
 		fatal_with_errno(EXIT_FAILURE, "listen(%d, %d) failed", fd, DS_LISTEN_BACKLOG);
 	}
 
-	upslogx(LOG_INFO, "Listening on socket %s", sockfn);
+	if (!getenv("NUT_QUIET_INIT_LISTENER"))
+		upslogx(LOG_INFO, "Listening on socket %s", sockfn);
 
 #else /* WIN32 */
 
@@ -201,7 +202,8 @@ static TYPE_FD sock_open(const char *fn)
 	/* Wait for a connection */
 	ConnectNamedPipe(fd, &connect_overlapped);
 
-	upslogx(LOG_INFO, "Listening on named pipe %s", fn);
+	if (!getenv("NUT_QUIET_INIT_LISTENER"))
+		upslogx(LOG_INFO, "Listening on named pipe %s", fn);
 
 #endif
 
