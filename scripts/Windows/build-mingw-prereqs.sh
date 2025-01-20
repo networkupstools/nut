40fd96f8ca81e0ca24fd8c30c7721bf6e9f6cc18
Jim Klimov <jimklimov+nut@gmail.com>
2025-01-20 12:09:30 +0100

    scripts/Windows/build-mingw-prereqs.sh: add a way to provide_libmodbus_git() [#2609, #1473, #1491]
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- scripts/Windows/build-mingw-prereqs.sh
+++ scripts/Windows/build-mingw-prereqs.sh
@@ -141,8 +141,99 @@ provide_netsnmp() (
 	$SUDO $MAKE install
 )
 
+provide_libmodbus_git() (
+	# Provide the libmodbus version with RTU USB support
+	PKGCFG_NAME="libmodbus"
+	DEP_PRJNAME="libmodbus-git"
+	DEP_GITREPO="https://github.com/networkupstools/libmodbus"
+	DEP_VERSION="rtu_usb"	# Git branch/tag/commit point
+	DEP_DIRNAME="${DEP_PRJNAME}-${DEP_VERSION}"
+
+	FORCE=false
+	if [ x"${1-}" = x"-f" ] ; then FORCE=true ; fi
+
+	# Make sure we have current repo code checked out
+	cd "${DLDIR}"
+	if [ -d "${DEP_DIRNAME}" ] ; then
+		{
+			# FIXME: Use `git ls-remote {URL|.}` to not modify the local FS if
+			# there's nothing to change (avoid re-packaging of CI artifact cache)
+			cd "${DEP_DIRNAME}" && \
+			git fetch --tags && \
+			git fetch --all && \
+			git checkout "${DEP_VERSION}" && \
+			_GITDIFF="`git diff "origin/${DEP_VERSION}"`" && \
+			if [ -n "${_GITDIFF}" ] ; then
+				FORCE=true
+				# Ensure rebase etc. or fail
+				git pull || exit
+				./autogen.sh || exit
+			else
+				:
+			fi
+		} || { chmod -R +w "${DEP_DIRNAME}" || true ; rm -rf "${DEP_DIRNAME}" ; }
+	fi
+
+	cd "${DLDIR}"
+	if [ ! -d "${DEP_DIRNAME}/.git" ] ; then
+		FORCE=true
+		chmod -R +w "${DEP_DIRNAME}" || true
+		rm -rf "${DEP_DIRNAME}"
+		git clone "${DEP_GITREPO}" -b "${DEP_VERSION}" "${DEP_DIRNAME}" || exit
+	fi
+	if [ ! -s "${DEP_DIRNAME}/configure" ] || [ x"$FORCE" = x"true" ] ; then
+		(cd "${DEP_DIRNAME}" && ./autogen.sh) || exit
+	fi
+
+	set +e
+	if [ x"$FORCE" = x"false" ] ; then
+		# TODO: Check version - harder with git rolling
+		#if pkg-config --exists "$PKGCFG_NAME" ; then return 0 ; fi
+
+		# Quickly install if prebuilt
+		if [ -d "${WSDIR}/${DEP_DIRNAME}/.inst" ]; then (
+			cd "${WSDIR}/${DEP_DIRNAME}/.inst" || exit
+			(command -v rsync) && $SUDO rsync -cavPHK ./ / && exit
+			$SUDO cp -pr ./ / && exit
+			exit 1
+			) && return 0
+		fi
+
+		# no stashed .inst; any Makefile at least?
+		if [ -s "${WSDIR}/${DEP_DIRNAME}/Makefile" ]; then ( cd "${WSDIR}/${DEP_DIRNAME}" && $SUDO $MAKE install ) && return ; fi
+
+		# Not pre-built, fall through
+	fi
+
+	# (Re-)make and install from scratch
+	set -e
+
+	cd "${WSDIR}"
+	# Take care of read-only destdir pieces
+	chmod -R +w "./${DEP_DIRNAME}" || echo ""
+	rm -rf "./${DEP_DIRNAME}" || echo ""
+	mkdir -p "./${DEP_DIRNAME}" || exit
+	cd "./${DEP_DIRNAME}" || exit
+
+	"${DLDIR}/${DEP_DIRNAME}/configure" --prefix="$PREFIX" --with-libusb --enable-static --disable-shared --enable-Werror
+
+	$MAKE || exit
+	$MAKE check || echo "WARNING: make check is flaky or failed outright" >&2
+
+	# Beside leaving a pre-install location for future runs,
+	# this may build some more artifacts:
+	rm -rf "`pwd`/.inst" || echo ""
+	$MAKE DESTDIR="`pwd`/.inst" install || exit
+
+	# Summarize what we have got
+	find ./ -type f -name "*.dll" -o -name "*.a"
+
+	$SUDO $MAKE install
+)
+
 prepareEnv || exit
 
 # TODO: Loop, params, help, etc...
 # For now, let it pass "-f" to the builder
 provide_netsnmp "$@"
+provide_libmodbus_git "$@"
