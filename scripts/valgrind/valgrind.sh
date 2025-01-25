47daa47b2cb169eba5692dfb1c05f7aa2771ede9
Jim Klimov <jimklimov+nut@gmail.com>
2025-01-25 13:36:03 +0100

    scripts/valgrind/valgrind.sh: allow to customize the VALGRIND to be used
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- scripts/valgrind/valgrind.sh
+++ scripts/valgrind/valgrind.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (C) 2024 by Jim Klimov <jimklimov+nut@gmail.com>
+# Copyright (C) 2024-2025 by Jim Klimov <jimklimov+nut@gmail.com>
 
 SCRIPTDIR="`dirname "$0"`"
 SCRIPTDIR="`cd "$SCRIPTDIR" && pwd`"
@@ -8,11 +8,12 @@ SCRIPTDIR="`cd "$SCRIPTDIR" && pwd`"
 LTEXEC=""
 [ -n "${LIBTOOL-}" ] || LIBTOOL="`command -v libtool`" 2>/dev/null
 [ -z "${LIBTOOL}"  ] || LTEXEC="${LIBTOOL} --mode=execute "
+[ -n "${VALGRIND-}"  ] || VALGRIND="valgrind"
 
 # TODO: Also wrap tool=callgrind e.g. via $0 symlink name?
 
 exec ${LTEXEC} \
-valgrind \
+${VALGRIND} \
 	--tool=memcheck --verbose \
 	--leak-check=full --show-reachable=yes --error-exitcode=1 --show-error-list=yes \
 	--trace-children=yes --track-fds=yes --show-leak-kinds=all --track-origins=yes \
