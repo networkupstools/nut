fd1bf907a3de2f54818b34355bf0567a8741d8b6
RikyPlaza <rikyrod2001@gmail.com>
2025-03-26 16:35:29 +0000

    drivers/phoenixcontact_modbus.c: updated version
    Signed-off-by: RikyPlaza <rikyrod2001@gmail.com>

--- drivers/phoenixcontact_modbus.c
+++ drivers/phoenixcontact_modbus.c
@@ -26,7 +26,7 @@
 #include "nut_stdint.h"
 
 #define DRIVER_NAME	"NUT PhoenixContact Modbus driver"
-#define DRIVER_VERSION	"0.07"
+#define DRIVER_VERSION	"0.08"
 
 #define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
 #define MODBUS_SLAVE_ID 192
