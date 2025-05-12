63c767503510c69c11895ede1186a525f6d244cc
Jim Klimov <jimklimov+nut@gmail.com>
2025-05-12 13:47:58 +0200

    Makefile.am, data/Makefile.am, tools/driver-list-format.sh, tools/Makefile.am: add a check (not change) mode for driver.list markup [#2952]
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- tools/driver-list-format.sh
+++ tools/driver-list-format.sh
@@ -18,15 +18,29 @@ else
 	exit 1
 fi
 
-echo "Checking whether driver.list[.in] are well formatted"
+ACTION="Ensuring"
+if [ x"${1-}" = x"--check" ] ; then
+	ACTION="Checking"
+fi
+
+RES=0
+
+echo "$ACTION whether driver.list[.in] are well formatted"
 for drvfile in driver.list.in driver.list
 do
 	if [ -f "${DRVLIST_PATH}/data/${drvfile}" ]; then
-		sed -e '/^#/!s/\" \+\"/\"\t\"/g' -e "/^#/!s/[[:blank:]]*$//" < "${DRVLIST_PATH}/data/${drvfile}" > "${DRVLIST_PATH}/data/${drvfile}.tabbed"
-		mv -f "${DRVLIST_PATH}/data/${drvfile}.tabbed" "${DRVLIST_PATH}/data/${drvfile}"
+		sed -e '/^#/!s/\" \+\"/\"\t\"/g' -e "/^#/!s/[[:blank:]]*$//" < "${DRVLIST_PATH}/data/${drvfile}" > "${DRVLIST_PATH}/data/${drvfile}.tabbed" && \
+		if [ x"${ACTION}" = xEnsuring ] ; then
+			mv -f "${DRVLIST_PATH}/data/${drvfile}.tabbed" "${DRVLIST_PATH}/data/${drvfile}"
+		else # Checking
+			diff "${DRVLIST_PATH}/data/${drvfile}.tabbed" "${DRVLIST_PATH}/data/${drvfile}" >/dev/null \
+			|| { echo "File '${DRVLIST_PATH}/data/${drvfile}' markup needs to be fixed (run $0 and commit the git change, please)" >&2 ; RES=1 ; }
+		fi || RES=$?
 		echo "Processed ${DRVLIST_PATH}/data/${drvfile}"
 	else
 		echo "Skipping ${drvfile} as it is missing..."
 	fi
 done
 echo "done"
+
+exit $RES
