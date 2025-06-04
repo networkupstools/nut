c5c30c26596b51f8a8cd6eb9db7b2a4717fe237b
gksmsk <90905316+gksmsk@users.noreply.github.com>
2025-06-04 12:50:53 +0300

    Create nutdrv_qx_megatec.c
    Added a parameter battery.runtime

---
+++ nutdrv_qx_megatec.c
@@ -0,0 +1,149 @@
+/* nutdrv_qx_megatec.c - Subdriver for Megatec protocol based UPSes
+ *
+ * Copyright (C)
+ *   2013 Daniele Pezzini <hyouko@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+#include "main.h"
+#include "nutdrv_qx.h"
+#include "nutdrv_qx_blazer-common.h"
+
+#include "nutdrv_qx_megatec.h"
+
+#define MEGATEC_VERSION "Megatec 0.07"
+
+/* qx2nut lookup table */
+static item_t	megatec_qx2nut[] = {
+
+	/*
+	 * > [Q1\r]
+	 * < [(226.0 195.0 226.0 014 49.0 27.5 30.0 00001000\r]
+	 *    01234567890123456789012345678901234567890123456
+	 *    0         1         2         3         4
+	 */
+
+	{ "input.voltage",		0,	NULL,	"Q1\r",	"",	47,	'(',	"",	1,	5,	"%.1f",	0,	NULL,	NULL,	NULL },
+	{ "input.voltage.fault",	0,	NULL,	"Q1\r",	"",	47,	'(',	"",	7,	11,	"%.1f",	0,	NULL,	NULL,	NULL },
+	{ "output.voltage",		0,	NULL,	"Q1\r",	"",	47,	'(',	"",	13,	17,	"%.1f",	0,	NULL,	NULL,	NULL },
+	{ "ups.load",			0,	NULL,	"Q1\r",	"",	47,	'(',	"",	19,	21,	"%.0f",	0,	NULL,	NULL,	NULL },
+	{ "input.frequency",		0,	NULL,	"Q1\r",	"",	47,	'(',	"",	23,	26,	"%.1f",	0,	NULL,	NULL,	NULL },
+	{ "battery.voltage",		0,	NULL,	"Q1\r",	"",	47,	'(',	"",	28,	31,	"%.2f",	0,	NULL,	NULL,	qx_multiply_battvolt },
+	{ "ups.temperature",		0,	NULL,	"Q1\r",	"",	47,	'(',	"",	33,	36,	"%.1f",	0,	NULL,	NULL,	NULL },
+	/* Status bits */
+	{ "ups.status",			0,	NULL,	"Q1\r",	"",	47,	'(',	"",	38,	38,	NULL,	QX_FLAG_QUICK_POLL,	NULL,	NULL,	blazer_process_status_bits },	/* Utility Fail (Immediate) */
+	{ "ups.status",			0,	NULL,	"Q1\r",	"",	47,	'(',	"",	39,	39,	NULL,	QX_FLAG_QUICK_POLL,	NULL,	NULL,	blazer_process_status_bits },	/* Battery Low */
+	{ "ups.status",			0,	NULL,	"Q1\r",	"",	47,	'(',	"",	40,	40,	NULL,	QX_FLAG_QUICK_POLL,	NULL,	NULL,	blazer_process_status_bits },	/* Bypass/Boost or Buck Active */
+	{ "ups.alarm",			0,	NULL,	"Q1\r",	"",	47,	'(',	"",	41,	41,	NULL,	0,			NULL,	NULL,	blazer_process_status_bits },	/* UPS Failed */
+	{ "ups.type",			0,	NULL,	"Q1\r",	"",	47,	'(',	"",	42,	42,	"%s",	QX_FLAG_STATIC,		NULL,	NULL,	blazer_process_status_bits },	/* UPS Type */
+	{ "ups.status",			0,	NULL,	"Q1\r",	"",	47,	'(',	"",	43,	43,	NULL,	QX_FLAG_QUICK_POLL,	NULL,	NULL,	blazer_process_status_bits },	/* Test in Progress */
+	{ "ups.status",			0,	NULL,	"Q1\r",	"",	47,	'(',	"",	44,	44,	NULL,	QX_FLAG_QUICK_POLL,	NULL,	NULL,	blazer_process_status_bits },	/* Shutdown Active */
+	{ "ups.beeper.status",		0,	NULL,	"Q1\r",	"",	47,	'(',	"",	45,	45,	"%s",	0,			NULL,	NULL,	blazer_process_status_bits },	/* Beeper status */
+
+	/*
+	 * > [F\r]
+	 * < [#220.0 000 024.0 50.0\r]
+	 *    0123456789012345678901
+	 *    0         1         2
+	 */
+
+	{ "input.voltage.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	1,	5,	"%.0f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
+	{ "input.current.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	7,	9,	"%.1f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
+	{ "battery.voltage.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	11,	15,	"%.1f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
+	{ "input.frequency.nominal",	0,	NULL,	"F\r",	"",	22,	'#',	"",	17,	20,	"%.0f",	QX_FLAG_STATIC,	NULL,	NULL,	NULL },
+
+	/*
+	 * > [I\r]
+	 * < [#-------------   ------     VT12046Q  \r]
+	 *    012345678901234567890123456789012345678
+	 *    0         1         2         3
+	 */
+    /***********
+	Megatec IC adds serial/runtime support function
+	pc->ups:   I\r
+	ups->pc:   #MegaTecabcedfhi k130v12345 v3.400000\r
+	device.mfr=MegaTec
+	ups.serial=abcedfhi
+	device.model=k130v
+	battery.runtime=12345
+	ups.firmware=v3.400000
+	***************/
+	{ "device.mfr",			0,	NULL,	"I\r",	"",	39,	'#',	"",	1,	7,	"%s",	QX_FLAG_STATIC | QX_FLAG_TRIM,	NULL,	NULL,	NULL },//
+	{ "ups.serial",			0,	NULL,	"I\r",	"",	39,	'#',	"",	8,	15,	"%s",	0,	NULL,	NULL,	NULL },//Megatec IC adds "ups.serial" support function
+	{ "device.model",		0,	NULL,	"I\r",	"",	39,	'#',	"",	17,	21,	"%s",	QX_FLAG_STATIC | QX_FLAG_TRIM,	NULL,	NULL,	NULL },
+	{ "battery.runtime",		0,	NULL,	"I\r",	"",	39,	'#',	"",	22,	26,	"%s",	0,	NULL,	NULL,	NULL },//Megatec IC adds "ups.runtime" support function 
+	{ "ups.firmware",		0,	NULL,	"I\r",	"",	39,	'#',	"",	28,	37,	"%s",	QX_FLAG_STATIC | QX_FLAG_TRIM,	NULL,	NULL,	NULL },//
+
+	/* Instant commands */
+	{ "beeper.toggle",		0,	NULL,	"Q\r",		"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	NULL },
+	{ "load.off",			0,	NULL,	"S00R0000\r",	"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	NULL },
+	{ "load.on",			0,	NULL,	"C\r",		"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	NULL },
+	{ "shutdown.return",		0,	NULL,	"S%s\r",	"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	blazer_process_command },
+	{ "shutdown.stayoff",		0,	NULL,	"S%sR0000\r",	"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	blazer_process_command },
+	{ "shutdown.stop",		0,	NULL,	"C\r",		"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	NULL },
+	{ "test.battery.start",		0,	NULL,	"T%02d\r",	"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	blazer_process_command },
+	{ "test.battery.start.deep",	0,	NULL,	"TL\r",		"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	NULL },
+	{ "test.battery.start.quick",	0,	NULL,	"T\r",		"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	NULL },
+	{ "test.battery.stop",		0,	NULL,	"CT\r",		"",	0,	0,	"",	0,	0,	NULL,	QX_FLAG_CMD,	NULL,	NULL,	NULL },
+
+	/* Server-side settable vars */
+	{ "ups.delay.start",		ST_FLAG_RW,	blazer_r_ondelay,	NULL,	"",	0,	0,	"",	0,	0,	DEFAULT_ONDELAY,	QX_FLAG_ABSENT | QX_FLAG_SETVAR | QX_FLAG_RANGE,	NULL,	NULL,	blazer_process_setvar },
+	{ "ups.delay.shutdown",		ST_FLAG_RW,	blazer_r_offdelay,	NULL,	"",	0,	0,	"",	0,	0,	DEFAULT_OFFDELAY,	QX_FLAG_ABSENT | QX_FLAG_SETVAR | QX_FLAG_RANGE,	NULL,	NULL,	blazer_process_setvar },
+
+	/* End of structure. */
+	{ NULL,				0,	NULL,	NULL,		"",	0,	0,	"",	0,	0,	NULL,	0,	NULL,	NULL,	NULL }
+};
+
+/* Testing table */
+#ifdef TESTING
+static testing_t	megatec_testing[] = {
+	{ "Q1\r",	"(215.0 195.0 230.0 014 49.0 22.7 30.0 00000000\r",	-1 },
+	{ "F\r",	"#230.0 000 024.0 50.0\r",	-1 },
+	{ "I\r",	"#NOT_A_LIVE_UPS  TESTING    TESTING   \r",	-1 },
+	{ "Q\r",	"",	-1 },
+	{ "S03\r",	"",	-1 },
+	{ "C\r",	"",	-1 },
+	{ "S02R0005\r",	"",	-1 },
+	{ "S.5R0000\r",	"",	-1 },
+	{ "T04\r",	"",	-1 },
+	{ "TL\r",	"",	-1 },
+	{ "T\r",	"",	-1 },
+	{ "CT\r",	"",	-1 },
+	{ NULL }
+};
+#endif	/* TESTING */
+
+/* Subdriver-specific initups */
+static void	megatec_initups(void)
+{
+	blazer_initups(megatec_qx2nut);
+}
+
+/* Subdriver interface */
+subdriver_t	megatec_subdriver = {
+	MEGATEC_VERSION,
+	blazer_claim,
+	megatec_qx2nut,
+	megatec_initups,
+	NULL,
+	blazer_makevartable,
+	"ACK",
+	NULL,
+#ifdef TESTING
+	megatec_testing,
+#endif	/* TESTING */
+};
