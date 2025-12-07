fd62ccfde4ea54fb0ea98fcaef54970431f2e957
Jim Klimov <jimklimov+nut@gmail.com>
2025-12-07 15:15:35 +0100

    NEWS.adoc, drivers/libhid.c::HIDGetIndexString(), drivers/usb-common.c::nut_usb_get_string(): trace the bytes going around [#3201, #2604]
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- drivers/libhid.c
+++ drivers/libhid.c
@@ -559,6 +559,7 @@ int HIDGetItemValue(hid_dev_handle_t udev, const char *hidpath, double *Value, u
 char *HIDGetIndexString(hid_dev_handle_t udev, const int Index, char *buf, size_t buflen)
 {
 	usb_ctrl_strindex	idx;
+	int	ret;
 
 #if (defined HAVE_PRAGMA_GCC_DIAGNOSTIC_PUSH_POP) && ( (defined HAVE_PRAGMA_GCC_DIAGNOSTIC_IGNORED_TYPE_LIMITS) || (defined HAVE_PRAGMA_GCC_DIAGNOSTIC_IGNORED_TAUTOLOGICAL_CONSTANT_OUT_OF_RANGE_COMPARE) || (defined HAVE_PRAGMA_GCC_DIAGNOSTIC_IGNORED_TAUTOLOGICAL_UNSIGNED_ZERO_COMPARE) || (defined HAVE_PRAGMA_GCC_DIAGNOSTIC_IGNORED_UNREACHABLE_CODE) || (defined HAVE_PRAGMA_GCC_DIAGNOSTIC_IGNORED_TAUTOLOGICAL_TYPE_LIMIT_COMPARE) )
 # pragma GCC diagnostic push
@@ -626,8 +627,15 @@ char *HIDGetIndexString(hid_dev_handle_t udev, const int Index, char *buf, size_
 # pragma GCC diagnostic pop
 #endif
 
-	if (comm_driver->get_string(udev, idx, buf, (usb_ctrl_charbufsize)buflen) < 1)
+	ret = comm_driver->get_string(udev, idx, buf, (usb_ctrl_charbufsize)buflen);
+	upsdebugx(6, "%s: get_string() at index %d returned %d %s",
+		__func__, Index, ret,
+		ret < 0 ? "error code" : "useful bytes");
+	if (ret < 1)
 		buf[0] = '\0';
+	else
+		upsdebug_hex(6, "...into data buffer", buf,
+			((size_t)ret < buflen ? (size_t)ret : buflen));
 
 	return str_rtrim(buf, '\n');
 }
