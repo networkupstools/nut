3d3522e5c78b468e79610f5ff5e54e3fa94b231b
Jim Klimov <jimklimov+nut@gmail.com>
2026-02-02 10:44:14 +0100

    drivers/apc_modbus.c: bump DRIVER_VERSION due to refined setting of mapping table entry types etc. via enum values [#3293]
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- drivers/apc_modbus.c
+++ drivers/apc_modbus.c
@@ -44,7 +44,7 @@
 #endif
 
 #define DRIVER_NAME	"NUT APC Modbus driver " DRIVER_NAME_NUT_MODBUS_HAS_USB_WITH_STR " USB support (libmodbus link type: " NUT_MODBUS_LINKTYPE_STR ")"
-#define DRIVER_VERSION	"0.17"
+#define DRIVER_VERSION	"0.18"
 
 #if defined NUT_MODBUS_HAS_USB
 
