63ed473558af35766e4d76d695c83644117d8c98
jawz101 <jawz101@users.noreply.github.com>
2026-02-02 17:46:45 -0600

    Enhance Vertiv PSI5 MIB with control features
    Added shutdown and start delay control for UPS.

--- drivers/vertiv-psi5-mib.c
+++ drivers/vertiv-psi5-mib.c
@@ -1,28 +1,9 @@
-/* drivers/vertiv-psi5-mib.c - Driver for Vertiv Liebert PSI5 UPS
- *
- *	Copyright (C)
- *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 2 of the License, or
- *	(at your option) any later version.
- *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
- *
- *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- */
-
+/* drivers/vertiv-psi5-mib.c */
 #include "vertiv-psi5-mib.h"
 
 /* Vertiv PSI5-750MT120 Mapping 
-   Based on integer branch at .1.3.6.1.4.1.476.1.42.3.9.30... */
+   Monitoring: Based on integer branch at .1.3.6.1.4.1.476.1.42.3.9.30... 
+   Control: Based on standard Liebert Environmental MIB at .1.3.6.1.4.1.476.1.42.3.3.5... */
 
 static snmp_info_t vertiv_psi5_mib[] = {
 	/* Load and Temperature */
@@ -39,7 +20,7 @@ static snmp_info_t vertiv_psi5_mib[] = {
 		NULL, NULL },
 	{ "battery.runtime", 0, SU_INFOTYPE_INT, NULL,
 		".1.3.6.1.4.1.476.1.42.3.9.30.1.20.1.2.1.4150",
-		NULL, NULL }, /* Note: Raw value is minutes, NUT usually expects seconds */
+		NULL, NULL }, /* Raw value is minutes */
 	{ "battery.voltage", 0, SU_INFOTYPE_INT, NULL,
 		".1.3.6.1.4.1.476.1.42.3.9.30.1.20.1.2.1.4148",
 		NULL, NULL },
@@ -62,6 +43,15 @@ static snmp_info_t vertiv_psi5_mib[] = {
 	{ "output.power", 0, SU_INFOTYPE_INT, NULL,
 		".1.3.6.1.4.1.476.1.42.3.9.30.1.20.1.2.1.4208",
 		SU_FLAG_OK, NULL },
+
+	/* Shutdown / Restart Control 
+	   Derived from lgpEnvControl (LIEBERT-GP-ENVIRONMENTAL-MIB) */
+	{ "ups.delay.shutdown", 0, SU_INFOTYPE_INT, 
+		".1.3.6.1.4.1.476.1.42.3.3.5.1.0", 
+		SU_FLAG_RW | SU_FLAG_OK, NULL },
+	{ "ups.delay.start", 0, SU_INFOTYPE_INT, 
+		".1.3.6.1.4.1.476.1.42.3.3.5.2.0", 
+		SU_FLAG_RW | SU_FLAG_OK, NULL },
 
 	/* Status Flags */
 	{ "ups.alarm", 0, SU_INFOTYPE_INT, NULL,
