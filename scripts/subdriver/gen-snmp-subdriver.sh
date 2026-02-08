networkupstools/nut

commit 8b038f41e4f9ec6e234865f380c77eca5d4138b8

scripts/subdriver/gen-snmp-subdriver.sh: avoid "GREP -e", it is also not ubiquitous [#3099]
Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

scripts/subdriver/gen-snmp-subdriver.sh
@@ -456,8 +456,8 @@ natural (upper- and lowercase) capitalization, e.g., 'Belkin', 'APC'."
 done
 
 # remove blank and "End of MIB" lines
-${EGREP} -e "^[[:space:]]?$" -e "End of MIB" -v "${NUMWALKFILE}" > "${TMP_NUMWALKFILE}"
-${EGREP} -e "^[[:space:]]?$" -e "End of MIB" -v "${STRWALKFILE}" > "${TMP_STRWALKFILE}"
+${EGREP} "^[[:space:]]?$" | ${GREP} "End of MIB" | ${GREP} -v "${NUMWALKFILE}" > "${TMP_NUMWALKFILE}"
+${EGREP} "^[[:space:]]?$" | ${GREP} "End of MIB" | ${GREP} -v "${STRWALKFILE}" > "${TMP_STRWALKFILE}"
 NUMWALKFILE="${TMP_NUMWALKFILE}"
 STRWALKFILE="${TMP_STRWALKFILE}"
 

commit 4385bed3c4360c235526a5288f1b1050bc3a9b85

Fix hang in gen-snmp-subdriver.sh
Fix an issue introduced in commit 8b038f4 whereby grep-snmp-subdriver.sh hangs indefinitely.

Issue https://github.com/networkupstools/nut/issues/3308

scripts/subdriver/gen-snmp-subdriver.sh
@@ -487,8 +487,8 @@ done
 
 # remove blank and "End of MIB" lines
 TABCHAR="`printf '\t'`"
-${EGREP} "^[ ${TABCHAR}]?\$" | ${GREP} "End of MIB" | ${GREP} -v "${NUMWALKFILE}" > "${TMP_NUMWALKFILE}"
-${EGREP} "^[ ${TABCHAR}]?\$" | ${GREP} "End of MIB" | ${GREP} -v "${STRWALKFILE}" > "${TMP_STRWALKFILE}"
+${EGREP} -v "^[ ${TABCHAR}]?\$" "${NUMWALKFILE}" | ${GREP} -v "End of MIB" > "${TMP_NUMWALKFILE}"
+${EGREP} -v "^[ ${TABCHAR}]?\$" "${STRWALKFILE}" | ${GREP} -v "End of MIB" > "${TMP_STRWALKFILE}"
 NUMWALKFILE="${TMP_NUMWALKFILE}"
 STRWALKFILE="${TMP_STRWALKFILE}"
 