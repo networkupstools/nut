bf410e78b0a6a2e7dc5b87cfc266158d65c4c889
Jim Klimov <jimklimov+nut@gmail.com>
2026-02-26 23:02:03 +0100

    server/conf.c and docs: Extended processing of `CERTREQUEST` setting to handle numeric or specific string values, to match both ways of reading ambiguous documentation [#3330]
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- server/netssl.c
+++ server/netssl.c
@@ -457,7 +457,7 @@ void net_starttls(nut_ctype_t *client, size_t numarg, const char **arg)
 	if (status != SECSuccess) {
 		PRErrorCode code = PR_GetError();
 		if (code==SSL_ERROR_NO_CERTIFICATE) {
-			upslogx(LOG_WARNING, "Client %s do not provide certificate.",
+			upslogx(LOG_WARNING, "Client %s did not provide any certificate.",
 				client->addr);
 		} else {
 			nss_error("net_starttls / SSL_ForceHandshake");
@@ -634,14 +634,16 @@ void ssl_init(void)
 	}
 
 #ifdef WITH_CLIENT_CERTIFICATE_VALIDATION
-	if (certrequest < NETSSL_CERTREQ_NO &&
-		certrequest > NETSSL_CERTREQ_REQUEST) {
+	if (certrequest < NETSSL_CERTREQ_NO		/* < 0 */
+	 || certrequest > NETSSL_CERTREQ_REQUIRE	/* > 2 */
+	) {
 		upslogx(LOG_ERR, "Invalid certificate requirement");
 		return;
 	}
 
-	if (certrequest == NETSSL_CERTREQ_REQUEST ||
-		certrequest == NETSSL_CERTREQ_REQUIRE ) {
+	if (certrequest == NETSSL_CERTREQ_REQUEST
+	 || certrequest == NETSSL_CERTREQ_REQUIRE
+	) {
 		status = SSL_OptionSetDefault(SSL_REQUEST_CERTIFICATE, PR_TRUE);
 		if (status != SECSuccess) {
 			upslogx(LOG_ERR, "Can not enable certificate request");
@@ -650,7 +652,7 @@ void ssl_init(void)
 		}
 	}
 
-	if (certrequest == NETSSL_CERTREQ_REQUIRE ) {
+	if (certrequest == NETSSL_CERTREQ_REQUIRE) {
 		status = SSL_OptionSetDefault(SSL_REQUIRE_CERTIFICATE, PR_TRUE);
 		if (status != SECSuccess) {
 			upslogx(LOG_ERR, "Can not enable certificate requirement");
