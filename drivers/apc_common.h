1fdc5380c1dcc1236a8b2f49732154054664b581
Lukas Schmid <lukas.schmid@netcube.li>
2026-05-18 22:17:11 +0200

    add Axel Gembe copyright to apc_common as the code is at the very least inspired by the original code from apc-modbus
    Signed-off-by: Lukas Schmid <lukas.schmid@netcube.li>

--- drivers/apc_common.h
+++ drivers/apc_common.h
@@ -1,5 +1,6 @@
 /* apc_common.h - Shared APC driver helpers
  *
+ * Copyright (C) 2023 Axel Gembe <axel@gembe.net>
  * Copyright (C) 2026 Lukas Schmid <lukas.schmid@netcube.li>
  *
  * This program is free software; you can redistribute it and/or modify
