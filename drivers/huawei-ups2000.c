networkupstools/nut

commit e9f02e242acf09e95a32413a26dceb44b112547d

drivers/huawei-ups2000.c: instcmd(): range-check and cast for ups2000_write_register() and handler_func()

drivers/huawei-ups2000.c
@@ -1495,13 +1495,18 @@ static int instcmd(const char *cmd, const char *extra)
 
 	if (cmd_action->handler_func) {
 		/* handled by a function */
-		status = cmd_action->handler_func(cmd_action->reg1);
+		if (cmd_action->reg1 < 0) {
+			upslogx(LOG_WARNING, "instcmd: command [%s] reg1 is negative", cmd);
+			return STAT_INSTCMD_UNKNOWN;
+		} else {
+			status = cmd_action->handler_func((uint16_t)cmd_action->reg1);
+		}
 	}
-	else if (cmd_action->reg1 != -1 && cmd_action->val1 != -1) {
+	else if (cmd_action->reg1 >= 0 && cmd_action->val1 >= 0) {
 		/* handled by a register write */
 		int r = ups2000_write_register(modbus_ctx,
 					       10000 + cmd_action->reg1,
-					       cmd_action->val1);
+					       (uint16_t)cmd_action->val1);
 		if (r == 1)
 			status = STAT_INSTCMD_HANDLED;
 		else
@@ -1511,10 +1516,10 @@ static int instcmd(const char *cmd, const char *extra)
 		 * if the previous write succeeds and there is an additional
 		 * register to write.
 		 */
-		if (r == 1 && cmd_action->reg2 != -1 && cmd_action->val2 != -1) {
+		if (r == 1 && cmd_action->reg2 >= 0 && cmd_action->val2 >= 0) {
 			r = ups2000_write_register(modbus_ctx,
 						   10000 + cmd_action->reg2,
-						   cmd_action->val2);
+						   (uint16_t)cmd_action->val2);
 			if (r == 1)
 				status = STAT_INSTCMD_HANDLED;
 			else

commit 43b0de07c145ddc7cb3de301be45946690f18c53

huawei-ups2000: fix broken instant command invocations, close #3603.
A serious regression was introduced during the "fightwarn" campaign that
completely broke bypass.start, shutdown.return, shutdown.reboot, and
shutdown.reboot.graceful. Any attempt to use them will fail, with an error
logged to `syslog`:

    huawei-ups2000: instcmd: command [bypass.start] reg1 is negative
    huawei-ups2000: instcmd: command [shutdown.return] reg1 is negative
    huawei-ups2000: instcmd: command [shutdown.reboot] reg1 is negative
    huawei-ups2000: instcmd: command [shutdown.reboot.graceful] reg1 is negative

Because the regression was introduced shortly after the initial driver
was merged into the upstream, these instant commands do no work in any
NUT version (with the exception of the earliest development build). This
problem was not discovered during pre-merge test for the same reason: it's
a regression that occurred after the Pull Request was merged.

In huawei-ups2000, all instant commands are driven by a lookup table to
consolidate the logic to a central dispatcher. Some commands are dispatched
to a (*handler_func)(uint16_t reg1) function, which is used to handle commands
that needs additional processing.

Not all *handler_func actually reads "reg1", it's an optional argument.
Because declaring the function to accept a variadic or a (void *) would
be over-engineering, a simple unsigned 16-bit value is used, *handler_func
may or may not use it.

During the project-wide "fightwarn" refactor campaign, it was noticed that
"reg1" is stored in the lookup table as "int16_t" (-1 indicates an unneeded
register), but *handler_func accepts a "uint16_t". In case that *handler_func
doesn't need the register ID from the lookup table, "reg1" is implicitly
converted from -1 to 65535. This is harmless, since "reg1" is not actually
used in this case.

Unfortunately, in an attempt to make the code "safe" by suppressing compiler
warning, an incorrect range check was added to the function, which refuses
to invoke *handler_func if "reg1" is negative.

As a result, all instant commands that don't use "reg1" are broken. These
commands include bypass.start, shutdown.return, shutdown.reboot, and
shutdown.reboot.graceful.

To fix this problem, fix the implicit type conversion correctly by change
the "reg1" data type in the lookup table from "int16_t" to "uint16_t", and
remove the "negative reg1" check. As defensive programming. all *handler_func
that actually need "reg1" would check and abort if "reg1" is 0.

Fixes: e9f02e242acf ("drivers/huawei-ups2000.c: instcmd(): range-check and cast for ups2000_write_register() and handler_func()")

drivers/huawei-ups2000.c
@@ -1446,14 +1446,17 @@ static int ups2000_delay_set(const char *var, const char *string)
  *
  * 3. Calling "*handler_func" and passing "reg1". This is
  * used to handle commands that needs additional processing.
- * If "reg1" is not necessary or unsuitable, "-1" is used.
+ * If "reg1" is not necessary or unsuitable, "0" is used.
  */
-#define REG_NULL  -1, -1
+#define REG_NULL  0, -1
 #define FUNC_NULL NULL
 
 static struct ups2000_cmd_t {
 	const char *cmd;
-	const int16_t reg1, val1, reg2, val2;
+	const uint16_t reg1;
+	const int16_t val1;
+	const uint16_t reg2;
+	const int16_t val2;
 	int (*const handler_func)(const uint16_t);
 } ups2000_cmd[] =
 {
@@ -1471,7 +1474,7 @@ static struct ups2000_cmd_t {
 	{ "shutdown.return",          REG_NULL, REG_NULL, ups2000_instcmd_shutdown_return          },
 	{ "shutdown.reboot",          REG_NULL, REG_NULL, ups2000_instcmd_shutdown_reboot          },
 	{ "shutdown.reboot.graceful", REG_NULL, REG_NULL, ups2000_instcmd_shutdown_reboot_graceful },
-	{ NULL, -1, -1, -1, -1, NULL },
+	{ NULL, 0, -1, 0, -1, NULL },
 };
 
 
@@ -1510,15 +1513,9 @@ static int instcmd(const char *cmd, const char *extra)
 
 	if (cmd_action->handler_func) {
 		/* handled by a function */
-		if (cmd_action->reg1 < 0) {
-			/* FIXME: ...INSTCMD_CONVERSION_FAILED ? */
-			upslogx(LOG_INSTCMD_UNKNOWN, "instcmd: command [%s] reg1 is negative", cmd);
-			return STAT_INSTCMD_UNKNOWN;
-		} else {
-			status = cmd_action->handler_func((uint16_t)cmd_action->reg1);
-		}
+		status = cmd_action->handler_func(cmd_action->reg1);
 	}
-	else if (cmd_action->reg1 >= 0 && cmd_action->val1 >= 0) {
+	else if (cmd_action->reg1 > 0 && cmd_action->val1 >= 0) {
 		/* handled by a register write */
 		int r = ups2000_write_register(modbus_ctx,
 			10000 + cmd_action->reg1,
@@ -1532,7 +1529,7 @@ static int instcmd(const char *cmd, const char *extra)
 		 * if the previous write succeeds and there is an additional
 		 * register to write.
 		 */
-		if (r == 1 && cmd_action->reg2 >= 0 && cmd_action->val2 >= 0) {
+		if (r == 1 && cmd_action->reg2 > 0 && cmd_action->val2 >= 0) {
 			r = ups2000_write_register(modbus_ctx,
 				10000 + cmd_action->reg2,
 				(uint16_t)cmd_action->val2);
@@ -1558,6 +1555,12 @@ static int ups2000_instcmd_load_on(const uint16_t reg)
 {
 	int r;
 	const char *status;
+
+	if (reg == 0) {
+		upslogx(LOG_INSTCMD_FAILED,
+			"invalid register in LUT, please file a bug report!");
+		return STAT_INSTCMD_FAILED;
+	}
 
 	/* force refresh UPS status */
 	status_init();
@@ -1650,6 +1653,12 @@ static int ups2000_instcmd_beeper_toggle(const uint16_t reg)
 	int r;
 	const char *string;
 
+	if (reg == 0) {
+		upslogx(LOG_INSTCMD_FAILED,
+			"invalid register in LUT, please file a bug report!");
+		return STAT_INSTCMD_FAILED;
+	}
+
 	r = ups2000_beeper_get(reg);
 	if (r != 0)
 		return STAT_INSTCMD_FAILED;
@@ -1677,6 +1686,12 @@ static int ups2000_instcmd_shutdown_stayoff(const uint16_t reg)
 {
 	uint16_t val;
 	int r;
+
+	if (reg == 0) {
+		upslogx(LOG_INSTCMD_FAILED,
+			"invalid register in LUT, please file a bug report!");
+		return STAT_INSTCMD_FAILED;
+	}
 
 	r = setvar("ups.start.auto", "no");
 	if (r != STAT_SET_HANDLED)